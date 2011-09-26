7d0d7accf2df4ed7fbb4196ae55d81cf4caaf99d
Piotr Murach <pmurach@gmail.com>
2011-09-26 19:58:30 +0100

    Add forks and keys apis.

---
+++ lib/github/repos/forks.rb
@@ -0,0 +1,27 @@
+module Github
+  module Repos
+    module Forks
+      
+      # List forks 
+      #
+      # GET /repos/:user/:repo/forks
+      #
+      # Examples
+      #
+      def list(user, repo)
+        get("/repos/#{user}/#{repo}/forks")
+      end
+      
+      # Create a fork for the authenticated user
+      # 
+      # POST /repos/:user/:repo/forks
+      #
+      def create(user, repo, params={})
+        _normalize_params_keys(params)
+        _filter_params_keys(%w[ org ], params)
+
+        post("/repos/#{user}/#{repo}/forks", params)
+      end
+    end
+  end
+end # Github
---
+++ lib/github/repos/keys.rb
@@ -0,0 +1,51 @@
+module Github
+  module Repos
+    module Keys
+      
+      # List keys
+      #
+      # GET /repos/:user/:repo/keys
+      #
+      def list(user, repo)
+        get("/repos/#{user}/#{repo}/keys")
+      end
+
+      # Get a key
+      #
+      # GET /repos/:user/:repo/keys/:id
+      #
+      def get(user, repo, key_id)
+        get("/repos/#{user}/#{repo}/keys/#{key_id}")
+      end
+
+      # Create a key
+      #
+      # POST /repos/:user/:repo/keys
+      def create(user, repo, params={})
+        _normalize_params_keys(params)
+        _filter_params_keys(%w[ title key ], params)
+
+        post("/repos/#{user}/#{repo}/keys", params)
+      end
+
+      # Edit key
+      #
+      # PATCH /repos/:user/:repo/keys/:id
+      #
+      def edit(user, repo, key_id)
+        _normalize_params_keys(params)
+        _filter_params_keys(%w[ title key ], params)
+
+        patch("/repos/#{user}/#{repo}/keys/#{key_id}")
+      end
+      
+      # Delete key
+      #
+      # DELETE /repos/:user/:repo/keys/:id
+      def delete(user, repo, key_id)
+        delete("/repos/#{user}/#{repo}/keys/#{key_id}")
+      end
+
+    end # Keys
+  end # Repos
+end # Github
