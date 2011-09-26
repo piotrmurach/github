8da0ac0979a0a8d30b19c0766ed6e0374d15c218
Piotr Murach <pmurach@gmail.com>
2011-09-26 12:18:11 +0100

    Add specs structure.

---
+++ spec/github/client_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+describe "Github::Client" do
+  
+  it "should have some tests"
+end
---
+++ spec/github/repos_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe "Repos" do
+  
+  context "list" do
+    it "should list all repos by default" 
+  end
+end
