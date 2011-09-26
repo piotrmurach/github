a6843641ba6cbc7ab3af1ec732c36dd3506f938e
Piotr Murach <pmurach@gmail.com>
2011-09-26 20:40:50 +0100

    Add hooks and watching apis to repositories.

---
+++ lib/github/repos/hooks.rb
@@ -0,0 +1,65 @@
+module Github
+  module Repos
+    module Hooks
+
+      REQUIRED_PARAMS = %w[ name config ]
+
+      # List repository hooks
+      # 
+      # GET /repos/:user/:repo/hooks
+      #
+      def list(user, repo)
+        get("/repos/#{user}/#{repo}/hooks")
+      end
+
+      # Get a single hook 
+      #
+      # GET /repos/:user/:repo/hooks/:id
+      #
+      def get(user, repo, hook_id)
+        get("/repos/#{user}/#{repo}/hooks/#{hook_id}")
+      end
+
+      # Create a hook
+      #
+      # POST /repos/:user/:repo/hooks
+      #
+      def create(user, repo, params)
+        _normalize_params_keys(params)
+        _filter_params_keys(%w[ name config active ], params)
+        raise ArgumentError, "Required parameters are: #{REQUIRED_PARAMS.join(', ')}" unless _validate_inputs(REQUIRED_PARAMS, params)
+
+        post("/repos/#{user}/#{repo}/hooks", params)
+      end
+
+      # Edit a hook
+      #
+      # PATCH /repos/:user/:repo/hooks/:id
+      # 
+      def edit(user, repo, hook_id, params)
+        _normalize_params_keys(params)
+        _filter_params_keys(%w[ name config active ], params)
+        raise ArgumentError, "Required parameters are: #{REQUIRED_PARAMS.join(', ')}" unless _validate_inputs(REQUIRED_PARAMS, params)
+
+        patch("/repos/#{user}/#{repo}/hooks/#{hook_id}")
+      end
+
+      # Test a hook
+      #
+      # POST /repos/:user/:repo/hooks/:id/test
+      #
+      def test(user, repo, hook_id)
+        post("/repos/#{user}/#{repo}/hooks/#{hook_id}/test")
+      end
+
+      # Delete a hook
+      #
+      # DELETE /repos/:user/:repo/hooks/:id
+      #
+      def delete(user, repo, hook_id)
+        delete("/repos/#{user}/#{repo}/hooks/#{hook_id}")
+      end
+
+    end # Hooks
+  end # Repos
+end # Github
---
+++ lib/github/repos/watching.rb
@@ -0,0 +1,48 @@
+module Github
+  module Repos
+    module Watching
+      
+      # List repo watchers
+      #
+      # GET /repos/:user/:repo/watchers
+      #
+      def watchers
+        get("/repos/#{user}/#{repo}/watchers")
+      end
+      
+      # List repos being watched
+      def watched(user=nil)
+        if user
+          get("/users/#{user}/watched")
+        else
+          get("/user/watched")
+        end
+      end
+
+      # Check if you are watching a repo
+      #
+      # GET /user/watched/:user/:repo
+      #
+      def watching?(user, repo)
+        get("/user/watched/#{user}/#{repo}")
+      end
+
+      # Watch a repo 
+      #
+      # PUT /user/watched/:user/:repo
+      #
+      def start_watching(user, repo)
+        put("/user/watched/#{user}/#{repo}")
+      end
+      
+      # Stop watching a repo
+      #
+      # DELETE /user/watched/:user/:repo
+      #
+      def stop_watching(user, repo)
+        delete("/user/watched/#{user}/#{repo}")
+      end
+
+    end
+  end
+end
