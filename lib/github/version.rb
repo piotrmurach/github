c209b2f761e1c30a691b4680eee880101d446cf8
Piotr Murach <pmurach@gmail.com>
2011-10-07 15:23:19 +0100

    Add build versoning.

--- lib/github/version.rb
+++ lib/github/version.rb
@@ -5,7 +5,8 @@ module Github
     MAJOR = 0
     MINOR = 1
     PATCH = 0
+    BUILD = 'pre'
 
-    STRING = [MAJOR, MINOR, PATCH].compact.join('.');
+    STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.');
   end
 end
