b05443e02cd98d55b2a658a241aa8fc5e40a841d
Piotr Murach <pmurach@gmail.com>
2011-10-22 12:26:53 +0100

    Add CI.

--- spec/github_spec.rb
+++ spec/github_spec.rb
@@ -2,6 +2,11 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe Github do
 
+  before do
+    Github.user = nil
+    Github.repo = nil
+  end
+
   it "should respond to 'new' message" do
     Github.should respond_to :new
   end
