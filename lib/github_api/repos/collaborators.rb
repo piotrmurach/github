69853ae377c514f92d994e751a526c3c7a7299e2
Piotr Murach <pmurach@gmail.com>
2011-10-25 22:31:07 +0100

    Add specs for repos collaborators api, refactored code, added validations.

--- lib/github_api/repos/collaborators.rb
+++ lib/github_api/repos/collaborators.rb
@@ -3,56 +3,78 @@
 module Github
   class Repos
     module Collaborators
-      
+
       # Add collaborator
       #
       # Examples:
       #  @github = Github.new
-      #  @github.collaborators.add_collaborator('user', 'repo', 'collaborator') 
+      #  @github.collaborators.add_collaborator 'user', 'repo', 'collaborator'
       #
       #  @repos = Github::Repos.new
-      #  @repos.add_collaborator('user', 'repo', 'collaborator')
+      #  @repos.add_collaborator 'user', 'repo', 'collaborator'
       #
-      def add_collaborator(user, repo, collaborator)
-        put("/repos/#{user}/#{repo}/collaborators/#{collaborator}")
+      def add_collaborator(user_name, repo_name, collaborator, params={})
+        _update_user_repo_params(user_name, repo_name)
+        _validate_user_repo_params(user, repo) unless user? && repo?
+        _validate_presence_of collaborator
+        _normalize_params_keys(params)
+
+        put("/repos/#{user}/#{repo}/collaborators/#{collaborator}", params)
       end
-      
-      
+      alias :add_collab :add_collaborator
+
       # Checks if user is a collaborator for a given repository
       #
       # Examples:
       #  @github = Github.new
       #  @github.collaborators.collaborator?('user', 'repo', 'collaborator')
       #
-      def collaborator?(user_name, repo_name, collaborator)
-        get("/repos/#{user}/#{repo}/collaborators/#{collaborator}")
+      def collaborator?(user_name, repo_name, collaborator, params={})
+        _update_user_repo_params(user_name, repo_name)
+        _validate_user_repo_params(user, repo) unless user? && repo?
+        _validate_presence_of collaborator
+        _normalize_params_keys(params)
+
+        get("/repos/#{user}/#{repo}/collaborators/#{collaborator}", params)
+        true
+      rescue Github::ResourceNotFound
+        false
       end
 
       # List collaborators
       #
       # Examples:
       #   @github = Github.new
-      #   @github.repos.collaborators('user-name', 'repo-name')
-      #   @github.repos.collaborators('user-name', 'repo-name') { |cbr| .. }
+      #   @github.repos.collaborators 'user-name', 'repo-name'
+      #   @github.repos.collaborators 'user-name', 'repo-name' { |cbr| .. }
       #
-      def collaborators(user_name=nil, repo_name=nil)
-        _update_user_repo_params(user_name, repo_name)      
+      def collaborators(user_name=nil, repo_name=nil, params={})
+        _update_user_repo_params(user_name, repo_name)
         _validate_user_repo_params(user, repo) unless (user? && repo?)
-        
-        response = get("/repos/#{user}/#{repo}/collaborators")
+        _normalize_params_keys(params)
+
+        response = get("/repos/#{user}/#{repo}/collaborators", params)
         return response unless block_given?
         response.each { |el| yield el }
       end
-      
+      alias :list_collaborators :collaborators
+      alias :collabs :collaborators
+
       # Removes collaborator
       #
       # Examples:
       #  @github = Github.new
-      #  @github.collaborators.remove('user', 'repo', 'collaborator')
+      #  @github.repos.remove_collaborator 'user', 'repo', 'collaborator'
       #
-      def remove_collabolator(user, repo, collaborator) 
-        delete("/repos/#{user}/#{repo}/collaborators/#{user}")
+      def remove_collaborator(user_name, repo_name, collaborator, params={})
+        _update_user_repo_params(user_name, repo_name)
+        _validate_user_repo_params(user, repo) unless user? && repo?
+        _validate_presence_of collaborator
+        _normalize_params_keys(params)
+
+        delete("/repos/#{user}/#{repo}/collaborators/#{collaborator}", params)
       end
+      alias :remove_collab :remove_collaborator
 
     end # Collaborators
   end # Repos
--- spec/github/repos/collaborators_spec.rb
+++ spec/github/repos/collaborators_spec.rb
@@ -1,6 +1,169 @@
 require 'spec_helper'
 
 describe Github::Repos::Collaborators do
-  
-  pending
-end
+
+  let(:github) { Github.new }
+  let(:user)   { 'peter-murach' }
+  let(:repo)   { 'github' }
+  let(:collaborator) { 'octocat' }
+
+  describe "collaborators" do
+    context "resource found" do
+      before do
+        stub_get("/repos/#{user}/#{repo}/collaborators").
+          to_return(:body => fixture('repos/collaborators.json'), :status => 200, :headers => {:content_type => "application/json; charset=utf-8"})
+      end
+
+      it "should fail to get resource without username" do
+        github.user, github.repo = nil, nil
+        expect { github.repos.collaborators }.to raise_error(ArgumentError)
+      end
+
+      it "should get the resources" do
+        github.repos.collaborators user, repo
+        a_get("/repos/#{user}/#{repo}/collaborators").should have_been_made
+      end
+
+      it "should return array of resources" do
+        collaborators = github.repos.collaborators user, repo
+        collaborators.should be_an Array
+        collaborators.should have(1).items
+      end
+
+      it "should be a mash type" do
+        collaborators = github.repos.collaborators user, repo
+        collaborators.first.should be_a Hashie::Mash
+      end
+
+      it "should get collaborator information" do
+        collaborators = github.repos.collaborators user, repo
+        collaborators.first.login.should == 'octocat'
+      end
+
+      it "should yield to a block" do
+        github.repos.should_receive(:collaborators).with(user, repo).and_yield('web')
+        github.repos.collaborators(user, repo) { |param| 'web' }
+      end
+    end
+
+    context "resource not found" do
+      before do
+        stub_get("/repos/#{user}/#{repo}/collaborators").
+          to_return(:body => "", :status => [404, "Not Found"], :headers => {:content_type => "application/json; charset=utf-8"})
+      end
+
+      it "should return 404 with a message 'Not Found'" do
+        expect {
+          github.repos.collaborators user, repo
+        }.to raise_error(Github::ResourceNotFound)
+      end
+    end
+  end # collaborators
+
+
+  describe "collaborator?" do
+    context "resource found " do
+      before do
+        stub_get("/repos/#{user}/#{repo}/collaborators/#{collaborator}").
+          to_return(:body => '', :status => 204, :headers => {:content_type => "application/json; charset=utf-8"})
+      end
+
+      it "should fail to get resource without collaborator name" do
+        expect {
+          github.repos.collaborator?(user, repo, nil)
+        }.to raise_error(ArgumentError)
+      end
+
+      it "should get the resource" do
+        github.repos.collaborator? user, repo, collaborator
+        a_get("/repos/#{user}/#{repo}/collaborators/#{collaborator}").should have_been_made
+      end
+
+      it "should find collaborator" do
+        github.repos.should_receive(:collaborator?).with(user, repo, collaborator).and_return true
+        github.repos.collaborator? user, repo, collaborator
+      end
+    end
+
+    context "resource not found" do
+      before do
+        stub_get("/repos/#{user}/#{repo}/collaborators/#{collaborator}").
+          to_return(:body => '', :status => 404, :headers => {:content_type => "application/json; charset=utf-8"})
+      end
+
+      it "should fail to retrieve resource" do
+        github.repos.should_receive(:collaborator?).with(user, repo, collaborator).and_return false
+        github.repos.collaborator? user, repo, collaborator
+      end
+    end
+  end # collaborator?
+
+  describe "add_collaborator" do
+
+    context "resouce added" do
+      before do
+        stub_put("/repos/#{user}/#{repo}/collaborators/#{collaborator}").
+          to_return(:body => '', :status => 204, :headers => {:content_type => "application/json; charset=utf-8"})
+      end
+
+      it "should fail to add resource if 'collaborator' input is missing" do
+        expect {
+          github.repos.add_collaborator user, repo, nil
+        }.to raise_error(ArgumentError)
+      end
+
+      it "should add resource successfully" do
+        github.repos.add_collaborator user, repo, collaborator
+        a_put("/repos/#{user}/#{repo}/collaborators/#{collaborator}").should have_been_made
+      end
+    end
+
+    context "failed to add resource" do
+      before do
+        stub_put("/repos/#{user}/#{repo}/collaborators/#{collaborator}").
+          to_return(:body => '', :status => 404, :headers => {:content_type => "application/json; charset=utf-8"})
+      end
+
+      it "should fail to add resource" do
+        expect {
+          github.repos.add_collaborator user, repo, collaborator
+        }.to raise_error(Github::ResourceNotFound)
+      end
+    end
+  end # add_collaborator
+
+  describe "remove_collaborator" do
+
+    context "resouce added" do
+      before do
+        stub_delete("/repos/#{user}/#{repo}/collaborators/#{collaborator}").
+          to_return(:body => '', :status => 204, :headers => {:content_type => "application/json; charset=utf-8"})
+      end
+
+      it "should fail to add resource if 'collaborator' input is missing" do
+        expect {
+          github.repos.remove_collaborator user, repo, nil
+        }.to raise_error(ArgumentError)
+      end
+
+      it "should add resource successfully" do
+        github.repos.remove_collaborator user, repo, collaborator
+        a_delete("/repos/#{user}/#{repo}/collaborators/#{collaborator}").should have_been_made
+      end
+    end
+
+    context "failed to remove resource" do
+      before do
+        stub_delete("/repos/#{user}/#{repo}/collaborators/#{collaborator}").
+          to_return(:body => '', :status => 404, :headers => {:content_type => "application/json; charset=utf-8"})
+      end
+
+      it "should fail to remove resource" do
+        expect {
+          github.repos.remove_collaborator user, repo, collaborator
+        }.to raise_error(Github::ResourceNotFound)
+      end
+    end
+  end # remove_collaborator
+
+end # Github::Repos::Collaborators
