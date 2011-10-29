7efce7dbeb0e6243e4b5f2a8a8835f7881968ffc
Piotr Murach <pmurach@gmail.com>
2011-10-29 20:39:31 +0100

    Rewrite repos forks api to utilse new methods and improve docs.

--- lib/github_api/repos/forks.rb
+++ lib/github_api/repos/forks.rb
@@ -3,27 +3,46 @@
 module Github
   class Repos
     module Forks
-      
-      # List forks 
+
+      # List repository forks
       #
-      # GET /repos/:user/:repo/forks
+      # = Examples
+      #  @github = Github.new
+      #  @github.repos.forks 'user-name', 'repo-name'
+      #  @github.repos.forks 'user-name', 'repo-name' { |hook| ... }
       #
-      # Examples
-      #
-      def forks(user, repo)
-        get("/repos/#{user}/#{repo}/forks")
+      def forks(user_name=nil, repo_name=nil, params = {})
+        _update_user_repo_params(user_name, repo_name)
+        _validate_user_repo_params(user, repo) unless user? && repo?
+        _normalize_params_keys(params)
+
+        response = get("/repos/#{user}/#{repo}/forks")
+        return response unless block_given?
+        response.each { |el| yield el }
       end
-      
+      alias :repo_forks :forks
+      alias :repository_forks :forks
+
       # Create a fork for the authenticated user
-      # 
-      # POST /repos/:user/:repo/forks
       #
-      def create_fork(user, repo, params={})
+      # = Inputs
+      # * <tt>:org</tt> - Optional string - the name of the service that is being called.
+      #
+      # = Examples
+      #  @github = Github.new
+      #  @github.repos.create_fork 'user-name', 'repo-name',
+      #    "org" =>  "github"
+      #
+      def create_fork(user_name=nil, repo_name=nil, params={})
+        _update_user_repo_params(user_name, repo_name)
+        _validate_user_repo_params(user, repo) unless user? && repo?
+
         _normalize_params_keys(params)
         _filter_params_keys(%w[ org ], params)
 
         post("/repos/#{user}/#{repo}/forks", params)
       end
-    end
-  end
+
+    end # Forks
+  end # Repos
 end # Github
