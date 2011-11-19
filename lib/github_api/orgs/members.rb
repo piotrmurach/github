6886ea0ec5e0af243479bf8367ee212016c35ad9
Piotr Murach <pmurach@gmail.com>
2011-11-19 20:13:45 +0000

    Update method signatures in orgs members api.

--- lib/github_api/orgs/members.rb
+++ lib/github_api/orgs/members.rb
@@ -6,15 +6,16 @@ module Github
 
       # List members
       #
-      # List all users who are members of an organization. A member is a user 
-      # that belongs to at least 1 team in the organization. 
-      # If the authenticated user is also a member of this organization then 
-      # both concealed and public members will be returned. 
+      # List all users who are members of an organization. A member is a user
+      # that belongs to at least 1 team in the organization.
+      # If the authenticated user is also a member of this organization then
+      # both concealed and public members will be returned.
       # Otherwise only public members are returned.
       #
       # = Examples
       #  @github = Github.new
       #  @github.orgs.members 'org-name'
+      #  @github.orgs.members 'org-name' { |memb| ... }
       #
       def members(org_name, params={})
         _validate_presence_of org_name
@@ -23,6 +24,7 @@ module Github
         return response unless block_given?
         response.each { |el| yield el }
       end
+      alias :list_members :members
 
       # Check if user is a member of an organization
       #
@@ -38,6 +40,7 @@ module Github
       rescue Github::ResourceNotFound
         false
       end
+      alias :is_member? :member?
 
       # Remove a member
       # Removing a user from this list will remove them from all teams and
@@ -52,12 +55,14 @@ module Github
         _normalize_params_keys(params)
         delete("/orgs/#{org_name}/members/#{member_name}", params)
       end
+      alias :remove_member :delete_member
 
       # List public members
       # Members of an organization can choose to have their membership publicized or not.
       # = Examples
       #  @github = Github.new
       #  @github.orgs.public_members 'org-name'
+      #  @github.orgs.public_members 'org-name' { |memb| ... }
       #
       def public_members(org_name, params={})
         _validate_presence_of org_name
@@ -66,6 +71,8 @@ module Github
         return response unless block_given?
         response.each { |el| yield el }
       end
+      alias :list_public_members :public_members
+
       # Get if a user is a public member of an organization
       #
       # = Examples
@@ -92,6 +99,8 @@ module Github
         _normalize_params_keys(params)
         put("/orgs/#{org_name}/public_members/#{member_name}", params)
       end
+      alias :make_public :publicize
+      alias :publicize_membership :publicize
 
       # Conceal a user’s membership
       #
@@ -104,6 +113,7 @@ module Github
         _normalize_params_keys(params)
         delete("/orgs/#{org_name}/public_members/#{member_name}", params)
       end
+      alias :conceal_membership :conceal
 
     end # Members
   end # Orgs
