f409a6c5c4275233a611a0beb53895d62cab04a5
Piotr Murach <pmurach@gmail.com>
2011-11-19 16:06:54 +0000

    Add json parsing.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
 require 'rspec'
+require 'json'
 require 'webmock/rspec'
 require 'github_api'
 
