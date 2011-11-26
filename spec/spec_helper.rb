e612da04890844fc319a68529e0a1947e57f5307
Piotr Murach <pmurach@gmail.com>
2011-11-26 16:57:50 +0000

    Add helper method for authentication params resetting.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -66,6 +66,12 @@ end
 
 OAUTH_TOKEN = 'bafec72922f31fe86aacc8aca4261117f3bd62cf'
 
+def reset_authentication_for(object)
+  ['basic_auth', 'oauth_token', 'login', 'password' ].each do |item|
+    object.send("#{item}=", nil)
+  end
+end
+
 class Hash
   def except(*keys)
     cpy = self.dup
