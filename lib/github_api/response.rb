2f65bff7bb06ac9b0c6eb45e8cc7ed9e060345a8
Piotr Murach <pmurach@gmail.com>
2011-12-03 11:47:30 +0000

    Generalize response processing.

--- lib/github_api/response.rb
+++ lib/github_api/response.rb
@@ -6,5 +6,18 @@ module Github
   # Contains methods and attributes that act on the response returned from the 
   # request
   class Response < Faraday::Response::Middleware
+
+    class << self
+      attr_accessor :parser
+    end
+
+    def self.define_parser(&block)
+      @parser = block
+    end
+
+    def response_type(env)
+      env[:response_headers][CONTENT_TYPE].to_s
+    end
+
   end # Response
-end
+end # Github
