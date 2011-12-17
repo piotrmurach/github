2ff08714dda5413f202ee11bbaa2e469a34ff0cb
Piotr Murach <pmurach@gmail.com>
2011-12-17 14:30:00 +0000

    Move coverage setup to its own file.

---
+++ spec/coverage_adapter.rb
@@ -0,0 +1,14 @@
+require 'simplecov'
+
+SimpleCov.adapters.define 'github_api' do
+  add_filter "/spec/"
+  add_filter "/features/"
+
+  add_group 'Repos',   'lib/github_api/repos'
+  add_group 'Orgs',    'lib/github_api/orgs'
+  add_group 'Users',   'lib/github_api/users'
+  add_group 'Issues',  'lib/github_api/issues'
+  add_group 'Gists',   'lib/github_api/gists'
+  add_group 'Git Data','lib/github_api/git_data'
+  add_group 'Pull Requests','lib/github_api/pull_requests'
+end # SimpleCov
