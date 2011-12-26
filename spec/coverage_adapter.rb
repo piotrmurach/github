ae38ec66468ef6609d54a04746c5e16348cfea34
Piotr Murach <pmurach@gmail.com>
2011-12-26 12:35:20 +0000

    Add events category for specs grouping.

--- spec/coverage_adapter.rb
+++ spec/coverage_adapter.rb
@@ -8,6 +8,7 @@ SimpleCov.adapters.define 'github_api' do
   add_group 'Orgs',    'lib/github_api/orgs'
   add_group 'Users',   'lib/github_api/users'
   add_group 'Issues',  'lib/github_api/issues'
+  add_group 'Events',  'lib/github_api/events'
   add_group 'Gists',   'lib/github_api/gists'
   add_group 'Git Data','lib/github_api/git_data'
   add_group 'Pull Requests','lib/github_api/pull_requests'
