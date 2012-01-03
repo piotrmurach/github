2083e8c3972e4253b0524650be63646ea03d18ac
Piotr Murach <pmurach@gmail.com>
2012-01-03 16:34:57 +0000

    Add steps for easy api traversal.

--- features/step_definitions/github_api_steps.rb
+++ features/step_definitions/github_api_steps.rb
@@ -2,10 +2,33 @@ Given /^I have github instance$/ do
   @github = Github.new
 end
 
+Given /^I have "([^"]*)" instance$/ do |api_classes|
+  constant = Object
+  api_classes.split('::').each do |api_class|
+    constant = constant.const_get api_class
+  end
+  @instance = constant.new
+end
+
 When /^I fetch "([^"]*)"$/ do |method|
   @response = @github.send(method.to_sym)
 end
 
 When /^I will have access to "([^"]*)" API$/ do |api|
   @response.class.to_s.should match api
+end
+
+When /^I am looking for "([^"]*)" with the following params:$/ do |method, table|
+  table.hashes.each do |attributes|
+    @method = method.to_sym
+    @attributes = attributes
+  end
+end
+
+When /^I make request$/ do
+  @response = @instance.send @method, *@attributes.values
+end
+
+When /^I make request with hash params$/ do
+  @response = @instance.send @method, @attributes
 end
