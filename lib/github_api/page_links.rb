f3fb165074338ee48bccd72a0c314d42d1fa677e
Piotr Murach <pmurach@gmail.com>
2012-01-08 19:42:29 +0000

    Add header links parsing class.

---
+++ lib/github_api/page_links.rb
@@ -0,0 +1,45 @@
+module Github
+  # Determines the links in the current response link header to be used
+  # to find the links to other pages of request responses. These will
+  # only be present if the result set size exceeds the per page limit.
+  class PageLinks
+    include Github::Constants
+
+    DELIM_LINKS = ",".freeze # :nodoc:
+
+    # Hold the extracted values for URI from the Link header
+    # for the first, last, next and previous page.
+    attr_accessor :first, :last, :next, :prev
+
+    # Parses links from executed request
+    #
+    def initialize(response_headers)
+      link_header = response_headers[HEADER_LINK]
+      if link_header
+        # return unless link_header =~ /(next|first|last|prev)/
+
+        link_header.split(DELIM_LINKS).each do |link|
+          if link.strip =~ /<([^>]+)>; rel=\"([^\"]+)\"/
+            url_part, meta_part = $1, $2
+            next if !url_part || !meta_part
+            case meta_part
+            when META_FIRST
+              self.first = url_part
+            when META_LAST
+              self.last = url_part
+            when META_NEXT
+              self.next = url_part
+            when META_PREV
+              self.prev = url_part
+            end
+          end
+        end
+      else
+        # When on the first page
+        self.next = response_headers[HEADER_NEXT]
+        self.last = response_headers[HEADER_LAST]
+      end
+    end
+
+  end # PageLinks
+end # Github
---
+++ spec/github/page_links_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe Github::PageLinks do
+
+  let(:link) {
+    "<https://api.github.com/users/wycats/repos?page=4&per_page=20>; rel=\"next\", <https://api.github.com/users/wycats/repos?page=6&per_page=20>; rel=\"last\", <https://api.github.com/users/wycats/repos?page=1&per_page=20>; rel=\"first\", <https://api.github.com/users/wycats/repos?page=2&per_page=20>; rel=\"prev\""
+  }
+  let(:response_headers)  { { 'Link' => link } }
+
+  let(:first) { "https://api.github.com/users/wycats/repos?page=1&per_page=20" }
+  let(:last)  { "https://api.github.com/users/wycats/repos?page=6&per_page=20" }
+  let(:prev)  { "https://api.github.com/users/wycats/repos?page=2&per_page=20" }
+  let(:nexxt)  { "https://api.github.com/users/wycats/repos?page=4&per_page=20" }
+
+  context 'build page links instance' do
+
+    it 'parses first link successfully' do
+      Github::PageLinks.new(response_headers).first.should eql first
+    end
+
+    it 'parses last link successfully' do
+      Github::PageLinks.new(response_headers).last.should eql last
+    end
+
+    it 'parses next link successfully' do
+      Github::PageLinks.new(response_headers).next.should eql nexxt
+    end
+
+    it 'parses prev link successfully' do
+      Github::PageLinks.new(response_headers).prev.should eql prev
+    end
+  end
+
+end # Github::PageLinks
