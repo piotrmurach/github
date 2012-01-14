2bd0524ea69336c8b5a01b2db625953f9b86e968
Piotr Murach <pmurach@gmail.com>
2012-01-14 15:13:21 +0000

    Bump gem version to 0.4.0

--- lib/github_api/version.rb
+++ lib/github_api/version.rb
@@ -3,8 +3,8 @@
 module Github
   module VERSION
     MAJOR = 0
-    MINOR = 3
-    PATCH = 9
+    MINOR = 4
+    PATCH = 0
     BUILD = nil
 
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.');
