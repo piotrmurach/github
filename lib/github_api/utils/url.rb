e8d4b599f0953771c2eb72910e0c0ddae8acedbb
Piotr Murach <pmurach@gmail.com>
2012-01-14 11:32:04 +0000

    Add url parsing utilities.

---
+++ lib/github_api/utils/url.rb
@@ -0,0 +1,46 @@
+require 'cgi'
+
+module Github
+  module Utils
+    module Url
+      extend self
+
+      DEFAULT_QUERY_SEP = /[&;] */n
+
+      KEY_VALUE_SEP = '='.freeze
+
+      def escape(s) CGI.escape s.to_s end
+
+      def unescape(s) CGI.unescape s.to_s end
+
+      def parse_query(query_string)
+        return '' if query_string.nil? || query_string.empty?
+        params = {}
+
+        query_string.split(DEFAULT_QUERY_SEP).each do |part|
+          k, v = part.split(KEY_VALUE_SEP, 2).map { |el| unescape(el) }
+
+          if cur = params[k]
+            if cur.class == Array
+              params[k] << v
+            else
+              params[k] = [cur, v]
+            end
+          else
+            params[k] = v
+          end
+        end
+        params
+      end
+
+      def parse_query_for_param(query_string, name)
+        parse_query(query_string).each do |key, val|
+          return val.first if (name == key) && val.is_a?(Array)
+          return val if (name == key)
+        end
+        return nil
+      end
+
+    end # Url
+  end # Utils
+end # Github
---
+++ spec/github/utils/url_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe Github::Utils::Url do
+
+  it 'escapes correctly' do
+    described_class.escape('<html>').should eql '%3Chtml%3E'
+    described_class.escape('a space').should eql 'a+space'
+    described_class.escape('\\/?}!@#$%^&*()').should eql '%5C%2F%3F%7D%21%40%23%24%25%5E%26%2A%28%29'
+  end
+
+  it 'unescapes correctly' do
+    described_class.unescape('%3Chtml%3E').should eql '<html>'
+    described_class.unescape('a+space').should eql 'a space'
+    described_class.unescape('%5C%2F%3F%7D%21%40%23%24%25%5E%26%2A%28%29').should eql '\\/?}!@#$%^&*()'
+  end
+
+  context 'parses query strings correctly' do
+    it { described_class.parse_query("a=b").should eq 'a' => 'b' }
+    it { described_class.parse_query("a=b&a=c").should eq 'a' => ['b','c'] }
+    it { described_class.parse_query("a=b&c=d").should eq 'a' => 'b', 'c' => 'd' }
+    it { described_class.parse_query("a+b=%28c%29").should eq 'a b' => '(c)' }
+  end
+
+  context 'parse_query_for_param' do
+    it 'returns nil if cannot find parameter' do
+      described_class.parse_query_for_param("param1=a;param2=b", 'param3').
+        should be_nil
+    end
+
+    it 'returns value for given parameter name' do
+      described_class.parse_query_for_param("param1=a;param2=b", 'param1').
+        should eq 'a'
+    end
+  end
+
+end # Github::Utils::Url
