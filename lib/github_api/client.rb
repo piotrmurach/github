3ad15e978a617aed5773b049049c26680ff0dc32
Piotr Murach <pmurach@gmail.com>
2012-02-04 21:08:39 +0000

    Change client to use new api factory.

--- lib/github_api/client.rb
+++ lib/github_api/client.rb
@@ -4,51 +4,51 @@ module Github
   class Client < API
 
     def gists(options = {})
-      @gists ||= _create_instance Github::Gists, options
+      @gists ||= ApiFactory.new 'Gists', options
     end
 
     # The Git Database API gives you access to read and write raw Git objects
     # to your Git database on GitHub and to list and update your references
     # (branch heads and tags).
     def git_data(options = {})
-      @git_data ||= _create_instance Github::GitData, options
+      @git_data ||= ApiFactory.new 'GitData', options
     end
     alias :git :git_data
 
     def issues(options = {})
-      @issues ||= _create_instance Github::Issues, options
+      @issues ||= ApiFactory.new 'Issues', options
     end
 
     def orgs(options = {})
-      @orgs ||= _create_instance Github::Orgs, options
+      @orgs ||= ApiFactory.new 'Orgs', options
     end
     alias :organizations :orgs
 
     def pull_requests(options = {})
-      @pull_requests ||= _create_instance Github::PullRequests, options
+      @pull_requests ||= ApiFactory.new 'PullRequests', options
     end
 
     def repos(options = {})
-      @repos ||= _create_instance Github::Repos, options
+      @repos ||= ApiFactory.new 'Repos', options
     end
     alias :repositories :repos
 
     # Many of the resources on the users API provide a shortcut for getting 
     # information about the currently authenticated user.
     def users(options = {})
-      @users ||= _create_instance Github::Users, options
+      @users ||= ApiFactory.new 'Users', options
     end
 
     # This is a read-only API to the GitHub events.
     # These events power the various activity streams on the site.
     def events(options = {})
-      @events ||= _create_instance Github::Events, options
+      @events ||= ApiFactory.new 'Events', options
     end
 
     # An API for users to manage their own tokens. You can only access your own
     # tokens, and only through Basic Authentication.
     def oauth(options = {})
-      @oauth ||= _create_instance Github::Authorizations, options
+      @oauth ||= ApiFactory.new 'Authorizations', options
     end
 
   end # Client
