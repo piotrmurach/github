3e900aabb5799678b5961c6089777b5e803ab468
Piotr Murach <pmurach@gmail.com>
2012-02-05 14:25:57 +0000

    Change method signatures, updated docs and added aliases.

--- lib/github_api/gists/comments.rb
+++ lib/github_api/gists/comments.rb
@@ -4,7 +4,11 @@ module Github
   class Gists
     module Comments
 
-      REQUIRED_GIST_COMMENT_INPUTS = %w[ 
+      REQUIRED_GIST_COMMENT_INPUTS = %w[
+        body
+      ].freeze
+
+      ALLOWED_GIST_COMMENT_INPUTS = %w[
         body
         mime_type
         resource
@@ -14,72 +18,90 @@ module Github
       #
       # = Examples
       #  @github = Github.new
-      #  @github.gists.gist_comments 'gist-id'
+      #  @github.gists.comments 'gist-id'
       #
-      def gist_comments(gist_id, params={})
+      def comments(gist_id, params={})
         _normalize_params_keys(params)
+        _validate_presence_of(gist_id)
         _merge_mime_type(:gist_comment, params)
 
-        get("/gists/#{gist_id}/comments", params)
+        response = get("/gists/#{gist_id}/comments", params)
+        return response unless block_given?
+        response.each { |el| yield el }
       end
+      alias :list_comments :comments
+      alias :gist_comments :comments
 
       # Get a single comment
       #
       # = Examples
       #  @github = Github.new
-      #  @github.gists.gist_comment 'comment-id'
+      #  @github.gists.comment 'comment-id'
       #
-      def gist_comment(comment_id, params={})
+      def comment(comment_id, params={})
         _normalize_params_keys(params)
+        _validate_presence_of(comment_id)
         _merge_mime_type(:gist_comment, params)
 
         get("/gists/comments/#{comment_id}", params)
       end
+      alias :gist_comment :comment
+      alias :get_comment  :comment
 
       # Create a comment
       #
       # = Examples
       #  @github = Github.new
-      #  @github.gists.create_gist_comment 'gist-id'
+      #  @github.gists.create_comment 'gist-id'
       #
-      def create_gist_comment(gist_id, params={})
+      def create_comment(gist_id, params={})
         _normalize_params_keys(params)
         _merge_mime_type(:gist_comment, params)
-        _filter_params_keys(REQUIRED_GIST_COMMENT_INPUTS, params)
+        _filter_params_keys(ALLOWED_GIST_COMMENT_INPUTS, params)
 
-        raise ArgumentError, "Required inputs are: :body" unless _validate_inputs(REQUIRED_GIST_COMMENT_INPUTS, params)
+        unless _validate_inputs(REQUIRED_GIST_COMMENT_INPUTS, params)
+          puts params
+          raise ArgumentError, "Required inputs are: :body"
+        end
 
         post("/gists/#{gist_id}/comments", params)
       end
+      alias :create_gist_comment :create_comment
 
       # Edit a comment
       #
       # = Examples
       #  @github = Github.new
-      #  @github.gists.edit_gist_comment 'comment-id'
+      #  @github.gists.edit_comment 'comment-id'
       #
-      def edit_gist_comment(comment_id, params={})
+      def edit_comment(comment_id, params={})
         _normalize_params_keys(params)
+        _validate_presence_of(comment_id)
         _merge_mime_type(:gist_comment, params)
-        _filter_params_keys(REQUIRED_GIST_COMMENT_INPUTS, params)
+        _filter_params_keys(ALLOWED_GIST_COMMENT_INPUTS, params)
 
-        raise ArgumentError, "Required inputs are: :body" unless _validate_inputs(REQUIRED_GIST_COMMENT_INPUTS, params)
+        unless _validate_inputs(REQUIRED_GIST_COMMENT_INPUTS, params)
+          raise ArgumentError, "Required inputs are: :body"
+        end
 
         patch("/gists/comments/#{comment_id}", params)
       end
+      alias :edit_gist_comment :edit_comment
 
       # Delete a comment
       #
       # = Examples
       #  @github = Github.new
-      #  @github.gists.delete_gist_comment 'comment-id'
+      #  @github.gists.delete_comment 'comment-id'
       #
-      def delete_gist_comment(comment_id, params={})
+      def delete_comment(comment_id, params={})
         _normalize_params_keys(params)
+        _validate_presence_of(comment_id)
         _merge_mime_type(:gist_comment, params)
 
         delete("/gists/comments/#{comment_id}", params)
       end
+      alias :delete_gist_comment :delete_comment
 
     end # Comments
   end # Gists
