b4b94bc7745a24e9b0029a908105cf49b1c5b9be
Piotr Murach <pmurach@gmail.com>
2012-02-25 23:34:56 +0000

    Change response to use new error github service types.

--- lib/github_api/response/raise_error.rb
+++ lib/github_api/response/raise_error.rb
@@ -9,27 +9,22 @@ module Github
     def on_complete(env)
       case env[:status].to_i
       when 400
-        raise Github::BadRequest.new(response_message(env), env[:response_headers])
+        raise Github::Error::BadRequest.new(env)
       when 401
-        raise Github::Unauthorised.new(response_message(env), env[:response_headers])
+        raise Github::Error::Unauthorized.new(env)
       when 403
-        raise Github::Forbidden.new(response_message(env), env[:response_headers])
+        raise Github::Error::Forbidden.new(env)
       when 404
-        raise Github::ResourceNotFound.new(response_message(env), env[:response_headers])
-
+        raise Github::Error::NotFound.new(env)
       when 422
-        raise Github::UnprocessableEntity.new(response_message(env), env[:response_headers])
+        raise Github::Error::UnprocessableEntity.new(env)
       when 500
-        raise Github::InternalServerError.new(response_message(env), env[:response_headers])
+        raise Github::Error::InternalServerError.new(env)
       when 503
-        raise Github::ServiceUnavailable.new(response_message(env), env[:response_headers])
+        raise Github::Error::ServiceUnavailable.new(env)
       when 400...600
-        raise Github::Error.new(response_message(env), env[:response_headers])
+        raise Github::Error::ServiceError.new(env)
       end
-    end
-
-    def response_message(env)
-      "#{env[:method].to_s.upcase} #{env[:url].to_s}: #{env[:status]}#{env[:body]}"
     end
 
   end # Response::RaiseError
