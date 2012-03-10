c56aa31a158bc3a617eda0bfa2221da348e5d985
Piotr Murach <pmurach@gmail.com>
2012-03-10 23:19:19 +0000

    Change to new methods.

--- features/support/vcr.rb
+++ features/support/vcr.rb
@@ -1,7 +1,7 @@
 require 'vcr'
 
-VCR.config do |conf|
-  conf.stub_with :webmock
+VCR.configure do |conf|
+  conf.hook_into :webmock
   conf.cassette_library_dir = 'features/cassettes'
   conf.default_cassette_options = { :record => :new_episodes }
   conf.filter_sensitive_data('<***>') { ''}
