19130f5e356fa07ee3eed12a47ba85b9df933234
Piotr Murach <pmurach@gmail.com>
2012-03-11 12:52:27 +0000

    Add test randomization.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -28,7 +28,7 @@ end
 
 RSpec.configure do |config|
   config.include WebMock::API
-  # config.order = :rand
+  config.order = :rand
   config.color_enabled = true
   config.treat_symbols_as_metadata_keys_with_true_values = true
 
