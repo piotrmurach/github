ac93a4928b3febf60ba5a04122b380c075a328c7
Piotr Murach <pmurach@gmail.com>
2012-03-18 13:50:48 +0000

    Change git data trees api calls.

--- spec/github/git_data/trees_spec.rb
+++ spec/github/git_data/trees_spec.rb
@@ -12,9 +12,8 @@ describe Github::GitData::Trees do
 
   it { described_class::VALID_TREE_PARAM_NAMES.should_not be_nil }
 
-  describe "tree" do
-    it { github.git_data.should respond_to :tree }
-    it { github.git_data.should respond_to :get_tree }
+  describe "#get" do
+    it { github.git_data.trees.should respond_to :find }
 
     context "non-resursive" do
       before do
@@ -23,21 +22,23 @@ describe Github::GitData::Trees do
       end
 
       it "should fail to get resource without sha" do
-        expect { github.git_data.tree(user, repo, nil)}.to raise_error(ArgumentError)
+        expect {
+          github.git_data.trees.get user, repo, nil
+        }.to raise_error(ArgumentError)
       end
 
       it "should get the resource" do
-        github.git_data.tree user, repo, sha
+        github.git_data.trees.get user, repo, sha
         a_get("/repos/#{user}/#{repo}/git/trees/#{sha}").should have_been_made
       end
 
       it "should get tree information" do
-        tree = github.git_data.tree user, repo, sha
+        tree = github.git_data.trees.get user, repo, sha
         tree.sha.should eql sha
       end
 
       it "should return mash" do
-        tree = github.git_data.tree user, repo, sha
+        tree = github.git_data.trees.get user, repo, sha
         tree.should be_a Hashie::Mash
       end
     end
@@ -49,17 +50,17 @@ describe Github::GitData::Trees do
       end
 
       it "should get the resource" do
-        github.git_data.tree user, repo, sha, 'recursive' => true
+        github.git_data.trees.get user, repo, sha, 'recursive' => true
         a_get("/repos/#{user}/#{repo}/git/trees/#{sha}?recursive=1").should have_been_made
       end
 
       it "should get tree information" do
-        tree = github.git_data.tree user, repo, sha, 'recursive' => true
+        tree = github.git_data.trees.get user, repo, sha, 'recursive' => true
         tree.sha.should eql sha
       end
 
       it "should return mash" do
-        tree = github.git_data.tree user, repo, sha, 'recursive' => true
+        tree = github.git_data.trees.get user, repo, sha, 'recursive' => true
         tree.should be_a Hashie::Mash
       end
     end
@@ -72,13 +73,13 @@ describe Github::GitData::Trees do
 
       it "should fail to retrive resource" do
         expect {
-          github.git_data.tree user, repo, sha
+          github.git_data.trees.get user, repo, sha
         }.to raise_error(Github::Error::NotFound)
       end
     end
-  end # tree
+  end # get
 
-  describe "create_tree" do
+  describe "#create" do
     let(:inputs) {
       {
         "tree" => [
@@ -101,22 +102,22 @@ describe Github::GitData::Trees do
 
       it "should fail to create resource if 'content' input is missing" do
         expect {
-          github.git_data.create_tree user, repo, inputs.except('tree')
+          github.git_data.trees.create user, repo, inputs.except('tree')
         }.to raise_error(Github::Error::RequiredParams)
       end
 
       it "should create resource successfully" do
-        github.git_data.create_tree user, repo, inputs
+        github.git_data.trees.create user, repo, inputs
         a_post("/repos/#{user}/#{repo}/git/trees").with(inputs).should have_been_made
       end
 
       it "should return the resource" do
-        tree_sha = github.git_data.create_tree user, repo, inputs
+        tree_sha = github.git_data.trees.create user, repo, inputs
         tree_sha.should be_a Hashie::Mash
       end
 
       it "should get the tree information" do
-        tree_sha = github.git_data.create_tree user, repo, inputs
+        tree_sha = github.git_data.trees.create user, repo, inputs
         tree_sha.sha.should == sha
       end
     end
@@ -130,10 +131,10 @@ describe Github::GitData::Trees do
 
       it "should faile to retrieve resource" do
         expect {
-          github.git_data.create_tree user, repo, inputs
+          github.git_data.trees.create user, repo, inputs
         }.to raise_error(Github::Error::NotFound)
       end
     end
-  end # create_tree
+  end # create
 
 end # Github::GitData::Trees
