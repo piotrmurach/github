9b35d8f12495ba061eab5b70d7e1cdb414399050
Piotr Murach <pmurach@gmail.com>
2012-03-18 13:19:48 +0000

    Change git data blobs api calls and update specs.

--- lib/github_api/git_data/blobs.rb
+++ lib/github_api/git_data/blobs.rb
@@ -1,52 +1,55 @@
 # encoding: utf-8
 
 module Github
-  class GitData
+  class GitData::Blobs < API
     # Since blobs can be any arbitrary binary data, the input and responses for
     # the blob api takes an encoding parameter that can be either utf-8 or base64.
     # If your data cannot be losslessly sent as a UTF-8 string, you can base64 encode it.
-    module Blobs
 
-      VALID_BLOB_PARAM_NAMES = %w[ content encoding ].freeze
+    VALID_BLOB_PARAM_NAMES = %w[ content encoding ].freeze
 
-      # Get a blob
-      #
-      # = Examples
-      #  @github = Github.new
-      #  @github.git_data.blob 'user-name', 'repo-name', 'sha'
-      #
-      def blob(user_name, repo_name, sha, params={})
-        _update_user_repo_params(user_name, repo_name)
-        _validate_user_repo_params(user, repo) unless user? && repo?
-        _validate_presence_of sha
-        _normalize_params_keys params
+    # Creates new GitData::Blobs API
+    def initialize(options = {})
+      super(options)
+    end
 
-        get("/repos/#{user}/#{repo}/git/blobs/#{sha}", params)
-      end
-      alias :get_blob :blob
+    # Get a blob
+    #
+    # = Examples
+    #  github = Github.new
+    #  github.git_data.blobs.get 'user-name', 'repo-name', 'sha'
+    #
+    def get(user_name, repo_name, sha, params={})
+      _update_user_repo_params(user_name, repo_name)
+      _validate_user_repo_params(user, repo) unless user? && repo?
+      _validate_presence_of sha
+      _normalize_params_keys params
 
-      # Create a blob
-      #
-      # = Inputs
-      # * <tt>:content</tt> - String of content
-      # * <tt>:encoding</tt> - String containing encoding<tt>utf-8</tt> or <tt>base64</tt>
-      # = Examples
-      #  @github = Github.new
-      #  @github.git_data.create_blob 'user-name', 'repo-name',
-      #    "content" => "Content of the blob",
-      #    "encoding" => "utf-8"
-      #
-      def create_blob(user_name, repo_name, params={})
-        _update_user_repo_params(user_name, repo_name)
-        _validate_user_repo_params(user, repo) unless user? && repo?
+      get_request("/repos/#{user}/#{repo}/git/blobs/#{sha}", params)
+    end
+    alias :find :get
 
-        _normalize_params_keys(params)
-        _filter_params_keys(VALID_BLOB_PARAM_NAMES, params)
-        _validate_inputs(VALID_BLOB_PARAM_NAMES, params)
+    # Create a blob
+    #
+    # = Inputs
+    # * <tt>:content</tt> - String of content
+    # * <tt>:encoding</tt> - String containing encoding<tt>utf-8</tt> or <tt>base64</tt>
+    # = Examples
+    #  github = Github.new
+    #  github.git_data.blobs.create 'user-name', 'repo-name',
+    #    "content" => "Content of the blob",
+    #    "encoding" => "utf-8"
+    #
+    def create(user_name, repo_name, params={})
+      _update_user_repo_params(user_name, repo_name)
+      _validate_user_repo_params(user, repo) unless user? && repo?
 
-        post("/repos/#{user}/#{repo}/git/blobs", params)
-      end
+      _normalize_params_keys(params)
+      _filter_params_keys(VALID_BLOB_PARAM_NAMES, params)
+      _validate_inputs(VALID_BLOB_PARAM_NAMES, params)
 
-    end # Blobs
-  end # GitData
+      post("/repos/#{user}/#{repo}/git/blobs", params)
+    end
+
+  end # GitData::Blobs
 end # Github
--- spec/github/git_data/blobs_spec.rb
+++ spec/github/git_data/blobs_spec.rb
@@ -12,32 +12,35 @@ describe Github::GitData::Blobs do
 
   it { described_class::VALID_BLOB_PARAM_NAMES.should_not be_nil }
 
-  describe "blob" do
-    it { github.git_data.should respond_to :blob }
-    it { github.git_data.should respond_to :get_blob }
+  describe "#get" do
+    it { github.git_data.blobs.should respond_to :find }
 
     context "resource found" do
       before do
         stub_get("/repos/#{user}/#{repo}/git/blobs/#{sha}").
-          to_return(:body => fixture('git_data/blob.json'), :status => 200, :headers => {:content_type => "application/json; charset=utf-8"})
+          to_return(:body => fixture('git_data/blob.json'),
+            :status => 200,
+            :headers => {:content_type => "application/json; charset=utf-8"})
       end
 
       it "should fail to get resource without sha" do
-        expect { github.git_data.blob(user, repo, nil)}.to raise_error(ArgumentError)
+        expect {
+          github.git_data.blobs.get user, repo, nil
+        }.to raise_error(ArgumentError)
       end
 
       it "should get the resource" do
-        github.git_data.blob user, repo, sha
+        github.git_data.blobs.get user, repo, sha
         a_get("/repos/#{user}/#{repo}/git/blobs/#{sha}").should have_been_made
       end
 
       it "should get blob information" do
-        blob = github.git_data.blob user, repo, sha
+        blob = github.git_data.blobs.get user, repo, sha
         blob.content.should eql "Content of the blob"
       end
 
       it "should return mash" do
-        blob = github.git_data.blob user, repo, sha
+        blob = github.git_data.blobs.get user, repo, sha
         blob.should be_a Hashie::Mash
       end
     end
@@ -45,18 +48,20 @@ describe Github::GitData::Blobs do
     context "resource not found" do
       before do
         stub_get("/repos/#{user}/#{repo}/git/blobs/#{sha}").
-          to_return(:body => fixture('git_data/blob.json'), :status => 404, :headers => {:content_type => "application/json; charset=utf-8"})
+          to_return(:body => fixture('git_data/blob.json'),
+            :status => 404,
+            :headers => {:content_type => "application/json; charset=utf-8"})
       end
 
       it "should fail to retrive resource" do
         expect {
-          github.git_data.blob user, repo, sha
+          github.git_data.blobs.get user, repo, sha
         }.to raise_error(Github::Error::NotFound)
       end
     end
-  end # blob
+  end # get
 
-  describe "create_blob" do
+  describe "#create" do
     let(:inputs) {
       {
         "content" => "Content of the blob",
@@ -69,33 +74,35 @@ describe Github::GitData::Blobs do
       before do
         stub_post("/repos/#{user}/#{repo}/git/blobs").
           with(:body => JSON.generate(inputs.except('unrelated'))).
-          to_return(:body => fixture('git_data/blob_sha.json'), :status => 201, :headers => {:content_type => "application/json; charset=utf-8"})
+          to_return(:body => fixture('git_data/blob_sha.json'),
+            :status => 201,
+            :headers => {:content_type => "application/json; charset=utf-8"})
       end
 
       it "should fail to create resource if 'content' input is missing" do
         expect {
-          github.git_data.create_blob user, repo, inputs.except('content')
+          github.git_data.blobs.create user, repo, inputs.except('content')
         }.to raise_error(Github::Error::RequiredParams)
       end
 
       it "should fail to create resource if 'encoding' input is missing" do
         expect {
-          github.git_data.create_blob user, repo, inputs.except('encoding')
+          github.git_data.blobs.create user, repo, inputs.except('encoding')
         }.to raise_error(Github::Error::RequiredParams)
       end
 
       it "should create resource successfully" do
-        github.git_data.create_blob user, repo, inputs
+        github.git_data.blobs.create user, repo, inputs
         a_post("/repos/#{user}/#{repo}/git/blobs").with(inputs).should have_been_made
       end
 
       it "should return the resource" do
-        blob_sha = github.git_data.create_blob user, repo, inputs
+        blob_sha = github.git_data.blobs.create user, repo, inputs
         blob_sha.should be_a Hashie::Mash
       end
 
       it "should get the blob information" do
-        blob_sha = github.git_data.create_blob user, repo, inputs
+        blob_sha = github.git_data.blobs.create user, repo, inputs
         blob_sha.sha.should == sha
       end
     end
@@ -109,10 +116,10 @@ describe Github::GitData::Blobs do
 
       it "should faile to retrieve resource" do
         expect {
-          github.git_data.create_blob user, repo, inputs
+          github.git_data.blobs.create user, repo, inputs
         }.to raise_error(Github::Error::NotFound)
       end
     end
-  end # create_blob
+  end # create
 
 end # Github::GitData::Blobs
