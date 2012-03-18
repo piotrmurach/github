fa8d3647aaa09518b85b9c27f21fb2ea302deb2a
Piotr Murach <pmurach@gmail.com>
2012-03-18 13:37:28 +0000

    Change git data references api calls.

--- lib/github_api/git_data/references.rb
+++ lib/github_api/git_data/references.rb
@@ -1,126 +1,128 @@
 # encoding: utf-8
 
 module Github
-  class GitData
-    module References
+  class GitData::References < API
 
-      VALID_REF_PARAM_NAMES = %w[ ref sha force ].freeze
+    VALID_REF_PARAM_NAMES = %w[ ref sha force ].freeze
 
-      VALID_REF_PARAM_VALUES = {
-        'ref' => %r{^refs\/\w+\/\w+(\/\w+)*} # test fully qualified reference
-      }
+    VALID_REF_PARAM_VALUES = {
+      'ref' => %r{^refs\/\w+\/\w+(\/\w+)*} # test fully qualified reference
+    }
 
-      # Get all references
-      #
-      # This will return an array of all the references on the system,
-      # including things like notes and stashes if they exist on the server.
-      # Anything in the namespace, not just <tt>heads</tt> and <tt>tags</tt>,
-      # though that would be the most common.
-      #
-      # = Examples
-      #  @github = Github.new
-      #  @github.git_data.references 'user-name', 'repo-name'
-      #
-      #  @github.git_data.references 'user-name', 'repo-name', 'tags'
-      #
-      def references(user_name, repo_name, ref=nil, params={})
-        _update_user_repo_params(user_name, repo_name)
-        _validate_user_repo_params(user, repo) unless user? && repo?
-        _normalize_params_keys(params)
+    # Creates new GitData::References API
+    def initialize(options = {})
+      super(options)
+    end
 
-        response = if ref
-          _validate_reference ref
-          get("/repos/#{user}/#{repo}/git/refs/#{ref}", params)
-        else
-          get("/repos/#{user}/#{repo}/git/refs", params)
-        end
-        return response unless block_given?
-        response.each { |el| yield el }
-      end
-      alias :list_references :references
-      alias :get_all_references :references
+    # Get all references
+    #
+    # This will return an array of all the references on the system,
+    # including things like notes and stashes if they exist on the server.
+    # Anything in the namespace, not just <tt>heads</tt> and <tt>tags</tt>,
+    # though that would be the most common.
+    #
+    # = Examples
+    #  github = Github.new
+    #  github.git_data.references.list 'user-name', 'repo-name'
+    #
+    #  github.git_data.references.list 'user-name', 'repo-name', 'tags'
+    #
+    def list(user_name, repo_name, ref=nil, params={})
+      _update_user_repo_params(user_name, repo_name)
+      _validate_user_repo_params(user, repo) unless user? && repo?
+      _normalize_params_keys(params)
 
-      # Get a reference
-      #
-      # The ref in the URL must be formatted as <tt>heads/branch</tt>,
-      # not just branch. For example, the call to get the data for a
-      # branch named <tt>sc/featureA</tt> would be formatted as
-      # <tt>heads/sc/featureA</tt>
-      #
-      # = Examples
-      #  @github = Github.new
-      #  @github.git_data.reference 'user-name', 'repo-name', 'reference'
-      #
-      def reference(user_name, repo_name, ref, params={})
-        _update_user_repo_params(user_name, repo_name)
-        _validate_user_repo_params(user, repo) unless user? && repo?
-
-        _validate_presence_of ref
+      response = if ref
         _validate_reference ref
-        _normalize_params_keys params
-
-        get("/repos/#{user}/#{repo}/git/refs/#{ref}", params)
+        get_request("/repos/#{user}/#{repo}/git/refs/#{ref}", params)
+      else
+        get_request("/repos/#{user}/#{repo}/git/refs", params)
       end
-      alias :get_reference :reference
+      return response unless block_given?
+      response.each { |el| yield el }
+    end
+    alias :all :list
+
+    # Get a reference
+    #
+    # The ref in the URL must be formatted as <tt>heads/branch</tt>,
+    # not just branch. For example, the call to get the data for a
+    # branch named <tt>sc/featureA</tt> would be formatted as
+    # <tt>heads/sc/featureA</tt>
+    #
+    # = Examples
+    #  github = Github.new
+    #  github.git_data.references.get 'user-name', 'repo-name', 'reference'
+    #
+    def get(user_name, repo_name, ref, params={})
+      _update_user_repo_params(user_name, repo_name)
+      _validate_user_repo_params(user, repo) unless user? && repo?
+
+      _validate_presence_of ref
+      _validate_reference ref
+      _normalize_params_keys params
+
+      get_request("/repos/#{user}/#{repo}/git/refs/#{ref}", params)
+    end
+    alias :find :get
 
 
-      # Create a reference
-      #
-      # = Inputs
-      # * <tt>:ref</tt> - String of the name of the fully qualified reference (ie: refs/heads/master). If it doesn’t start with ‘refs’ and have at least two slashes, it will be rejected.
-      # * <tt>:sha</tt> - String of the SHA1 value to set this reference to
-      #
-      # = Examples
-      #  @github = Github.new
-      #  @github.git_data.create_reference 'user-name', 'repo-name',
-      #    "ref" => "refs/heads/master",
-      #    "sha" =>  "827efc6d56897b048c772eb4087f854f46256132"
-      #
-      def create_reference(user_name, repo_name, params={})
-        _update_user_repo_params(user_name, repo_name)
-        _validate_user_repo_params(user, repo) unless user? && repo?
+    # Create a reference
+    #
+    # = Inputs
+    # * <tt>:ref</tt> - String of the name of the fully qualified reference (ie: refs/heads/master). If it doesn’t start with ‘refs’ and have at least two slashes, it will be rejected.
+    # * <tt>:sha</tt> - String of the SHA1 value to set this reference to
+    #
+    # = Examples
+    #  github = Github.new
+    #  github.git_data.references.create 'user-name', 'repo-name',
+    #    "ref" => "refs/heads/master",
+    #    "sha" =>  "827efc6d56897b048c772eb4087f854f46256132"
+    #
+    def create(user_name, repo_name, params={})
+      _update_user_repo_params(user_name, repo_name)
+      _validate_user_repo_params(user, repo) unless user? && repo?
 
-        _normalize_params_keys params
-        _filter_params_keys VALID_REF_PARAM_NAMES, params
-        _validate_reference params['ref']
-        _validate_inputs(%w[ ref sha ], params)
+      _normalize_params_keys params
+      _filter_params_keys VALID_REF_PARAM_NAMES, params
+      _validate_reference params['ref']
+      _validate_inputs(%w[ ref sha ], params)
 
-        post("/repos/#{user}/#{repo}/git/refs", params)
+      post_request("/repos/#{user}/#{repo}/git/refs", params)
+    end
+
+    # Update a reference
+    #
+    # = Inputs
+    # * <tt>:sha</tt> - String of the SHA1 value to set this reference to
+    # * <tt>:force</tt> - Boolean indicating whether to force the update or to make sure the update is a fast-forward update. The default is <tt>false</tt>, so leaving this out or setting it to false will make sure you’re not overwriting work.
+    #
+    # = Examples
+    #  github = Github.new
+    #  github.git_data.references.create 'user-name', 'repo-name',
+    #    "sha" =>  "827efc6d56897b048c772eb4087f854f46256132",
+    #    "force" => true
+    #
+    def update(user_name, repo_name, ref, params={})
+      _update_user_repo_params(user_name, repo_name)
+      _validate_user_repo_params(user, repo) unless user? && repo?
+
+      _validate_presence_of ref
+      _validate_reference ref
+      _normalize_params_keys(params)
+      _filter_params_keys(VALID_REF_PARAM_NAMES, params)
+      _validate_inputs(%w[ sha ], params)
+
+      patch("/repos/#{user}/#{repo}/git/refs/#{ref}", params)
+    end
+
+  private
+
+    def _validate_reference ref
+      unless VALID_REF_PARAM_VALUES['ref'] =~ "refs/#{ref}"
+        raise ArgumentError, "Provided 'reference' is invalid"
       end
+    end
 
-      # Update a reference
-      #
-      # = Inputs
-      # * <tt>:sha</tt> - String of the SHA1 value to set this reference to
-      # * <tt>:force</tt> - Boolean indicating whether to force the update or to make sure the update is a fast-forward update. The default is <tt>false</tt>, so leaving this out or setting it to false will make sure you’re not overwriting work.
-      #
-      # = Examples
-      #  @github = Github.new
-      #  @github.git_data.create_reference 'user-name', 'repo-name',
-      #    "sha" =>  "827efc6d56897b048c772eb4087f854f46256132",
-      #    "force" => true
-      #
-      def update_reference(user_name, repo_name, ref, params={})
-        _update_user_repo_params(user_name, repo_name)
-        _validate_user_repo_params(user, repo) unless user? && repo?
-
-        _validate_presence_of ref
-        _validate_reference ref
-        _normalize_params_keys(params)
-        _filter_params_keys(VALID_REF_PARAM_NAMES, params)
-        _validate_inputs(%w[ sha ], params)
-
-        patch("/repos/#{user}/#{repo}/git/refs/#{ref}", params)
-      end
-
-    private
-
-      def _validate_reference ref
-        unless VALID_REF_PARAM_VALUES['ref'] =~ "refs/#{ref}"
-          raise ArgumentError, "Provided 'reference' is invalid"
-        end
-      end
-
-    end # References
-  end # GitData
+  end # GitData::References
 end # Github
--- spec/github/git_data/references_spec.rb
+++ spec/github/git_data/references_spec.rb
@@ -14,13 +14,8 @@ describe Github::GitData::References do
   it { described_class::VALID_REF_PARAM_NAMES.should_not be_nil }
   it { described_class::VALID_REF_PARAM_VALUES.should_not be_nil }
 
-  describe "references" do
-
-    context 'check aliases' do
-      it { github.git_data.should respond_to :references }
-      it { github.git_data.should respond_to :list_references }
-      it { github.git_data.should respond_to :get_all_references }
-    end
+  describe "#list" do
+    it { github.git_data.references.should respond_to :all }
 
     context "get all the refernces based on sub-namespace" do
       before do
@@ -29,39 +24,42 @@ describe Github::GitData::References do
       end
 
       it "should fail to get resource without username" do
-        expect { github.git_data.references }.to raise_error(ArgumentError)
+        expect {
+          github.git_data.references.list nil, repo
+        }.to raise_error(ArgumentError)
       end
 
       it "should fail to call with invalid reference" do
         expect {
-          github.git_data.references user, repo, 'branch'
+          github.git_data.references.list user, repo, 'branch'
         }.to raise_error(ArgumentError)
       end
 
       it "should get the resources" do
-        github.git_data.references user, repo, ref
+        github.git_data.references.list user, repo, ref
         a_get("/repos/#{user}/#{repo}/git/refs/#{ref}").should have_been_made
       end
 
       it "should return array of resources" do
-        references = github.git_data.references user, repo, ref
+        references = github.git_data.references.list user, repo, ref
         references.should be_an Array
         references.should have(3).items
       end
 
       it "should be a mash type" do
-        references = github.git_data.references user, repo, ref
+        references = github.git_data.references.list user, repo, ref
         references.first.should be_a Hashie::Mash
       end
 
       it "should get reference information" do
-        references = github.git_data.references user, repo, ref
+        references = github.git_data.references.list user, repo, ref
         references.first.ref.should eql 'refs/heads/master'
       end
 
       it "should yield to a block" do
-        github.git_data.should_receive(:references).with(user, repo, ref).and_yield('web')
-        github.git_data.references(user, repo, ref) { |param| 'web' }
+        github.git_data.references.should_receive(:list).
+          with(user, repo, ref).and_yield('web')
+        github.git_data.references.list(user, repo, ref) { |param| 'web' }
       end
     end
 
@@ -72,7 +70,7 @@ describe Github::GitData::References do
       end
 
       it "should get the resources" do
-        github.git_data.references user, repo
+        github.git_data.references.list user, repo
         a_get("/repos/#{user}/#{repo}/git/refs").should have_been_made
       end
     end
@@ -85,15 +83,14 @@ describe Github::GitData::References do
 
       it "should return 404 with a message 'Not Found'" do
         expect {
-          github.git_data.references user, repo, ref
+          github.git_data.references.list user, repo, ref
         }.to raise_error(Github::Error::NotFound)
       end
     end
-  end # references
+  end # list
 
-  describe "reference" do
-    it { github.git_data.should respond_to :reference }
-    it { github.git_data.should respond_to :get_reference }
+  describe "#get" do
+    it { github.git_data.references.should respond_to :find }
 
     context "resource found" do
       before do
@@ -102,27 +99,29 @@ describe Github::GitData::References do
       end
 
       it "should fail to get resource without ref" do
-        expect { github.git_data.reference(user, repo, nil)}.to raise_error(ArgumentError)
+        expect {
+          github.git_data.references.get user, repo, nil
+        }.to raise_error(ArgumentError)
       end
 
       it "should fail to get resource with wrong ref" do
         expect {
-          github.git_data.reference user, repo, 'branch'
+          github.git_data.references.get user, repo, 'branch'
         }.to raise_error(ArgumentError)
       end
 
       it "should get the resource" do
-        github.git_data.reference user, repo, ref
+        github.git_data.references.get user, repo, ref
         a_get("/repos/#{user}/#{repo}/git/refs/#{ref}").should have_been_made
       end
 
       it "should get reference information" do
-        reference = github.git_data.reference user, repo, ref
+        reference = github.git_data.references.get user, repo, ref
         reference.first.ref.should eql "refs/heads/sc/featureA"
       end
 
       it "should return mash" do
-        reference = github.git_data.reference user, repo, ref
+        reference = github.git_data.references.get user, repo, ref
         reference.first.should be_a Hashie::Mash
       end
     end
@@ -135,13 +134,13 @@ describe Github::GitData::References do
 
       it "should fail to retrive resource" do
         expect {
-          github.git_data.reference user, repo, ref
+          github.git_data.references.get user, repo, ref
         }.to raise_error(Github::Error::NotFound)
       end
     end
-  end # reference
+  end # get
 
-  describe "create_reference" do
+  describe "#create" do
     let(:inputs) {
       {
         "ref" => "refs/heads/master",
@@ -159,34 +158,34 @@ describe Github::GitData::References do
 
       it "should fail to create resource if 'ref' input is missing" do
         expect {
-          github.git_data.create_reference user, repo, inputs.except('ref')
+          github.git_data.references.create user, repo, inputs.except('ref')
         }.to raise_error(ArgumentError)
       end
 
       it "should fail to create resource if 'sha' input is missing" do
         expect {
-          github.git_data.create_reference user, repo, inputs.except('sha')
+          github.git_data.references.create user, repo, inputs.except('sha')
         }.to raise_error(Github::Error::RequiredParams)
       end
 
       it "should fail to create resource if 'ref' is wrong" do
         expect {
-          github.git_data.create_reference user, repo, 'branch'
+          github.git_data.references.create user, repo, 'branch'
         }.to raise_error(ArgumentError)
       end
 
       it "should create resource successfully" do
-        github.git_data.create_reference user, repo, inputs
+        github.git_data.references.create user, repo, inputs
         a_post("/repos/#{user}/#{repo}/git/refs").with(inputs).should have_been_made
       end
 
       it "should return the resource" do
-        reference = github.git_data.create_reference user, repo, inputs
+        reference = github.git_data.references.create user, repo, inputs
         reference.first.should be_a Hashie::Mash
       end
 
       it "should get the reference information" do
-        reference = github.git_data.create_reference user, repo, inputs
+        reference = github.git_data.references.create user, repo, inputs
         reference.first.ref.should eql 'refs/heads/sc/featureA'
       end
     end
@@ -200,13 +199,13 @@ describe Github::GitData::References do
 
       it "should faile to retrieve resource" do
         expect {
-          github.git_data.create_reference user, repo, inputs
+          github.git_data.references.create user, repo, inputs
         }.to raise_error(Github::Error::NotFound)
       end
     end
-  end # create_reference
+  end # create
 
-  describe "update_reference" do
+  describe "#update" do
     let(:inputs) {
       {
         "sha" => "827efc6d56897b048c772eb4087f854f46256132",
@@ -224,28 +223,29 @@ describe Github::GitData::References do
 
       it "should fail to update resource if 'sha' input is missing" do
         expect {
-          github.git_data.update_reference user, repo, ref, inputs.except('sha')
+          github.git_data.references.update user, repo, ref, inputs.except('sha')
         }.to raise_error(Github::Error::RequiredParams)
       end
 
       it "should fail to update resource if 'ref' is wrong" do
         expect {
-          github.git_data.update_reference user, repo, 'branch', inputs
+          github.git_data.references.update user, repo, 'branch', inputs
         }.to raise_error(ArgumentError)
       end
 
       it "should update resource successfully" do
-        github.git_data.update_reference user, repo, ref, inputs
-        a_patch("/repos/#{user}/#{repo}/git/refs/#{ref}").with(inputs).should have_been_made
+        github.git_data.references.update user, repo, ref, inputs
+        a_patch("/repos/#{user}/#{repo}/git/refs/#{ref}").
+          with(inputs).should have_been_made
       end
 
       it "should return the resource" do
-        reference = github.git_data.update_reference user, repo, ref, inputs
+        reference = github.git_data.references.update user, repo, ref, inputs
         reference.first.should be_a Hashie::Mash
       end
 
       it "should get the reference information" do
-        reference = github.git_data.update_reference user, repo, ref, inputs
+        reference = github.git_data.references.update user, repo, ref, inputs
         reference.first.ref.should eql 'refs/heads/sc/featureA'
       end
     end
@@ -259,10 +259,10 @@ describe Github::GitData::References do
 
       it "should faile to retrieve resource" do
         expect {
-          github.git_data.update_reference user, repo, ref, inputs
+          github.git_data.references.update user, repo, ref, inputs
         }.to raise_error(Github::Error::NotFound)
       end
     end
-  end # update_reference
+  end # update
 
 end # Github::GitData::References
