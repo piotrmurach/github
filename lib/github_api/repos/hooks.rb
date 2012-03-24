7902dd7b264101c6f6099bd79dc3047ebb20d0e1
Piotr Murach <pmurach@gmail.com>
2012-03-24 11:56:26 +0000

    Change repository hooks api method names, update docs.

--- lib/github_api/repos/hooks.rb
+++ lib/github_api/repos/hooks.rb
@@ -1,172 +1,166 @@
 # encoding: utf-8
 
 module Github
-  class Repos
+  class Repos::Hooks < API
     # The Repository Hooks API manages the post-receive web and service hooks for a repository.
-    module Hooks
 
-      VALID_HOOK_PARAM_NAMES = %w[
-        name
-        config
-        active
-        events
-        add_events
-        remove_events
-      ].freeze # :nodoc:
+    VALID_HOOK_PARAM_NAMES = %w[
+      name
+      config
+      active
+      events
+      add_events
+      remove_events
+    ].freeze # :nodoc:
 
-      # Active hooks can be configured to trigger for one or more events. The default event is push.
-      # The available events are:
-      VALID_HOOK_PARAM_VALUES = {
-        'events' => %w[
-          push
-          issues
-          issue_comment
-          commit_comment
-          pull_request
-          gollum
-          watch
-          download
-          fork
-          fork_apply
-          member
-          public
-        ]
-      }.freeze # :nodoc:
+    # Active hooks can be configured to trigger for one or more events. The default event is push.
+    # The available events are:
+    VALID_HOOK_PARAM_VALUES = {
+      'events' => %w[
+        push
+        issues
+        issue_comment
+        commit_comment
+        pull_request
+        gollum
+        watch
+        download
+        fork
+        fork_apply
+        member
+        public
+      ]
+    }.freeze # :nodoc:
 
-      REQUIRED_PARAMS = %w[ name config ].freeze # :nodoc:
+    REQUIRED_PARAMS = %w[ name config ].freeze # :nodoc:
 
-      # List repository hooks
-      #
-      # = Examples
-      #  @github = Github.new
-      #  @github.repos.hooks 'user-name', 'repo-name'
-      #  @github.repos.hooks 'user-name', 'repo-name' { |hook| ... }
-      #
-      def hooks(user_name, repo_name, params={})
-        _update_user_repo_params(user_name, repo_name)
-        _validate_user_repo_params(user, repo) unless user? && repo?
-        _normalize_params_keys(params)
+    # List repository hooks
+    #
+    # = Examples
+    #  github = Github.new
+    #  github.repos.hooks.list 'user-name', 'repo-name'
+    #  github.repos.hooks.list 'user-name', 'repo-name' { |hook| ... }
+    #
+    def list(user_name, repo_name, params={})
+      _update_user_repo_params(user_name, repo_name)
+      _validate_user_repo_params(user, repo) unless user? && repo?
+      _normalize_params_keys(params)
 
-        response = get("/repos/#{user}/#{repo}/hooks", params)
-        return response unless block_given?
-        response.each { |el| yield el }
-      end
-      alias :repo_hooks :hooks
-      alias :repository_hooks :hooks
+      response = get_request("/repos/#{user}/#{repo}/hooks", params)
+      return response unless block_given?
+      response.each { |el| yield el }
+    end
+    alias :all :list
 
-      # Get a single hook
-      #
-      # = Examples
-      #  @github = Github.new
-      #  @github.repos.hook 'user-name', 'repo-name'
-      #
-      def hook(user_name, repo_name, hook_id, params={})
-        _update_user_repo_params(user_name, repo_name)
-        _validate_user_repo_params(user, repo) unless user? && repo?
-        _validate_presence_of hook_id
-        _normalize_params_keys(params)
+    # Get a single hook
+    #
+    # = Examples
+    #  github = Github.new
+    #  github.repos.hooks.get 'user-name', 'repo-name'
+    #
+    def get(user_name, repo_name, hook_id, params={})
+      _update_user_repo_params(user_name, repo_name)
+      _validate_user_repo_params(user, repo) unless user? && repo?
+      _validate_presence_of hook_id
+      _normalize_params_keys(params)
 
-        get("/repos/#{user}/#{repo}/hooks/#{hook_id}", params)
-      end
-      alias :get_hook :hook
-      alias :repo_hook :hook
-      alias :repository_hook :hook
-      alias :get_repo_hook :hook
+      get_request("/repos/#{user}/#{repo}/hooks/#{hook_id}", params)
+    end
+    alias :find :get
 
-      # Create a hook
-      #
-      # = Inputs
-      # * <tt>:name</tt> - Required string - the name of the service that is being called.
-      # * <tt>:config</tt> - Required hash - A Hash containing key/value pairs to provide settings for this hook.
-      # * <tt>:events</tt> - Optional array - Determines what events the hook is triggered for. Default: ["push"]
-      # * <tt>:active</tt> - Optional boolean - Determines whether the hook is actually triggered on pushes.
-      #
-      # = Examples
-      #  @github = Github.new
-      #  @github.repos.create_hook 'user-name', 'repo-name',
-      #    "name" =>  "web",
-      #    "active" => true,
-      #    "config" => {
-      #      "url" => "http://something.com/webhook"
-      #      }
-      #    }
-      #
-      def create_hook(user_name=nil, repo_name=nil, params={})
-        _update_user_repo_params(user_name, repo_name)
-        _validate_user_repo_params(user, repo) unless user? && repo?
+    # Create a hook
+    #
+    # = Inputs
+    # * <tt>:name</tt> - Required string - the name of the service that is being called.
+    # * <tt>:config</tt> - Required hash - A Hash containing key/value pairs to provide settings for this hook.
+    # * <tt>:events</tt> - Optional array - Determines what events the hook is triggered for. Default: ["push"]
+    # * <tt>:active</tt> - Optional boolean - Determines whether the hook is actually triggered on pushes.
+    #
+    # = Examples
+    #  github = Github.new
+    #  github.repos.hooks.create 'user-name', 'repo-name',
+    #    "name" =>  "web",
+    #    "active" => true,
+    #    "config" => {
+    #      "url" => "http://something.com/webhook"
+    #      }
+    #    }
+    #
+    def create(user_name, repo_name, params={})
+      _update_user_repo_params(user_name, repo_name)
+      _validate_user_repo_params(user, repo) unless user? && repo?
 
-        _normalize_params_keys(params)
-        _filter_params_keys(VALID_HOOK_PARAM_NAMES, params, :recursive => false)
-        _validate_inputs(REQUIRED_PARAMS, params)
+      _normalize_params_keys(params)
+      _filter_params_keys(VALID_HOOK_PARAM_NAMES, params, :recursive => false)
+      _validate_inputs(REQUIRED_PARAMS, params)
 
-        post("/repos/#{user}/#{repo}/hooks", params)
-      end
+      post_request("/repos/#{user}/#{repo}/hooks", params)
+    end
 
-      # Edit a hook
-      #
-      # = Inputs
-      # * <tt>:name</tt> - Required string - the name of the service that is being called.
-      # * <tt>:config</tt> - Required hash - A Hash containing key/value pairs to provide settings for this hook.
-      # * <tt>:events</tt> - Optional array - Determines what events the hook is triggered for. This replaces the entire array of events. Default: ["push"].
-      # * <tt>:add_events</tt> - Optional array - Determines a list of events to be added to the list of events that the Hook triggers for.
-      # * <tt>:remove_events</tt> - Optional array - Determines a list of events to be removed from the list of events that the Hook triggers for.
-      # * <tt>:active</tt> - Optional boolean - Determines whether the hook is actually triggered on pushes.
-      #
-      # = Examples
-      #  @github = Github.new
-      #  @github.repos.edit_hook 'user-name', 'repo-name',
-      #    "name" => "campfire",
-      #    "active" =>  true,
-      #    "config" =>  {
-      #      "subdomain" => "github",
-      #      "room" =>  "Commits",
-      #      "token" => "abc123"
-      #    }
-      #
-      def edit_hook(user_name, repo_name, hook_id, params={})
-        _update_user_repo_params(user_name, repo_name)
-        _validate_user_repo_params(user, repo) unless user? && repo?
-        _validate_presence_of hook_id
+    # Edit a hook
+    #
+    # = Inputs
+    # * <tt>:name</tt> - Required string - the name of the service that is being called.
+    # * <tt>:config</tt> - Required hash - A Hash containing key/value pairs to provide settings for this hook.
+    # * <tt>:events</tt> - Optional array - Determines what events the hook is triggered for. This replaces the entire array of events. Default: ["push"].
+    # * <tt>:add_events</tt> - Optional array - Determines a list of events to be added to the list of events that the Hook triggers for.
+    # * <tt>:remove_events</tt> - Optional array - Determines a list of events to be removed from the list of events that the Hook triggers for.
+    # * <tt>:active</tt> - Optional boolean - Determines whether the hook is actually triggered on pushes.
+    #
+    # = Examples
+    #  github = Github.new
+    #  github.repos.hooks.edit 'user-name', 'repo-name',
+    #    "name" => "campfire",
+    #    "active" =>  true,
+    #    "config" =>  {
+    #      "subdomain" => "github",
+    #      "room" =>  "Commits",
+    #      "token" => "abc123"
+    #    }
+    #
+    def edit(user_name, repo_name, hook_id, params={})
+      _update_user_repo_params(user_name, repo_name)
+      _validate_user_repo_params(user, repo) unless user? && repo?
+      _validate_presence_of hook_id
 
-        _normalize_params_keys(params)
-        _filter_params_keys(VALID_HOOK_PARAM_NAMES, params, :recursive => false)
-        _validate_inputs(REQUIRED_PARAMS, params)
+      _normalize_params_keys(params)
+      _filter_params_keys(VALID_HOOK_PARAM_NAMES, params, :recursive => false)
+      _validate_inputs(REQUIRED_PARAMS, params)
 
-        patch("/repos/#{user}/#{repo}/hooks/#{hook_id}", params)
-      end
+      patch_request("/repos/#{user}/#{repo}/hooks/#{hook_id}", params)
+    end
 
-      # Test a hook
-      #
-      # This will trigger the hook with the latest push to the current repository.
-      #
-      # = Examples
-      #  @github = Github.new
-      #  @github.repos.test_hook 'user-name', 'repo-name', 'hook-id'
-      #
-      def test_hook(user_name, repo_name, hook_id, params={})
-        _update_user_repo_params(user_name, repo_name)
-        _validate_user_repo_params(user, repo) unless user? && repo?
-        _validate_presence_of hook_id
-        _normalize_params_keys(params)
+    # Test a hook
+    #
+    # This will trigger the hook with the latest push to the current repository.
+    #
+    # = Examples
+    #  github = Github.new
+    #  github.repos.hooks.test 'user-name', 'repo-name', 'hook-id'
+    #
+    def test(user_name, repo_name, hook_id, params={})
+      _update_user_repo_params(user_name, repo_name)
+      _validate_user_repo_params(user, repo) unless user? && repo?
+      _validate_presence_of hook_id
+      _normalize_params_keys(params)
 
-        post("/repos/#{user}/#{repo}/hooks/#{hook_id}/test", params)
-      end
+      post_request("/repos/#{user}/#{repo}/hooks/#{hook_id}/test", params)
+    end
 
-      # Delete a hook
-      #
-      # = Examples
-      #  @github = Github.new
-      #  @github.repos.delete_hook 'user-name', 'repo-name', 'hook-id'
-      #
-      def delete_hook(user_name, repo_name, hook_id, params={})
-        _update_user_repo_params(user_name, repo_name)
-        _validate_user_repo_params(user, repo) unless user? && repo?
-        _validate_presence_of hook_id
-        _normalize_params_keys(params)
+    # Delete a hook
+    #
+    # = Examples
+    #  github = Github.new
+    #  github.repos.hooks.delete 'user-name', 'repo-name', 'hook-id'
+    #
+    def delete(user_name, repo_name, hook_id, params={})
+      _update_user_repo_params(user_name, repo_name)
+      _validate_user_repo_params(user, repo) unless user? && repo?
+      _validate_presence_of hook_id
+      _normalize_params_keys(params)
 
-        delete("/repos/#{user}/#{repo}/hooks/#{hook_id}", params)
-      end
+      delete_request("/repos/#{user}/#{repo}/hooks/#{hook_id}", params)
+    end
 
-    end # Hooks
-  end # Repos
+  end # Repos::Hooks
 end # Github
--- spec/github/repos/hooks_spec.rb
+++ spec/github/repos/hooks_spec.rb
@@ -13,47 +13,44 @@ describe Github::Repos::Hooks do
   it { described_class::VALID_HOOK_PARAM_VALUES.should_not be_nil }
   it { described_class::REQUIRED_PARAMS.should_not be_nil }
 
-  describe "hooks" do
-    context 'check aliases' do
-      it { github.repos.should respond_to :hooks }
-      it { github.repos.should respond_to :repo_hooks }
-      it { github.repos.should respond_to :repository_hooks }
-    end
+  describe "#list" do
+    it { github.repos.hooks.should respond_to :all }
 
     context "resource found" do
       before do
         stub_get("/repos/#{user}/#{repo}/hooks").
-          to_return(:body => fixture('repos/hooks.json'), :status => 200, :headers => {:content_type => "application/json; charset=utf-8"})
+          to_return(:body => fixture('repos/hooks.json'), :status => 200,
+            :headers => {:content_type => "application/json; charset=utf-8"})
       end
 
       it "should fail to get resource without username" do
-        expect { github.repos.hooks }.to raise_error(ArgumentError)
+        expect { github.repos.hooks.list }.to raise_error(ArgumentError)
       end
 
       it "should get the resources" do
-        github.repos.hooks user, repo
+        github.repos.hooks.list user, repo
         a_get("/repos/#{user}/#{repo}/hooks").should have_been_made
       end
 
       it "should return array of resources" do
-        hooks = github.repos.hooks user, repo
+        hooks = github.repos.hooks.list user, repo
         hooks.should be_an Array
         hooks.should have(1).items
       end
 
       it "should be a mash type" do
-        hooks = github.repos.hooks user, repo
+        hooks = github.repos.hooks.list user, repo
         hooks.first.should be_a Hashie::Mash
       end
 
       it "should get hook information" do
-        hooks = github.repos.hooks user, repo
+        hooks = github.repos.hooks.list user, repo
         hooks.first.name.should == 'web'
       end
 
       it "should yield to a block" do
-        github.repos.should_receive(:hooks).with(user, repo).and_yield('web')
-        github.repos.hooks(user, repo) { |param| 'web' }
+        github.repos.hooks.should_receive(:list).with(user, repo).and_yield('web')
+        github.repos.hooks.list(user, repo) { |param| 'web' }
       end
     end
 
@@ -65,46 +62,43 @@ describe Github::Repos::Hooks do
 
       it "should return 404 with a message 'Not Found'" do
         expect {
-          github.repos.hooks user, repo
+          github.repos.hooks.list user, repo
         }.to raise_error(Github::Error::NotFound)
       end
     end
-  end # hooks
+  end # list
 
-  describe "hook" do
+  describe "#get" do
     let(:hook_id) { 1 }
 
-    context 'check aliases' do
-      it { github.repos.should respond_to :hook }
-      it { github.repos.should respond_to :get_hook }
-      it { github.repos.should respond_to :repo_hook }
-      it { github.repos.should respond_to :get_repo_hook }
-      it { github.repos.should respond_to :repository_hook }
-    end
+    it { github.repos.hooks.should respond_to :find }
 
     context "resource found" do
       before do
         stub_get("/repos/#{user}/#{repo}/hooks/#{hook_id}").
-          to_return(:body => fixture('repos/hook.json'), :status => 200, :headers => {:content_type => "application/json; charset=utf-8"})
+          to_return(:body => fixture('repos/hook.json'), :status => 200,
+            :headers => {:content_type => "application/json; charset=utf-8"})
       end
 
       it "should fail to get resource without hook id" do
-        expect { github.repos.hook(user, repo, nil)}.to raise_error(ArgumentError)
+        expect {
+          github.repos.hooks.get user, repo, nil
+        }.to raise_error(ArgumentError)
       end
 
       it "should get the resource" do
-        github.repos.hook user, repo, hook_id
+        github.repos.hooks.get user, repo, hook_id
         a_get("/repos/#{user}/#{repo}/hooks/#{hook_id}").should have_been_made
       end
 
       it "should get hook information" do
-        hook = github.repos.hook user, repo, hook_id
+        hook = github.repos.hooks.get user, repo, hook_id
         hook.id.should == hook_id
         hook.name.should == 'web'
       end
 
       it "should return mash" do
-        hook = github.repos.hook user, repo, hook_id
+        hook = github.repos.hooks.get user, repo, hook_id
         hook.should be_a Hashie::Mash
       end
     end
@@ -117,13 +111,13 @@ describe Github::Repos::Hooks do
 
       it "should fail to retrive resource" do
         expect {
-          github.repos.hook user, repo, hook_id
+          github.repos.hooks.get user, repo, hook_id
         }.to raise_error(Github::Error::NotFound)
       end
     end
-  end # hook
+  end # get
 
-  describe "create_hook" do
+  describe "#create" do
     let(:inputs) {
       {
         :name => 'web',
@@ -143,33 +137,34 @@ describe Github::Repos::Hooks do
       before do
         stub_post("/repos/#{user}/#{repo}/hooks").
           with(:body => JSON.generate(inputs.except(:unrelated))).
-          to_return(:body => fixture('repos/hook.json'), :status => 201, :headers => {:content_type => "application/json; charset=utf-8"})
+          to_return(:body => fixture('repos/hook.json'), :status => 201,
+            :headers => {:content_type => "application/json; charset=utf-8"})
       end
 
       it "should fail to create resource if 'name' input is missing" do
         expect {
-          github.repos.create_hook user, repo, inputs.except(:name)
+          github.repos.hooks.create user, repo, inputs.except(:name)
         }.to raise_error(Github::Error::RequiredParams)
       end
 
       it "should failt to create resource if 'config' input is missing" do
         expect {
-          github.repos.create_hook user, repo, inputs.except(:config)
+          github.repos.hooks.create user, repo, inputs.except(:config)
         }.to raise_error(Github::Error::RequiredParams)
       end
 
       it "should create resource successfully" do
-        github.repos.create_hook user, repo, inputs
+        github.repos.hooks.create user, repo, inputs
         a_post("/repos/#{user}/#{repo}/hooks").with(inputs).should have_been_made
       end
 
       it "should return the resource" do
-        hook = github.repos.create_hook user, repo, inputs
+        hook = github.repos.hooks.create user, repo, inputs
         hook.should be_a Hashie::Mash
       end
 
       it "should get the hook information" do
-        hook = github.repos.create_hook(user, repo, inputs)
+        hook = github.repos.hooks.create user, repo, inputs
         hook.name.should == 'web'
       end
     end
@@ -177,18 +172,19 @@ describe Github::Repos::Hooks do
     context "fail to create resource" do
       before do
         stub_post("/repos/#{user}/#{repo}/hooks").with(inputs).
-          to_return(:body => fixture('repos/hook.json'), :status => 404, :headers => {:content_type => "application/json; charset=utf-8"})
+          to_return(:body => fixture('repos/hook.json'), :status => 404,
+            :headers => {:content_type => "application/json; charset=utf-8"})
       end
 
       it "should fail to retrieve resource" do
         expect {
-          github.repos.create_hook user, repo, inputs
+          github.repos.hooks.create user, repo, inputs
         }.to raise_error(Github::Error::NotFound)
       end
     end
-  end # create_hook
+  end # create
 
-  describe "edit_hook" do
+  describe "#edit" do
     let(:hook_id) { 1 }
     let(:inputs) {
       {
@@ -209,45 +205,45 @@ describe Github::Repos::Hooks do
       before do
         stub_patch("/repos/#{user}/#{repo}/hooks/#{hook_id}").
           with(:body => JSON.generate(inputs.except(:unrelated))).
-          to_return(:body => fixture("repos/hook.json"), :status => 200, :headers => { :content_type => "application/json; charset=utf-8"})
+          to_return(:body => fixture("repos/hook.json"), :status => 200,
+            :headers => { :content_type => "application/json; charset=utf-8"})
       end
 
       it "should fail to edit without 'user/repo' parameters" do
-        github.user, github.repo = nil, nil
-        expect { github.repos.edit_hook }.to raise_error(ArgumentError)
+        expect { github.repos.hooks.edit }.to raise_error(ArgumentError)
       end
 
       it "should fail to edit resource without 'name' parameter" do
         expect{
-          github.repos.edit_hook user, repo, inputs.except(:name)
+          github.repos.hooks.edit user, repo, inputs.except(:name)
         }.to raise_error(Github::Error::RequiredParams)
       end
 
       it "should fail to edit resource without 'hook_id'" do
         expect {
-          github.repos.edit_hook user, repo
+          github.repos.hooks.edit user, repo
         }.to raise_error(ArgumentError)
       end
 
       it "should fail to edit resource without 'config' parameter" do
         expect {
-          github.repos.edit_hook user, repo, hook_id, inputs.except(:config)
+          github.repos.hooks.edit user, repo, hook_id, inputs.except(:config)
         }.to raise_error(Github::Error::RequiredParams)
       end
 
       it "should edit the resource" do
-        github.repos.edit_hook user, repo, hook_id, inputs
+        github.repos.hooks.edit user, repo, hook_id, inputs
         a_patch("/repos/#{user}/#{repo}/hooks/#{hook_id}").
           with(inputs).should have_been_made
       end
 
       it "should return resource" do
-        hook = github.repos.edit_hook user, repo, hook_id, inputs
+        hook = github.repos.hooks.edit user, repo, hook_id, inputs
         hook.should be_a Hashie::Mash
       end
 
       it "should be able to retrieve information" do
-        hook = github.repos.edit_hook user, repo, hook_id, inputs
+        hook = github.repos.hooks.edit user, repo, hook_id, inputs
         hook.name.should == 'web'
       end
     end
@@ -255,39 +251,40 @@ describe Github::Repos::Hooks do
     context "failed to edit resource" do
       before do
         stub_patch("/repos/#{user}/#{repo}/hooks/#{hook_id}").with(inputs).
-          to_return(:body => fixture("repos/hook.json"), :status => 404, :headers => { :content_type => "application/json; charset=utf-8"})
+          to_return(:body => fixture("repos/hook.json"), :status => 404,
+            :headers => { :content_type => "application/json; charset=utf-8"})
       end
 
       it "should fail to find resource" do
         expect {
-          github.repos.edit_hook user, repo, hook_id, inputs
+          github.repos.hooks.edit user, repo, hook_id, inputs
         }.to raise_error(Github::Error::NotFound)
       end
     end
-  end # edit_hook
+  end # edit
 
-  describe "delete_hook" do
+  describe "#delete" do
     let(:hook_id) { 1 }
 
     context "resource removed successfully" do
       before do
         stub_delete("/repos/#{user}/#{repo}/hooks/#{hook_id}").
-          to_return(:body => '', :status => 204, :headers => { :content_type => "application/json; charset=utf-8"})
+          to_return(:body => '', :status => 204,
+            :headers => { :content_type => "application/json; charset=utf-8"})
       end
 
       it "should fail to delete without 'user/repo' parameters" do
-        github.user, github.repo = nil, nil
-        expect { github.repos.delete_hook }.to raise_error(ArgumentError)
+        expect { github.repos.hooks.delete }.to raise_error(ArgumentError)
       end
 
       it "should fail to delete resource without 'hook_id'" do
         expect {
-          github.repos.delete_hook user, repo
+          github.repos.hooks.delete user, repo
         }.to raise_error(ArgumentError)
       end
 
       it "should delete the resource" do
-        github.repos.delete_hook user, repo, hook_id
+        github.repos.hooks.delete user, repo, hook_id
         a_delete("/repos/#{user}/#{repo}/hooks/#{hook_id}").should have_been_made
       end
     end
@@ -300,34 +297,34 @@ describe Github::Repos::Hooks do
 
       it "should fail to find resource" do
         expect {
-          github.repos.delete_hook user, repo, hook_id
+          github.repos.hooks.delete user, repo, hook_id
         }.to raise_error(Github::Error::NotFound)
       end
     end
-  end # delete_hook
+  end # delete
 
-  describe "test_hook" do
+  describe "#test" do
     let(:hook_id) { 1 }
 
     context "resource tested successfully" do
       before do
         stub_post("/repos/#{user}/#{repo}/hooks/#{hook_id}/test").
-          to_return(:body => '', :status => 204, :headers => { :content_type => "application/json; charset=utf-8"})
+          to_return(:body => '', :status => 204,
+            :headers => { :content_type => "application/json; charset=utf-8"})
       end
 
       it "should fail to test without 'user/repo' parameters" do
-        github.user, github.repo = nil, nil
-        expect { github.repos.test_hook }.to raise_error(ArgumentError)
+        expect { github.repos.hooks.test }.to raise_error(ArgumentError)
       end
 
       it "should fail to test resource without 'hook_id'" do
         expect {
-          github.repos.test_hook user, repo
+          github.repos.hooks.test user, repo
         }.to raise_error(ArgumentError)
       end
 
       it "should trigger test for the resource" do
-        github.repos.test_hook user, repo, hook_id
+        github.repos.hooks.test user, repo, hook_id
         a_post("/repos/#{user}/#{repo}/hooks/#{hook_id}/test").should have_been_made
       end
     end
@@ -335,15 +332,16 @@ describe Github::Repos::Hooks do
     context "failed to test resource" do
       before do
         stub_post("/repos/#{user}/#{repo}/hooks/#{hook_id}/test").
-          to_return(:body => '', :status => 404, :headers => { :content_type => "application/json; charset=utf-8"})
+          to_return(:body => '', :status => 404,
+            :headers => { :content_type => "application/json; charset=utf-8"})
       end
 
       it "should fail to find resource" do
         expect {
-          github.repos.test_hook user, repo, hook_id
+          github.repos.hooks.test user, repo, hook_id
         }.to raise_error(Github::Error::NotFound)
       end
     end
-  end # test_hook
+  end # test
 
 end # Github::Repos::Hooks
