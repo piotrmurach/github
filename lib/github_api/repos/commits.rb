3b5ed066a52f07780497d4588bc6d77c7cbd6909
Piotr Murach <pmurach@gmail.com>
2012-03-24 10:32:48 +0000

    Change repository commits api method declarations.

--- lib/github_api/repos/commits.rb
+++ lib/github_api/repos/commits.rb
@@ -1,8 +1,7 @@
 # encoding: utf-8
 
 module Github
-  class Repos
-    module Commits
+  class Repos::Commits < API
 
       REQUIRED_COMMENT_PARAMS = %w[
         body
@@ -22,8 +21,8 @@ module Github
       # * <tt>:position</tt> - Required number - Line index in the diff to comment on.
       #
       # = Examples
-      #  @github = Github.new
-      #  @github.repos.create_comment 'user-name', 'repo-name', 'sha-key', 
+      #  github = Github.new
+      #  github.repos.commits.create_comment 'user-name', 'repo-name', 'sha-key', 
       #    "body" => "Nice change",
       #    "commit_id" => "6dcb09b5b57875f334f61aebed695e2e4193db5e",
       #    "line" => 1,
@@ -40,15 +39,14 @@ module Github
 
         _validate_inputs(REQUIRED_COMMENT_PARAMS, params)
 
-        post("/repos/#{user}/#{repo}/commits/#{sha}/comments", params)
+        post_request("/repos/#{user}/#{repo}/commits/#{sha}/comments", params)
       end
-      alias :create_commit_comment :create_comment
 
       # Deletes a commit comment
       #
       # = Examples
-      #  @github = Github.new
-      #  @github.repos.delete_comment 'user-name', 'repo-name', 'comment-id'
+      #  github = Github.new
+      #  github.repos.commits.delete_comment 'user-name', 'repo-name', 'comment-id'
       #
       def delete_comment(user_name, repo_name, comment_id, params={})
         _update_user_repo_params(user_name, repo_name)
@@ -56,9 +54,8 @@ module Github
         _validate_presence_of comment_id
         _normalize_params_keys(params)
 
-        delete("/repos/#{user}/#{repo}/comments/#{comment_id}", params)
+        delete_request("/repos/#{user}/#{repo}/comments/#{comment_id}", params)
       end
-      alias :delete_commit_comment :delete_comment
 
       # List commits on a repository
       #
@@ -67,37 +64,35 @@ module Github
       # * <tt>:path</tt>  Optional string. Only commits containing this file path will be returned
       #
       # = Examples
-      #  @github = Github.new
-      #  @github.repos.commits 'user-name', 'repo-name', :sha => '...'
-      #  @github.repos.commits 'user-name', 'repo-name', :sha => '...' { |commit| ... }
+      #  github = Github.new
+      #  github.repos.commits.list 'user-name', 'repo-name', :sha => '...'
+      #  github.repos.commits.list 'user-name', 'repo-name', :sha => '...' { |commit| ... }
       #
-      def commits(user_name=nil, repo_name=nil, params={})
+      def list(user_name, repo_name, params={})
         _update_user_repo_params(user_name, repo_name)
         _validate_user_repo_params(user, repo) unless user? && repo?
         _normalize_params_keys(params)
         _filter_params_keys(%w[ sha path], params)
 
-        response = get("/repos/#{user}/#{repo}/commits", params)
+        response = get_request("/repos/#{user}/#{repo}/commits", params)
         return response unless block_given?
         response.each { |el| yield el }
       end
-      alias :list_commits :commits
-      alias :list_repo_commits :commits
-      alias :list_repository_commits :commits
+      alias :all :list
 
       # List commit comments for a repository
       #
       # = Examples
-      #  @github = Github.new
-      #  @github.repos.repo_comments 'user-name', 'repo-name'
-      #  @github.repos.repo_comments 'user-name', 'repo-name' { |com| ... }
+      #  github = Github.new
+      #  github.repos.commits.repo_comments 'user-name', 'repo-name'
+      #  github.repos.commits.repo_comments 'user-name', 'repo-name' { |com| ... }
       #
-      def repo_comments(user_name=nil, repo_name=nil, params={})
+      def repo_comments(user_name, repo_name, params={})
         _update_user_repo_params(user_name, repo_name)
         _validate_user_repo_params(user, repo) unless user? && repo?
         _normalize_params_keys(params)
 
-        response = get("/repos/#{user}/#{repo}/comments", params)
+        response = get_request("/repos/#{user}/#{repo}/comments", params)
         return response unless block_given?
         response.each { |el| yield el }
       end
@@ -107,8 +102,8 @@ module Github
       # List comments for a single commit
       #
       # = Examples
-      #  @github = Github.new
-      #  @github.repos.commit_comments 'user-name', 'repo-name', '6dcb09b5b57875f334f61aebed695e2e4193db5e'
+      #  github = Github.new
+      #  github.repos.commits.commit_comments 'user-name', 'repo-name', '6dcb09b5b57875f334f61aebed695e2e4193db5e'
       #
       def commit_comments(user_name, repo_name, sha, params={})
         _update_user_repo_params(user_name, repo_name)
@@ -116,7 +111,7 @@ module Github
         _validate_presence_of sha
         _normalize_params_keys(params)
 
-        response = get("/repos/#{user}/#{repo}/commits/#{sha}/comments", params)
+        response = get_request("/repos/#{user}/#{repo}/commits/#{sha}/comments", params)
         return response unless block_given?
         response.each { |el| yield el }
       end
@@ -126,23 +121,23 @@ module Github
       #
       # = Examples
       #  @github = Github.new
-      #  @github.repos.commit 'user-name', 'repo-name', '6dcb09b5b57875f334f61aebed6')
+      #  @github.repos.commits.get 'user-name', 'repo-name', '6dcb09b5b57875f334f61aebed6')
       #
-      def commit(user_name, repo_name, sha, params={})
+      def get(user_name, repo_name, sha, params={})
         _update_user_repo_params(user_name, repo_name)
         _validate_user_repo_params(user, repo) unless user? && repo?
         _validate_presence_of sha
         _normalize_params_keys(params)
 
-        get("/repos/#{user}/#{repo}/commits/#{sha}", params)
+        get_request("/repos/#{user}/#{repo}/commits/#{sha}", params)
       end
-      alias :get_commit :commit
+      alias :find :get
 
       # Gets a single commit comment
       #
       # = Examples
-      #  @github = Github.new
-      #  @github.repos.commit_comment 'user-name', 'repo-name', 'comment-id'
+      #  github = Github.new
+      #  github.repos.commits.commit_comment 'user-name', 'repo-name', 'comment-id'
       #
       def commit_comment(user_name, repo_name, comment_id, params={})
         _update_user_repo_params(user_name, repo_name)
@@ -150,7 +145,7 @@ module Github
         _validate_presence_of comment_id
         _normalize_params_keys(params)
 
-        get("/repos/#{user}/#{repo}/comments/#{comment_id}", params)
+        get_request("/repos/#{user}/#{repo}/comments/#{comment_id}", params)
       end
       alias :get_commit_comment :commit_comment
 
@@ -160,9 +155,9 @@ module Github
       # * <tt>:body</tt> - Required string.
       #
       # = Examples
-      #  @github = Github.new
-      #  @github.repos.update_comment 'user-name', 'repo-name', 'comment-id',
-      #    "body" => "Nice change"
+      #  github = Github.new
+      #  github.repos.commits.update_comment 'user-name', 'repo-name',
+      #    'comment-id', "body" => "Nice change"
       #
       def update_comment(user_name, repo_name, comment_id, params={})
         _update_user_repo_params(user_name, repo_name)
@@ -172,9 +167,8 @@ module Github
         _normalize_params_keys(params)
         _validate_inputs(%w[ body ], params)
 
-        patch("/repos/#{user}/#{repo}/comments/#{comment_id}", params)
+        patch_request("/repos/#{user}/#{repo}/comments/#{comment_id}", params)
       end
 
-    end # Commits
-  end # Repos
+  end # Repos::Commits
 end # Github
--- spec/github/repos/commits_spec.rb
+++ spec/github/repos/commits_spec.rb
@@ -7,41 +7,46 @@ describe Github::Repos::Commits do
 
   after { github.user, github.repo, github.oauth_token = nil, nil, nil }
 
-  describe "commits" do
+  describe "#list" do
+    it { github.repos.commits.should respond_to :all }
+
     context "resource found" do
       before do
         stub_get("/repos/#{user}/#{repo}/commits").
-          to_return(:body => fixture('repos/commits.json'), :status => 200, :headers => {:content_type => "application/json; charset=utf-8"})
+          to_return(:body => fixture('repos/commits.json'),
+            :status => 200,
+            :headers => {:content_type => "application/json; charset=utf-8"})
       end
 
       it "should fail to get resource without username" do
-        expect { github.repos.commits }.to raise_error(ArgumentError)
+        expect { github.repos.commits.list }.to raise_error(ArgumentError)
       end
 
       it "should get the resources" do
-        github.repos.commits user, repo
+        github.repos.commits.list user, repo
         a_get("/repos/#{user}/#{repo}/commits").should have_been_made
       end
 
       it "should return array of resources" do
-        commits = github.repos.commits user, repo
+        commits = github.repos.commits.list user, repo
         commits.should be_an Array
         commits.should have(1).items
       end
 
       it "should be a mash type" do
-        commits = github.repos.commits user, repo
+        commits = github.repos.commits.list user, repo
         commits.first.should be_a Hashie::Mash
       end
 
       it "should get commit information" do
-        commits = github.repos.commits user, repo
+        commits = github.repos.commits.list user, repo
         commits.first.author.name.should == 'Scott Chacon'
       end
 
       it "should yield to a block" do
-        github.repos.should_receive(:commits).with(user, repo).and_yield('web')
-        github.repos.commits(user, repo) { |param| 'web' }
+        github.repos.commits.should_receive(:list).
+          with(user, repo).and_yield('web')
+        github.repos.commits.list(user, repo) { |param| 'web' }
       end
     end
 
@@ -53,14 +58,16 @@ describe Github::Repos::Commits do
 
       it "should return 404 with a message 'Not Found'" do
         expect {
-          github.repos.commits user, repo
+          github.repos.commits.list user, repo
         }.to raise_error(Github::Error::NotFound)
       end
     end
-  end # commits
+  end # list
 
-  describe "commit" do
+  describe "#get" do
     let(:sha) { '23432dfosfsufd' }
+
+    it { github.repos.commits.should respond_to :find }
 
     context "resource found" do
       before do
@@ -70,22 +77,22 @@ describe Github::Repos::Commits do
 
       it "should fail to get resource without sha key" do
         expect {
-          github.repos.commit(user, repo, nil)
+          github.repos.commits.get user, repo, nil
         }.to raise_error(ArgumentError)
       end
 
       it "should get the resource" do
-        github.repos.commit user, repo, sha
+        github.repos.commits.get user, repo, sha
         a_get("/repos/#{user}/#{repo}/commits/#{sha}").should have_been_made
       end
 
       it "should get commit information" do
-        commit = github.repos.commit user, repo, sha
+        commit = github.repos.commits.get user, repo, sha
         commit.commit.author.name.should == 'Monalisa Octocat'
       end
 
       it "should return mash" do
-        commit = github.repos.commit user, repo, sha
+        commit = github.repos.commits.get user, repo, sha
         commit.should be_a Hashie::Mash
       end
     end
@@ -98,11 +105,11 @@ describe Github::Repos::Commits do
 
       it "should fail to retrive resource" do
         expect {
-          github.repos.commit user, repo, sha
+          github.repos.commits.get user, repo, sha
         }.to raise_error(Github::Error::NotFound)
       end
     end
-  end # commit
+  end # get
 
   describe "commit comments" do
     context "resource found" do
@@ -112,33 +119,36 @@ describe Github::Repos::Commits do
       end
 
       it "should fail to get resource without username" do
-        expect { github.repos.repo_comments }.to raise_error(ArgumentError)
+        expect {
+          github.repos.commits.repo_comments
+        }.to raise_error(ArgumentError)
       end
 
       it "should get the resources" do
-        github.repos.repo_comments user, repo
+        github.repos.commits.repo_comments user, repo
         a_get("/repos/#{user}/#{repo}/comments").should have_been_made
       end
 
       it "should return array of resources" do
-        repo_comments = github.repos.repo_comments user, repo
+        repo_comments = github.repos.commits.repo_comments user, repo
         repo_comments.should be_an Array
         repo_comments.should have(1).items
       end
 
       it "should be a mash type" do
-        repo_comments = github.repos.repo_comments user, repo
+        repo_comments = github.repos.commits.repo_comments user, repo
         repo_comments.first.should be_a Hashie::Mash
       end
 
       it "should get commit comment information" do
-        repo_comments = github.repos.repo_comments user, repo
+        repo_comments = github.repos.commits.repo_comments user, repo
         repo_comments.first.user.login.should == 'octocat'
       end
 
       it "should yield to a block" do
-        github.repos.should_receive(:repo_comments).with(user, repo).and_yield('web')
-        github.repos.repo_comments(user, repo) { |param| 'web' }
+        github.repos.commits.should_receive(:repo_comments).
+          with(user, repo).and_yield('web')
+        github.repos.commits.repo_comments(user, repo) { |param| 'web' }
       end
     end
 
@@ -150,11 +160,11 @@ describe Github::Repos::Commits do
 
       it "should return 404 with a message 'Not Found'" do
         expect {
-          github.repos.repo_comments user, repo
+          github.repos.commits.repo_comments user, repo
         }.to raise_error(Github::Error::NotFound)
       end
     end
-  end # repo comments
+  end # repo_comments
 
   describe "commit_comments" do
     let(:sha) { '23432dfosfsufd' }
@@ -167,35 +177,36 @@ describe Github::Repos::Commits do
 
       it "should fail to get resource without sha key" do
         expect {
-          github.repos.commit_comments(user, repo, nil)
+          github.repos.commits.commit_comments user, repo, nil
         }.to raise_error(ArgumentError)
       end
 
       it "should get the resource" do
-        github.repos.commit_comments user, repo, sha
+        github.repos.commits.commit_comments user, repo, sha
         a_get("/repos/#{user}/#{repo}/commits/#{sha}/comments").
           should have_been_made
       end
 
       it "should return array of resources" do
-        commit_comments = github.repos.commit_comments user, repo, sha
+        commit_comments = github.repos.commits.commit_comments user, repo, sha
         commit_comments.should be_an Array
         commit_comments.should have(1).items
       end
 
       it "should be a mash type" do
-        commit_comments = github.repos.commit_comments user, repo, sha
+        commit_comments = github.repos.commits.commit_comments user, repo, sha
         commit_comments.first.should be_a Hashie::Mash
       end
 
       it "should get commit comment information" do
-        commit_comments = github.repos.commit_comments user, repo, sha
+        commit_comments = github.repos.commits.commit_comments user, repo, sha
         commit_comments.first.user.login.should == 'octocat'
       end
 
       it "should yield to a block" do
-        github.repos.should_receive(:commit_comments).with(user, repo, sha).and_yield('web')
-        github.repos.commit_comments(user, repo, sha) { |param| 'web' }
+        github.repos.commits.should_receive(:commit_comments).
+          with(user, repo, sha).and_yield('web')
+        github.repos.commits.commit_comments(user, repo, sha) { |param| 'web' }
       end
     end
 
@@ -207,13 +218,13 @@ describe Github::Repos::Commits do
 
       it "should fail to retrive resource" do
         expect {
-          github.repos.commit_comments user, repo, sha
+          github.repos.commits.commit_comments user, repo, sha
         }.to raise_error(Github::Error::NotFound)
       end
     end
   end # commit_comments
 
-  describe "commit_comment" do
+  describe "#commit_comment" do
     let(:comment_id) { 1 }
 
     context "resource found" do
@@ -224,22 +235,22 @@ describe Github::Repos::Commits do
 
       it "should fail to get resource without comment id" do
         expect {
-          github.repos.commit_comment user, repo, nil
+          github.repos.commits.commit_comment user, repo, nil
         }.to raise_error(ArgumentError)
       end
 
       it "should get the resource" do
-        github.repos.commit_comment user, repo, comment_id
+        github.repos.commits.commit_comment user, repo, comment_id
         a_get("/repos/#{user}/#{repo}/comments/#{comment_id}").should have_been_made
       end
 
       it "should get commit comment information" do
-        commit_comment = github.repos.commit_comment user, repo, comment_id
+        commit_comment = github.repos.commits.commit_comment user, repo, comment_id
         commit_comment.user.login.should == 'octocat'
       end
 
       it "should return mash" do
-        commit_comment = github.repos.commit_comment user, repo, comment_id
+        commit_comment = github.repos.commits.commit_comment user, repo, comment_id
         commit_comment.should be_a Hashie::Mash
       end
     end
@@ -252,7 +263,7 @@ describe Github::Repos::Commits do
 
       it "should fail to retrive resource" do
         expect {
-          github.repos.commit_comment user, repo, comment_id
+          github.repos.commits.commit_comment user, repo, comment_id
         }.to raise_error(Github::Error::NotFound)
       end
     end
@@ -276,46 +287,46 @@ describe Github::Repos::Commits do
 
       it "should fail to create resource if 'body' input is missing" do
         expect {
-          github.repos.create_comment user, repo, sha, inputs.except('body')
+          github.repos.commits.create_comment user, repo, sha, inputs.except('body')
         }.to raise_error(Github::Error::RequiredParams)
       end
 
       it "should fail to create resource if 'commit_id' input is missing" do
         expect {
-          github.repos.create_comment user, repo, sha, inputs.except(:commit_id)
+          github.repos.commits.create_comment user, repo, sha, inputs.except(:commit_id)
         }.to raise_error(Github::Error::RequiredParams)
       end
 
       it "should fail to create resource if 'line' input is missing" do
         expect {
-          github.repos.create_comment user, repo, sha, inputs.except(:line)
+          github.repos.commits.create_comment user, repo, sha, inputs.except(:line)
         }.to raise_error(Github::Error::RequiredParams)
       end
 
       it "should fail to create resource if 'path' input is missing" do
         expect {
-          github.repos.create_comment user, repo, sha, inputs.except(:path)
+          github.repos.commits.create_comment user, repo, sha, inputs.except(:path)
         }.to raise_error(Github::Error::RequiredParams)
       end
 
       it "should fail to create resource if 'position' input is missing" do
         expect {
-          github.repos.create_comment user, repo, sha, inputs.except(:position)
+          github.repos.commits.create_comment user, repo, sha, inputs.except(:position)
         }.to raise_error(Github::Error::RequiredParams)
       end
 
       it "should create resource successfully" do
-        github.repos.create_comment user, repo, sha, inputs
+        github.repos.commits.create_comment user, repo, sha, inputs
         a_post("/repos/#{user}/#{repo}/commits/#{sha}/comments").with(inputs).should have_been_made
       end
 
       it "should return the resource" do
-        comment = github.repos.create_comment user, repo, sha, inputs
+        comment = github.repos.commits.create_comment user, repo, sha, inputs
         comment.should be_a Hashie::Mash
       end
 
       it "should get the commit comment information" do
-        comment = github.repos.create_comment user, repo, sha, inputs
+        comment = github.repos.commits.create_comment user, repo, sha, inputs
         comment.user.login.should == 'octocat'
       end
     end
@@ -329,7 +340,7 @@ describe Github::Repos::Commits do
 
       it "should fail to retrieve resource" do
         expect {
-          github.repos.create_comment user, repo, sha, inputs
+          github.repos.commits.create_comment user, repo, sha, inputs
         }.to raise_error(Github::Error::NotFound)
       end
     end
@@ -341,22 +352,24 @@ describe Github::Repos::Commits do
     context "resource deleted successfully" do
       before do
         stub_delete("/repos/#{user}/#{repo}/comments/#{comment_id}").
-          to_return(:body => '', :status => 204, :headers => { :content_type => "application/json; charset=utf-8"})
+          to_return(:body => '', :status => 204,
+            :headers => { :content_type => "application/json; charset=utf-8"})
       end
 
       it "should fail to delete without 'user/repo' parameters" do
-        github.user, github.repo = nil, nil
-        expect { github.repos.delete_comment }.to raise_error(ArgumentError)
+        expect {
+          github.repos.commits.delete_comment
+        }.to raise_error(ArgumentError)
       end
 
       it "should fail to delete resource without 'comment_id'" do
         expect {
-          github.repos.delete_comment user, repo, nil
+          github.repos.commits.delete_comment user, repo, nil
         }.to raise_error(ArgumentError)
       end
 
       it "should delete the resource" do
-        github.repos.delete_comment user, repo, comment_id
+        github.repos.commits.delete_comment user, repo, comment_id
         a_delete("/repos/#{user}/#{repo}/comments/#{comment_id}").should have_been_made
       end
     end
@@ -370,41 +383,42 @@ describe Github::Repos::Commits do
 
       it "should fail to find resource" do
         expect {
-          github.repos.delete_comment user, repo, comment_id
+          github.repos.commits.delete_comment user, repo, comment_id
         }.to raise_error(Github::Error::NotFound)
       end
     end
   end # delete_comment
 
-  describe "update_comment" do
+  describe "#update_comment" do
     let(:comment_id) { 1 }
     let(:inputs) { {'body'=> 'web'} }
 
     context "resouce created" do
       before do
         stub_patch("/repos/#{user}/#{repo}/comments/#{comment_id}").with(inputs).
-          to_return(:body => fixture('repos/commit_comment.json'), :status => 200, :headers => {:content_type => "application/json; charset=utf-8"})
-
+          to_return(:body => fixture('repos/commit_comment.json'),
+            :status => 200,
+            :headers => {:content_type => "application/json; charset=utf-8"})
       end
 
       it "should fail to create resource if 'body' input is missing" do
         expect {
-          github.repos.update_comment user, repo, comment_id, inputs.except('body')
+          github.repos.commits.update_comment user, repo, comment_id, inputs.except('body')
         }.to raise_error(Github::Error::RequiredParams)
       end
 
       it "should create resource successfully" do
-        github.repos.update_comment user, repo, comment_id, inputs
+        github.repos.commits.update_comment user, repo, comment_id, inputs
         a_patch("/repos/#{user}/#{repo}/comments/#{comment_id}").with(inputs).should have_been_made
       end
 
       it "should return the resource" do
-        comment = github.repos.update_comment user, repo, comment_id, inputs
+        comment = github.repos.commits.update_comment user, repo, comment_id, inputs
         comment.should be_a Hashie::Mash
       end
 
       it "should get the commit comment information" do
-        comment = github.repos.update_comment user, repo, comment_id, inputs
+        comment = github.repos.commits.update_comment user, repo, comment_id, inputs
         comment.user.login.should == 'octocat'
       end
     end
@@ -418,7 +432,7 @@ describe Github::Repos::Commits do
 
       it "should fail to retrieve resource" do
         expect {
-          github.repos.update_comment user, repo, comment_id, inputs
+          github.repos.commits.update_comment user, repo, comment_id, inputs
         }.to raise_error(Github::Error::NotFound)
       end
     end
