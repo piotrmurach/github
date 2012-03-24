98587c10ac9e8ba4baf25eec4dfa9cec42f350b4
Piotr Murach <pmurach@gmail.com>
2012-03-24 11:32:35 +0000

    Change repository downloads api method names, update docs.

--- lib/github_api/repos/downloads.rb
+++ lib/github_api/repos/downloads.rb
@@ -1,154 +1,151 @@
 # encoding: utf-8
 
 module Github
-  class Repos
-    module Downloads
+  class Repos::Downloads < API
 
-      REQUIRED_PARAMS = %w[ name size ]
+    REQUIRED_PARAMS = %w[ name size ]
 
-      VALID_DOWNLOAD_PARAM_NAMES = %w[
-        name
-        size
-        description
-        content_type
-      ].freeze
+    VALID_DOWNLOAD_PARAM_NAMES = %w[
+      name
+      size
+      description
+      content_type
+    ].freeze
 
-      REQUIRED_S3_PARAMS = %w[
-        path
-        acl
-        name
-        accesskeyid
-        policy
-        signature
-        mime_type
-      ].freeze
+    REQUIRED_S3_PARAMS = %w[
+      path
+      acl
+      name
+      accesskeyid
+      policy
+      signature
+      mime_type
+    ].freeze
 
-      # Status code for successful upload to Amazon S3 service
-      SUCCESS_STATUS = 201
+    # Status code for successful upload to Amazon S3 service
+    SUCCESS_STATUS = 201
 
-      # List downloads for a repository
-      #
-      # = Examples
-      #  @github = Github.new
-      #  @github.repos.downloads 'user-name', 'repo-name'
-      #  @github.repos.downloads 'user-name', 'repo-name' { |downl| ... }
-      #
-      def downloads(user_name=nil, repo_name=nil, params={})
-        _update_user_repo_params(user_name, repo_name)
-        _validate_user_repo_params(user, repo) unless user? && repo?
-        _normalize_params_keys(params)
+    # List downloads for a repository
+    #
+    # = Examples
+    #  github = Github.new
+    #  github.repos.downloads.list 'user-name', 'repo-name'
+    #  github.repos.downloads.list 'user-name', 'repo-name' { |downl| ... }
+    #
+    def list(user_name, repo_name, params={})
+      _update_user_repo_params(user_name, repo_name)
+      _validate_user_repo_params(user, repo) unless user? && repo?
+      _normalize_params_keys(params)
 
-        response = get("/repos/#{user}/#{repo}/downloads", params)
-        return response unless block_given?
-        response.each { |el| yield el }
-      end
-      alias :list_downloads :downloads
-      alias :get_downloads :downloads
+      response = get_request("/repos/#{user}/#{repo}/downloads", params)
+      return response unless block_given?
+      response.each { |el| yield el }
+    end
+    alias :all :list
 
-      # Get a single download
-      #
-      # = Examples
-      #  @github = Github.new
-      #  @github.repos.download 'user-name', 'repo-name'
-      #
-      def download(user_name, repo_name, download_id, params={})
-        _update_user_repo_params(user_name, repo_name)
-        _validate_user_repo_params(user, repo) unless user? && repo?
-        _validate_presence_of download_id
-        _normalize_params_keys(params)
+    # Get a single download
+    #
+    # = Examples
+    #  github = Github.new
+    #  github.repos.downloads.get 'user-name', 'repo-name', 'download-id'
+    #
+    def get(user_name, repo_name, download_id, params={})
+      _update_user_repo_params(user_name, repo_name)
+      _validate_user_repo_params(user, repo) unless user? && repo?
+      _validate_presence_of download_id
+      _normalize_params_keys(params)
 
-        get("/repos/#{user}/#{repo}/downloads/#{download_id}", params)
-      end
-      alias :get_download :download
+      get_request("/repos/#{user}/#{repo}/downloads/#{download_id}", params)
+    end
+    alias :find :get
 
-      # Delete download from a repository
-      #
-      # = Examples
-      #  @github = Github.new
-      #  @github.repos.delete_download 'user-name', 'repo-name', 'download-id'
-      #
-      def delete_download(user_name, repo_name, download_id, params={})
-        _update_user_repo_params(user_name, repo_name)
-        _validate_user_repo_params(user, repo) unless user? && repo?
-        _validate_presence_of download_id
-        _normalize_params_keys(params)
+    # Delete download from a repository
+    #
+    # = Examples
+    #  github = Github.new
+    #  github.repos.downloads.delete 'user-name', 'repo-name', 'download-id'
+    #
+    def delete(user_name, repo_name, download_id, params={})
+      _update_user_repo_params(user_name, repo_name)
+      _validate_user_repo_params(user, repo) unless user? && repo?
+      _validate_presence_of download_id
+      _normalize_params_keys(params)
 
-        delete("/repos/#{user}/#{repo}/downloads/#{download_id}", params)
-      end
+      delete_request("/repos/#{user}/#{repo}/downloads/#{download_id}", params)
+    end
 
-      # Creating a new download is a two step process.
-      # You must first create a new download resource using this method.
-      # Response from this method is to be used in #upload method.
-      #
-      # = Inputs
-      # * <tt>:name</tt> - Required string - name of the file that is being created.
-      # * <tt>:size</tt> - Required number - size of file in bytes.
-      # * <tt>:description</tt> - Optional string
-      # * <tt>:content_type</tt> - Optional string
-      #
-      # = Examples
-      #  @github = Github.new
-      #  @github.repos.create_download 'user-name', 'repo-name',
-      #    "name" =>  "new_file.jpg",
-      #    "size" => 114034,
-      #    "description" => "Latest release",
-      #    "content_type" => "text/plain"
-      #
-      def create_download(user_name=nil, repo_name=nil, params={})
-        _update_user_repo_params(user_name, repo_name)
-        _validate_user_repo_params(user, repo) unless user? && repo?
+    # Creating a new download is a two step process.
+    # You must first create a new download resource using this method.
+    # Response from this method is to be used in #upload method.
+    #
+    # = Inputs
+    # * <tt>:name</tt> - Required string - name of the file that is being created.
+    # * <tt>:size</tt> - Required number - size of file in bytes.
+    # * <tt>:description</tt> - Optional string
+    # * <tt>:content_type</tt> - Optional string
+    #
+    # = Examples
+    #  github = Github.new
+    #  github.repos.downloads.create 'user-name', 'repo-name',
+    #    "name" =>  "new_file.jpg",
+    #    "size" => 114034,
+    #    "description" => "Latest release",
+    #    "content_type" => "text/plain"
+    #
+    def create(user_name, repo_name, params={})
+      _update_user_repo_params(user_name, repo_name)
+      _validate_user_repo_params(user, repo) unless user? && repo?
 
-        _normalize_params_keys(params)
-        _filter_params_keys(VALID_DOWNLOAD_PARAM_NAMES, params)
-        _validate_inputs(REQUIRED_PARAMS, params)
+      _normalize_params_keys(params)
+      _filter_params_keys(VALID_DOWNLOAD_PARAM_NAMES, params)
+      _validate_inputs(REQUIRED_PARAMS, params)
 
-        post("/repos/#{user}/#{repo}/downloads", params)
-      end
+      post_request("/repos/#{user}/#{repo}/downloads", params)
+    end
 
-      # Upload a file to Amazon, using the reponse instance from
-      # Github::Repos::Downloads#create_download. This can be done by passing
-      # the response object as an argument to upload method.
-      #
-      # = Parameters
-      # * <tt>resource</tt> - Required Hashie::Mash -resource of the create_download call
-      # * <tt>:size</tt> - Required number - size of file in bytes.
-      #
-      def upload(resource, filename)
-        _validate_presence_of resource, filename
-        raise ArgumentError, 'Need to provied resource of Github::Repose::Downloads#create_download call' unless resource.is_a? Hashie::Mash
+    # Upload a file to Amazon, using the reponse instance from
+    # Github::Repos::Downloads#create_download. This can be done by passing
+    # the response object as an argument to upload method.
+    #
+    # = Parameters
+    # * <tt>resource</tt> - Required Hashie::Mash -resource of the create_download call
+    # * <tt>:size</tt> - Required number - size of file in bytes.
+    #
+    def upload(resource, filename)
+      _validate_presence_of resource, filename
+      raise ArgumentError, 'Need to provied resource of Github::Repose::Downloads#create_download call' unless resource.is_a? Hashie::Mash
 
-        REQUIRED_S3_PARAMS.each do |key|
-          raise ArgumentError, "Expected following key: #{key}" unless resource.respond_to?(key)
-        end
-
-        # TODO use ordered hash if Ruby < 1.9
-        hash = ruby_18 {
-          require 'active_support'
-          ActiveSupport::OrderedHash.new } || ruby_19 { Hash.new }
-
-        mapped_params = {
-          'key'                   => resource.path,
-          'acl'                   => resource.acl,
-          'success_action_status' => SUCCESS_STATUS,
-          'Filename'              => resource.name,
-          'AWSAccessKeyId'        => resource.accesskeyid,
-          'Policy'                => resource.policy,
-          'Signature'             => resource.signature,
-          'Content-Type'          => resource.mime_type,
-          'file'                  => prepend_at_for(filename.to_s)
-        }
-
-        post('', mapped_params, { :url => resource.s3_url })
-      end
-      alias :upload_to_s3 :upload
-      alias :upload_to_amazon :upload
-
-    private
-
-      def prepend_at_for(file)
-        /^@.*/ =~ file ? '@' + file : file
+      REQUIRED_S3_PARAMS.each do |key|
+        raise ArgumentError, "Expected following key: #{key}" unless resource.respond_to?(key)
       end
 
-    end # Downloads
-  end # Repos
+      # TODO use ordered hash if Ruby < 1.9
+      hash = ruby_18 {
+        require 'active_support'
+        ActiveSupport::OrderedHash.new } || ruby_19 { Hash.new }
+
+      mapped_params = {
+        'key'                   => resource.path,
+        'acl'                   => resource.acl,
+        'success_action_status' => SUCCESS_STATUS,
+        'Filename'              => resource.name,
+        'AWSAccessKeyId'        => resource.accesskeyid,
+        'Policy'                => resource.policy,
+        'Signature'             => resource.signature,
+        'Content-Type'          => resource.mime_type,
+        'file'                  => prepend_at_for(filename.to_s)
+      }
+
+      post('', mapped_params, { :url => resource.s3_url })
+    end
+    alias :upload_to_s3 :upload
+    alias :upload_to_amazon :upload
+
+  private
+
+    def prepend_at_for(file)
+      /^@.*/ =~ file ? '@' + file : file
+    end
+
+  end # Repos::Downloads
 end # Github
--- spec/github/repos/downloads_spec.rb
+++ spec/github/repos/downloads_spec.rb
@@ -13,45 +13,45 @@ describe Github::Repos::Downloads do
   it { described_class::REQUIRED_PARAMS.should_not be_nil }
   it { described_class::REQUIRED_S3_PARAMS.should_not be_nil }
 
-  describe "downloads" do
-    it { github.repos.should respond_to :downloads }
-    it { github.repos.should respond_to :list_downloads }
-    it { github.repos.should respond_to :get_downloads }
+  describe "#list" do
+    it { github.repos.downloads.should respond_to :list }
 
     context "resource found" do
       before do
         stub_get("/repos/#{user}/#{repo}/downloads").
-          to_return(:body => fixture('repos/downloads.json'), :status => 200, :headers => {:content_type => "application/json; charset=utf-8"})
+          to_return(:body => fixture('repos/downloads.json'), :status => 200,
+            :headers => {:content_type => "application/json; charset=utf-8"})
       end
 
       it "should fail to get resource without username" do
-        expect { github.repos.downloads }.to raise_error(ArgumentError)
+        expect { github.repos.downloads.list }.to raise_error(ArgumentError)
       end
 
       it "should get the resources" do
-        github.repos.downloads  user, repo
+        github.repos.downloads.list  user, repo
         a_get("/repos/#{user}/#{repo}/downloads").should have_been_made
       end
 
       it "should return array of resources" do
-        downloads = github.repos.downloads user, repo
+        downloads = github.repos.downloads.list user, repo
         downloads.should be_an Array
         downloads.should have(1).items
       end
 
       it "should be a mash type" do
-        downloads = github.repos.downloads user, repo
+        downloads = github.repos.downloads.list user, repo
         downloads.first.should be_a Hashie::Mash
       end
 
       it "should get download information" do
-        downloads = github.repos.downloads user, repo
+        downloads = github.repos.downloads.list user, repo
         downloads.first.name.should == 'new_file.jpg'
       end
 
       it "should yield to a block" do
-        github.repos.should_receive(:downloads).with(user, repo).and_yield('web')
-        github.repos.downloads(user, repo) { |param| 'web' }
+        github.repos.downloads.should_receive(:list).
+          with(user, repo).and_yield('web')
+        github.repos.downloads.list(user, repo) { |param| 'web' }
       end
     end
 
@@ -63,43 +63,43 @@ describe Github::Repos::Downloads do
 
       it "should return 404 with a message 'Not Found'" do
         expect {
-          github.repos.downloads user, repo
+          github.repos.downloads.list user, repo
         }.to raise_error(Github::Error::NotFound)
       end
     end
-  end # downloads
+  end # list
 
-  describe "download" do
+  describe "#get" do
     let(:download_id) { 1 }
 
-    it { github.repos.should respond_to :download }
-    it { github.repos.should respond_to :get_download }
+    it { github.repos.downloads.should respond_to :find }
 
     context "resource found" do
       before do
         stub_get("/repos/#{user}/#{repo}/downloads/#{download_id}").
-          to_return(:body => fixture('repos/download.json'), :status => 200, :headers => {:content_type => "application/json; charset=utf-8"})
+          to_return(:body => fixture('repos/download.json'), :status => 200,
+            :headers => {:content_type => "application/json; charset=utf-8"})
       end
 
       it "should fail to get resource without download id" do
         expect {
-          github.repos.download(user, repo, nil)
+          github.repos.downloads.get user, repo, nil
         }.to raise_error(ArgumentError)
       end
 
       it "should get the resource" do
-        github.repos.download user, repo, download_id
+        github.repos.downloads.get user, repo, download_id
         a_get("/repos/#{user}/#{repo}/downloads/#{download_id}").should have_been_made
       end
 
       it "should get download information" do
-        download = github.repos.download user, repo, download_id
+        download = github.repos.downloads.get user, repo, download_id
         download.id.should == download_id
         download.name.should == 'new_file.jpg'
       end
 
       it "should return mash" do
-        download = github.repos.download user, repo, download_id
+        download = github.repos.downloads.get user, repo, download_id
         download.should be_a Hashie::Mash
       end
     end
@@ -112,34 +112,34 @@ describe Github::Repos::Downloads do
 
       it "should fail to retrive resource" do
         expect {
-          github.repos.download user, repo, download_id
+          github.repos.downloads.get user, repo, download_id
         }.to raise_error(Github::Error::NotFound)
       end
     end
-  end # download
+  end # get
 
-  describe "delete_download" do
+  describe "#delete" do
     let(:download_id) { 1 }
 
     context "resource edited successfully" do
       before do
         stub_delete("/repos/#{user}/#{repo}/downloads/#{download_id}").
-          to_return(:body => '', :status => 204, :headers => { :content_type => "application/json; charset=utf-8"})
+          to_return(:body => '', :status => 204,
+            :headers => { :content_type => "application/json; charset=utf-8"})
       end
 
       it "should fail to delete without 'user/repo' parameters" do
-        github.user, github.repo = nil, nil
-        expect { github.repos.delete_download }.to raise_error(ArgumentError)
+        expect { github.repos.downloads.delete }.to raise_error(ArgumentError)
       end
 
       it "should fail to delete resource without 'download_id'" do
         expect {
-          github.repos.delete_download user, repo
+          github.repos.downloads.delete user, repo
         }.to raise_error(ArgumentError)
       end
 
       it "should delete the resource" do
-        github.repos.delete_download user, repo, download_id
+        github.repos.downloads.delete user, repo, download_id
         a_delete("/repos/#{user}/#{repo}/downloads/#{download_id}").should have_been_made
       end
     end
@@ -147,19 +147,19 @@ describe Github::Repos::Downloads do
     context "failed to edit resource" do
       before do
         stub_delete("/repos/#{user}/#{repo}/downloads/#{download_id}").
-          to_return(:body => fixture("repos/download.json"), :status => 404, :headers => { :content_type => "application/json; charset=utf-8"})
-
+          to_return(:body => fixture("repos/download.json"), :status => 404,
+            :headers => { :content_type => "application/json; charset=utf-8"})
       end
 
       it "should fail to find resource" do
         expect {
-          github.repos.delete_download user, repo, download_id
+          github.repos.downloads.delete user, repo, download_id
         }.to raise_error(Github::Error::NotFound)
       end
     end
-  end # delete_download
+  end # delete
 
-  describe "create_download" do
+  describe "#create" do
     let(:inputs) { {:name => 'new_file.jpg', :size => 114034, :description => "Latest release", :content_type => 'text/plain'} }
 
     context "resouce created" do
@@ -171,28 +171,28 @@ describe Github::Repos::Downloads do
 
       it "should fail to create resource if 'name' input is missing" do
         expect {
-          github.repos.create_download user, repo, inputs.except(:name)
+          github.repos.downloads.create user, repo, inputs.except(:name)
         }.to raise_error(Github::Error::RequiredParams)
       end
 
       it "should failt to create resource if 'size' input is missing" do
         expect {
-          github.repos.create_download user, repo, inputs.except(:size)
+          github.repos.downloads.create user, repo, inputs.except(:size)
         }.to raise_error(Github::Error::RequiredParams)
       end
 
       it "should create resource successfully" do
-        github.repos.create_download user, repo, inputs
+        github.repos.downloads.create user, repo, inputs
         a_post("/repos/#{user}/#{repo}/downloads").with(inputs).should have_been_made
       end
 
       it "should return the resource" do
-        download = github.repos.create_download user, repo, inputs
+        download = github.repos.downloads.create user, repo, inputs
         download.should be_a Hashie::Mash
       end
 
       it "should get the download information" do
-        download = github.repos.create_download user, repo, inputs
+        download = github.repos.downloads.create user, repo, inputs
         download.name.should == 'new_file.jpg'
       end
     end
@@ -200,16 +200,17 @@ describe Github::Repos::Downloads do
     context "failed to create resource" do
       before do
         stub_post("/repos/#{user}/#{repo}/downloads").with(inputs).
-          to_return(:body => fixture('repos/download_s3.json'), :status => 404, :headers => {:content_type => "application/json; charset=utf-8"})
+          to_return(:body => fixture('repos/download_s3.json'), :status => 404,
+            :headers => {:content_type => "application/json; charset=utf-8"})
       end
 
       it "should faile to retrieve resource" do
         expect {
-          github.repos.create_download(user, repo, inputs)
+          github.repos.downloads.create user, repo, inputs
         }.to raise_error(Github::Error::NotFound)
       end
     end
-  end # create_download
+  end # create
 
   describe 'upload' do
     let(:inputs) do
@@ -228,11 +229,13 @@ describe Github::Repos::Downloads do
       end
 
       it "should fail if resource is of incorrect type" do
-        expect { github.repos.upload file, file }.to raise_error(ArgumentError)
+        expect {
+          github.repos.downloads.upload file, file
+        }.to raise_error(ArgumentError)
       end
 
       it "should upload resource successfully" do
-        github.repos.upload resource, file
+        github.repos.downloads.upload resource, file
         a_post('', 'https://github.s3.amazonaws.com').with(resource).should have_been_made
       end
 
@@ -245,7 +248,7 @@ describe Github::Repos::Downloads do
 
       it "should faile to retrieve resource" do
         expect {
-          github.repos.upload resource, file
+          github.repos.downloads.upload resource, file
         }.to raise_error(Github::Error::NotFound)
       end
     end
