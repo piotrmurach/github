d619d973439e003c6ebd28292ec5445440a0f794
Piotr Murach <pmurach@gmail.com>
2012-04-01 13:33:24 +0100

    Change repos watching api to new dsl.

--- lib/github_api/repos/watching.rb
+++ lib/github_api/repos/watching.rb
@@ -1,95 +1,93 @@
 # encoding: utf-8
 
 module Github
-  class Repos
-    module Watching
+  class Repos::Watching < API
 
-      # List repo watchers
-      #
-      # = Examples
-      #  @github = Github.new :user => 'user-name', :repo => 'repo-name'
-      #  @github.repos.watchers
-      #  @github.repos.watchers { |watcher| ... }
-      #
-      def watchers(user_name, repo_name, params={})
-        _update_user_repo_params(user_name, repo_name)
-        _validate_user_repo_params(user, repo) unless user? && repo?
-        _normalize_params_keys(params)
+    # List repo watchers
+    #
+    # = Examples
+    #  github = Github.new :user => 'user-name', :repo => 'repo-name'
+    #  github.repos.watching.watchers
+    #  github.repos.watching.watchers { |watcher| ... }
+    #
+    def watchers(user_name, repo_name, params={})
+      _update_user_repo_params(user_name, repo_name)
+      _validate_user_repo_params(user, repo) unless user? && repo?
+      _normalize_params_keys(params)
 
-        response = get("/repos/#{user}/#{repo}/watchers", params)
-        return response unless block_given?
-        response.each { |el| yield el }
+      response = get("/repos/#{user}/#{repo}/watchers", params)
+      return response unless block_given?
+      response.each { |el| yield el }
+    end
+
+    # List repos being watched by a user
+    #
+    # = Examples
+    #  github = Github.new
+    #  github.repos.watching.watched :user => 'user-name'
+    #
+    # List repos being watched by the authenticated user
+    #
+    # = Examples
+    #  github = Github.new :oauth_token => '...'
+    #  github.repos.watching.watched
+    #
+    def watched(*args)
+      params = args.last.is_a?(Hash) ? args.pop : {}
+      _normalize_params_keys(params)
+      _merge_user_into_params!(params) unless params.has_key?('user')
+
+      response = if (user_name = params.delete('user'))
+        get("/users/#{user_name}/watched", params)
+      else
+        get("/user/watched", params)
       end
+      return response unless block_given?
+      response.each { |el| yield el }
+    end
 
-      # List repos being watched by a user
-      #
-      # = Examples
-      #  @github = Github.new
-      #  @github.repos.watched :user => 'user-name'
-      #
-      # List repos being watched by the authenticated user
-      #
-      # = Examples
-      #  @github = Github.new :oauth_token => '...'
-      #  @github.repos.watched
-      #
-      def watched(*args)
-        params = args.last.is_a?(Hash) ? args.pop : {}
-        _normalize_params_keys(params)
-        _merge_user_into_params!(params) unless params.has_key?('user')
+    # Check if you are watching a repository
+    #
+    # Returns <tt>true</tt> if this repo is watched by you, <tt>false</tt> otherwise
+    # = Examples
+    #  github = Github.new
+    #  github.repos.watching.watching? 'user-name', 'repo-name'
+    #
+    def watching?(user_name, repo_name, params={})
+      _validate_presence_of user_name, repo_name
+      _normalize_params_keys(params)
+      get("/user/watched/#{user_name}/#{repo_name}", params)
+      true
+    rescue Github::Error::NotFound
+      false
+    end
 
-        response = if (user_name = params.delete('user'))
-          get("/users/#{user_name}/watched", params)
-        else
-          get("/user/watched", params)
-        end
-        return response unless block_given?
-        response.each { |el| yield el }
-      end
+    # Watch a repository
+    #
+    # You need to be authenticated to watch a repository
+    #
+    # = Examples
+    #  github = Github.new
+    #  github.repos.watching.start_watching 'user-name', 'repo-name'
+    #
+    def start_watching(user_name, repo_name, params={})
+      _validate_presence_of user_name, repo_name
+      _normalize_params_keys(params)
+      put_request("/user/watched/#{user_name}/#{repo_name}", params)
+    end
 
-      # Check if you are watching a repository
-      #
-      # Returns <tt>true</tt> if this repo is watched by you, <tt>false</tt> otherwise
-      # = Examples
-      #  @github = Github.new
-      #  @github.repos.watching? 'user-name', 'repo-name'
-      #
-      def watching?(user_name, repo_name, params={})
-        _validate_presence_of user_name, repo_name
-        _normalize_params_keys(params)
-        get("/user/watched/#{user_name}/#{repo_name}", params)
-        true
-      rescue Github::Error::NotFound
-        false
-      end
+    # Stop watching a repository
+    #
+    # You need to be authenticated to stop watching a repository.
+    # = Examples
+    #  github = Github.new
+    #  github.repos.watching.start_watching 'user-name', 'repo-name'
+    #
+    def stop_watching(user_name, repo_name, params={})
+      _validate_presence_of user_name, repo_name
+      _normalize_params_keys(params)
+      delete_request("/user/watched/#{user_name}/#{repo_name}", params)
+    end
 
-      # Watch a repository
-      #
-      # You need to be authenticated to watch a repository
-      #
-      # = Examples
-      #  @github = Github.new
-      #  @github.repos.start_watching 'user-name', 'repo-name'
-      #
-      def start_watching(user_name, repo_name, params={})
-        _validate_presence_of user_name, repo_name
-        _normalize_params_keys(params)
-        put("/user/watched/#{user_name}/#{repo_name}", params)
-      end
-
-      # Stop watching a repository
-      #
-      # You need to be authenticated to stop watching a repository.
-      # = Examples
-      #  @github = Github.new
-      #  @github.repos.start_watching 'user-name', 'repo-name'
-      #
-      def stop_watching(user_name, repo_name, params={})
-        _validate_presence_of user_name, repo_name
-        _normalize_params_keys(params)
-        delete("/user/watched/#{user_name}/#{repo_name}", params)
-      end
-
-    end # Watching
-  end # Repos
+  end # Repos::Watching
 end # Github
--- spec/github/repos/watching_spec.rb
+++ spec/github/repos/watching_spec.rb
@@ -11,38 +11,41 @@ describe Github::Repos::Watching do
 
   describe "watchers" do
     before do
-      github.oauth_token = nil
       stub_get("/repos/#{user}/#{repo}/watchers").
-        to_return(:body => fixture("repos/watchers.json"), :status => 200, :headers => {})
+        to_return(:body => fixture("repos/watchers.json"),
+                  :status => 200, :headers => {})
     end
 
     it "should fail to get resource without username" do
-      expect { github.repos.watchers }.to raise_error(ArgumentError)
+      expect {
+        github.repos.watching.watchers
+      }.to raise_error(ArgumentError)
     end
 
     it "should yield iterator if block given" do
-      github.repos.should_receive(:watchers).with(user, repo).and_yield('github')
-      github.repos.watchers(user, repo) { |param| 'github' }
+      github.repos.watching.should_receive(:watchers).
+        with(user, repo).and_yield('github')
+      github.repos.watching.watchers(user, repo) { |param| 'github' }
     end
 
     it "should get the resources" do
-      github.repos.watchers(user, repo)
+      github.repos.watching.watchers user, repo
       a_get("/repos/#{user}/#{repo}/watchers").should have_been_made
     end
 
     it "should return array of resources" do
-      watchers = github.repos.watchers(user, repo)
+      watchers = github.repos.watching.watchers user, repo
       watchers.should be_an Array
       watchers.should have(1).items
     end
 
     it "should return result of mash type" do
-      watchers = github.repos.watchers user, repo
+      watchers = github.repos.watching.watchers user, repo
       watchers.first.should be_a Hashie::Mash
     end
 
     it "should get watcher information" do
-      watchers = github.repos.watchers(user, repo)
+      watchers = github.repos.watching.watchers user, repo
       watchers.first.login.should == 'octocat'
     end
 
@@ -54,7 +57,7 @@ describe Github::Repos::Watching do
 
       it "should return 404 not found message" do
         expect {
-          github.repos.watchers(user, repo)
+          github.repos.watching.watchers user, repo
         }.to raise_error(Github::Error::NotFound)
       end
     end
@@ -62,20 +65,18 @@ describe Github::Repos::Watching do
 
   describe "#watched" do
     context "if user unauthenticated" do
-      before { github.oauth_token = nil }
-
       it "should fail to get resource without username " do
         stub_get("/user/watched").
           to_return(:body => fixture("repos/watched.json"), :status => 401, :headers => {})
         expect {
-          github.repos.watched
+          github.repos.watching.watched
         }.to raise_error(Github::Error::Unauthorized)
       end
 
       it "should get the resource with username" do
         stub_get("/users/#{user}/watched").
           to_return(:body => fixture("repos/watched.json"), :status => 200, :headers => {})
-        github.repos.watched(:user => user)
+        github.repos.watching.watched :user => user
         a_get("/users/#{user}/watched").should have_been_made
       end
     end
@@ -88,22 +89,20 @@ describe Github::Repos::Watching do
           to_return(:body => fixture("repos/watched.json"), :status => 200, :headers => {})
       end
 
-      after { github.oauth_token = nil }
-
       it "should get the resources" do
-        github.repos.watched
+        github.repos.watching.watched
         a_get("/user/watched").with(:query => {:access_token => OAUTH_TOKEN}).
           should have_been_made
       end
 
       it "should return array of resources" do
-        watched = github.repos.watched
+        watched = github.repos.watching.watched
         watched.should be_an Array
         watched.should have(1).items
       end
 
       it "should get watched information" do
-        watched = github.repos.watched
+        watched = github.repos.watching.watched
         watched.first.name.should == 'Hello-World'
         watched.first.owner.login.should == 'octocat'
       end
@@ -115,25 +114,28 @@ describe Github::Repos::Watching do
       context "this repo is being watched by the user"
         before do
           stub_get("/user/watched/#{user}/#{repo}").
-            to_return(:body => "", :status => 404, :headers => {:user_agent => github.user_agent})
+            to_return(:body => "", :status => 404,
+                      :headers => {:user_agent => github.user_agent})
         end
 
       it "should return false if resource not found" do
-        watching = github.repos.watching? user, repo
+        watching = github.repos.watching.watching? user, repo
         watching.should be_false
       end
 
       it "should return true if resoure found" do
           stub_get("/user/watched/#{user}/#{repo}").
             to_return(:body => "", :status => 200, :headers => {:user_agent => github.user_agent})
-        watching = github.repos.watching? user, repo
+        watching = github.repos.watching.watching? user, repo
         watching.should be_true
       end
     end
 
     context "without username and reponame passed" do
       it "should fail validation " do
-        expect { github.repos.watching?(nil, nil) }.to raise_error(ArgumentError)
+        expect {
+          github.repos.watching.watching?(nil, nil)
+        }.to raise_error(ArgumentError)
       end
     end
   end # watching?
@@ -148,10 +150,8 @@ describe Github::Repos::Watching do
             to_return(:body => "", :status => 204, :headers => {})
         end
 
-        after { github.oauth_token = nil }
-
         it "should successfully watch a repo" do
-          github.repos.start_watching(user, repo)
+          github.repos.watching.start_watching user, repo
           a_put("/user/watched/#{user}/#{repo}").
             with(:query => {:access_token => OAUTH_TOKEN}).
             should have_been_made
@@ -161,11 +161,10 @@ describe Github::Repos::Watching do
 
     context "user unauthenticated" do
       it "should fail" do
-        github.oauth_token = nil
         stub_put("/user/watched/#{user}/#{repo}").
           to_return(:body => "", :status => 401, :headers => {})
         expect {
-          github.repos.start_watching(user, repo)
+          github.repos.watching.start_watching user, repo
         }.to raise_error(Github::Error::Unauthorized)
       end
     end
@@ -175,26 +174,25 @@ describe Github::Repos::Watching do
     context "user authenticated" do
       context "with correct information" do
         before do
-          github.user, github.repo = nil, nil
           github.oauth_token = OAUTH_TOKEN
           stub_delete("/user/watched/#{user}/#{repo}?access_token=#{OAUTH_TOKEN}").
             to_return(:body => "", :status => 204, :headers => {})
         end
 
         it "should successfully watch a repo" do
-          github.repos.stop_watching(user, repo)
-          a_delete("/user/watched/#{user}/#{repo}?access_token=#{OAUTH_TOKEN}").should have_been_made
+          github.repos.watching.stop_watching user, repo
+          a_delete("/user/watched/#{user}/#{repo}?access_token=#{OAUTH_TOKEN}").
+            should have_been_made
         end
       end
     end
 
     context "user unauthenticated" do
       it "should fail" do
-        github.oauth_token = nil
         stub_delete("/user/watched/#{user}/#{repo}").
           to_return(:body => "", :status => 401, :headers => {})
         expect {
-          github.repos.stop_watching(user, repo)
+          github.repos.watching.stop_watching(user, repo)
         }.to raise_error(Github::Error::Unauthorized)
       end
     end
