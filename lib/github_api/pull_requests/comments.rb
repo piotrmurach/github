ffaa858e6f6a49e980747cf80042c1e13602fa90
Piotr Murach <pmurach@gmail.com>
2012-04-06 21:05:54 +0100

    Change pull comments api to new dsl, update docs.

--- lib/github_api/pull_requests/comments.rb
+++ lib/github_api/pull_requests/comments.rb
@@ -1,149 +1,146 @@
 # encoding: utf-8
 
 module Github
-  class PullRequests
-    module Comments
+  class PullRequests::Comments < API
 
-      VALID_REQUEST_COM_PARAM_NAMES = %w[
-        body
-        commit_id
-        path
-        position
-        in_reply_to
-        mime_type
-        resource
-      ].freeze
+    VALID_REQUEST_COM_PARAM_NAMES = %w[
+      body
+      commit_id
+      path
+      position
+      in_reply_to
+      mime_type
+      resource
+    ].freeze
 
-      # List comments on a pull request
-      #
-      # = Examples
-      #  @github = Github.new
-      #  @github.pull_requests.comments 'user-name', 'repo-name', 'request-id'
-      #
-      def comments(user_name, repo_name, request_id, params={})
-        _update_user_repo_params(user_name, repo_name)
-        _validate_user_repo_params(user, repo) unless user? && repo?
-        _validate_presence_of request_id
+    # List comments on a pull request
+    #
+    # = Examples
+    #  github = Github.new
+    #  github.pull_requests.comments.list 'user-name', 'repo-name', 'request-id'
+    #
+    def list(user_name, repo_name, request_id, params={})
+      _update_user_repo_params(user_name, repo_name)
+      _validate_user_repo_params(user, repo) unless user? && repo?
+      _validate_presence_of request_id
 
-        _normalize_params_keys(params)
-        # _merge_mime_type(:pull_comment, params)
+      _normalize_params_keys(params)
+      # _merge_mime_type(:pull_comment, params)
 
-        response = get("/repos/#{user}/#{repo}/pulls/#{request_id}/comments", params)
-        return response unless block_given?
-        response.each { |el| yield el }
+      response = get_request("/repos/#{user}/#{repo}/pulls/#{request_id}/comments", params)
+      return response unless block_given?
+      response.each { |el| yield el }
+    end
+    alias :all :list
+
+    # Get a single comment for pull requests
+    # = Examples
+    #  github = Github.new
+    #  github.pull_requests.comments.get 'user-name', 'repo-name', 'comment-id'
+    #
+    def get(user_name, repo_name, comment_id, params={})
+      _update_user_repo_params(user_name, repo_name)
+      _validate_user_repo_params(user, repo) unless user? && repo?
+      _validate_presence_of comment_id
+
+      _normalize_params_keys(params)
+      # _merge_mime_type(:pull_comment, params)
+
+      get_request("/repos/#{user}/#{repo}/pulls/comments/#{comment_id}", params)
+    end
+    alias :find :get
+
+    # Create a pull request comment
+    #
+    # = Inputs
+    # * <tt>:body</tt> - Required string
+    # * <tt>:commit_id</tt> - Required string - sha of the commit to comment on.
+    # * <tt>:path</tt> - Required string - Relative path of the file to comment on.
+    # * <tt>:position</tt> - Required number - Line index in the diff to comment on
+    #
+    # = Examples
+    #  github = Github.new
+    #  github.pull_requests.comments.create 'user-name','repo-name','request-id',
+    #   "body" => "Nice change",
+    #   "commit_id" => "6dcb09b5b57875f334f61aebed695e2e4193db5e",
+    #   "path" => "file1.txt",
+    #   "position" => 4
+    #
+    # = Alternative Inputs
+    # Instead of passing commit_id, path, and position you can reply to
+    # an existing Pull Request Comment like this
+    # * <tt>:body</tt> - Required string
+    # * <tt>:in_reply_to</tt> - Required number - comment id to reply to.
+    #
+    # = Examples
+    #  github = Github.new
+    #  github.pull_requests.comments.create 'user-name','repo-name','request-id',
+    #    "body" => "Nice change",
+    #    "in_reply_to" => 4
+    #
+    def create(user_name, repo_name, request_id, params={})
+      _update_user_repo_params(user_name, repo_name)
+      _validate_user_repo_params(user, repo) unless user? && repo?
+      _validate_presence_of request_id
+
+      _normalize_params_keys(params)
+      # _merge_mime_type(:pull_comment, params)
+      _filter_params_keys(VALID_REQUEST_COM_PARAM_NAMES, params)
+      # _validate_reply_to(params)
+
+      post_request("/repos/#{user}/#{repo}/pulls/#{request_id}/comments", params)
+    end
+
+    # Edit a pull request comment
+    #
+    # = Inputs
+    # * <tt>:body</tt> - Required string
+    #
+    # = Examples
+    #  github = Github.new
+    #  github.pull_requests.comments.edit 'user-name', 'repo-name','comment-id',
+    #    "body" => "Nice change"
+    #
+    def edit(user_name, repo_name, comment_id, params={})
+      _update_user_repo_params(user_name, repo_name)
+      _validate_user_repo_params(user, repo) unless user? && repo?
+      _validate_presence_of comment_id
+
+      _normalize_params_keys(params)
+      # _merge_mime_type(:pull_comment, params)
+      _filter_params_keys(VALID_REQUEST_COM_PARAM_NAMES, params)
+
+      patch_request("/repos/#{user}/#{repo}/pulls/comments/#{comment_id}", params)
+    end
+
+    # Delete a pull request comment
+    #
+    # = Examples
+    #  github = Github.new
+    #  github.pull_requests.comments.delete 'user-name', 'repo-name','comment-id'
+    #
+    def delete(user_name, repo_name, comment_id, params={})
+      _update_user_repo_params(user_name, repo_name)
+      _validate_user_repo_params(user, repo) unless user? && repo?
+      _validate_presence_of comment_id
+
+      _normalize_params_keys(params)
+      # _merge_mime_type(:pull_comment, params)
+
+      delete_request("/repos/#{user}/#{repo}/pulls/comments/#{comment_id}", params)
+    end
+
+  private
+
+    # To let user know that the params supplied are wrong before request is made
+    def _validate_reply_to(params)
+      if params['in_reply_to'] && !_validate_inputs(%w[ body in_reply_to ], params)
+        raise ArgumentError, "Required params are: #{%w[ body in_reply_to].join(',')}"
+
+      elsif !_validate_inputs(VALID_REQUEST_COM_PARAM_NAMES - %w[ in_reply_to ], params)
+        raise ArgumentError, "Required params are: #{VALID_REQUEST_COM_PARAM_NAMES.join(', ')}"
       end
-      alias :request_comments :comments
+    end
 
-      # Get a single comment for pull requests
-      # = Examples
-      #  @github = Github.new
-      #  @github.pull_requests.comment 'user-name', 'repo-name', 'comment-id'
-      #
-      def comment(user_name, repo_name, comment_id, params={})
-        _update_user_repo_params(user_name, repo_name)
-        _validate_user_repo_params(user, repo) unless user? && repo?
-        _validate_presence_of comment_id
-
-        _normalize_params_keys(params)
-        # _merge_mime_type(:pull_comment, params)
-
-        get("/repos/#{user}/#{repo}/pulls/comments/#{comment_id}", params)
-      end
-      alias :request_comment :comment
-      alias :get_comment :comment
-
-      # Create a pull request comment
-      #
-      # = Inputs
-      # * <tt>:body</tt> - Required string
-      # * <tt>:commit_id</tt> - Required string - sha of the commit to comment on.
-      # * <tt>:path</tt> - Required string - Relative path of the file to comment on.
-      # * <tt>:position</tt> - Required number - Line index in the diff to comment on
-      #
-      # = Examples
-      #  @github = Github.new
-      #  @github.pull_requests.create_comment 'user-name','repo-name','request-id',
-      #   "body" => "Nice change",
-      #   "commit_id" => "6dcb09b5b57875f334f61aebed695e2e4193db5e",
-      #   "path" => "file1.txt",
-      #   "position" => 4
-      #
-      # = Alternative Inputs
-      # Instead of passing commit_id, path, and position you can reply to
-      # an existing Pull Request Comment like this
-      # * <tt>:body</tt> - Required string
-      # * <tt>:in_reply_to</tt> - Required number - comment id to reply to.
-      #
-      # = Examples
-      #  @github = Github.new
-      #  @github.pull_requests.create_comment 'user-name','repo-name','request-id',
-      #    "body" => "Nice change",
-      #    "in_reply_to" => 4
-      #
-      def create_comment(user_name, repo_name, request_id, params={})
-        _update_user_repo_params(user_name, repo_name)
-        _validate_user_repo_params(user, repo) unless user? && repo?
-        _validate_presence_of request_id
-
-        _normalize_params_keys(params)
-        # _merge_mime_type(:pull_comment, params)
-        _filter_params_keys(VALID_REQUEST_COM_PARAM_NAMES, params)
-        # _validate_reply_to(params)
-
-        post("/repos/#{user}/#{repo}/pulls/#{request_id}/comments", params)
-      end
-
-      # Edit a pull request comment
-      #
-      # = Inputs
-      # * <tt>:body</tt> - Required string
-      #
-      # = Examples
-      #  @github = Github.new
-      #  @github.pull_requests.edit_comment 'user-name', 'repo-name','comment-id',
-      #    "body" => "Nice change"
-      #
-      def edit_comment(user_name, repo_name, comment_id, params={})
-        _update_user_repo_params(user_name, repo_name)
-        _validate_user_repo_params(user, repo) unless user? && repo?
-        _validate_presence_of comment_id
-
-        _normalize_params_keys(params)
-        # _merge_mime_type(:pull_comment, params)
-        _filter_params_keys(VALID_REQUEST_COM_PARAM_NAMES, params)
-
-        patch("/repos/#{user}/#{repo}/pulls/comments/#{comment_id}", params)
-      end
-
-      # Delete a pull request comment
-      #
-      # = Examples
-      #  @github = Github.new
-      #  @github.pull_requests.delete_comment 'user-name', 'repo-name','comment-id'
-      #
-      def delete_comment(user_name, repo_name, comment_id, params={})
-        _update_user_repo_params(user_name, repo_name)
-        _validate_user_repo_params(user, repo) unless user? && repo?
-        _validate_presence_of comment_id
-
-        _normalize_params_keys(params)
-        # _merge_mime_type(:pull_comment, params)
-
-        delete("/repos/#{user}/#{repo}/pulls/comments/#{comment_id}", params)
-      end
-
-    private
-
-      # To let user know that the params supplied are wrong before request is made
-      def _validate_reply_to(params)
-        if params['in_reply_to'] && !_validate_inputs(%w[ body in_reply_to ], params)
-          raise ArgumentError, "Required params are: #{%w[ body in_reply_to].join(',')}"
-
-        elsif !_validate_inputs(VALID_REQUEST_COM_PARAM_NAMES - %w[ in_reply_to ], params)
-          raise ArgumentError, "Required params are: #{VALID_REQUEST_COM_PARAM_NAMES.join(', ')}"
-        end
-      end
-
-    end # Comments
-  end # PullRequests
+  end # PullRequests::Comments
 end # Github
--- spec/github/pull_requests/comments_spec.rb
+++ spec/github/pull_requests/comments_spec.rb
@@ -11,11 +11,8 @@ describe Github::PullRequests::Comments do
 
   after { reset_authentication_for github }
 
-  describe "#comments" do
-    context 'check aliases' do
-      it { github.pull_requests.should respond_to :comments }
-      it { github.pull_requests.should respond_to :request_comments }
-    end
+  describe "#list" do
+    it { github.pull_requests.comments.should respond_to :all }
 
     context 'resource found' do
 
@@ -28,36 +25,36 @@ describe Github::PullRequests::Comments do
 
       it "throws error if comment id not provided" do
         expect {
-          github.pull_requests.comments user, repo, nil
+          github.pull_requests.comments.list user, repo, nil
         }.to raise_error(ArgumentError)
       end
 
       it "should get the resources" do
-        github.pull_requests.comments user, repo, pull_request_id
+        github.pull_requests.comments.list user, repo, pull_request_id
         a_get("/repos/#{user}/#{repo}/pulls/#{pull_request_id}/comments").
           should have_been_made
       end
 
       it "should return array of resources" do
-        comments = github.pull_requests.comments user, repo, pull_request_id
+        comments = github.pull_requests.comments.list user, repo, pull_request_id
         comments.should be_an Array
         comments.should have(1).items
       end
 
       it "should be a mash type" do
-        comments = github.pull_requests.comments user, repo, pull_request_id
+        comments = github.pull_requests.comments.list user, repo, pull_request_id
         comments.first.should be_a Hashie::Mash
       end
 
       it "should get pull request comment information" do
-        comments = github.pull_requests.comments user, repo, pull_request_id
+        comments = github.pull_requests.comments.list user, repo, pull_request_id
         comments.first.id.should == pull_request_id
       end
 
       it "should yield to a block" do
-        github.pull_requests.should_receive(:comments).
+        github.pull_requests.comments.should_receive(:list).
           with(user, repo, pull_request_id).and_yield('web')
-        github.pull_requests.comments(user, repo, pull_request_id) {|param| 'web' }
+        github.pull_requests.comments.list(user, repo, pull_request_id) {|param| 'web' }
       end
     end
 
@@ -69,17 +66,14 @@ describe Github::PullRequests::Comments do
 
       it "should return 404 with a message 'Not Found'" do
         expect {
-          github.pull_requests.comments user, repo, pull_request_id
+          github.pull_requests.comments.list user, repo, pull_request_id
         }.to raise_error(Github::Error::NotFound)
       end
     end
-  end # comments
+  end # list
 
-  describe "#comment" do
-    context 'check aliases' do
-      it { github.pull_requests.should respond_to :comment }
-      it { github.pull_requests.should respond_to :get_comment }
-    end
+  describe "#get" do
+    it { github.pull_requests.comments.should respond_to :find }
 
     context 'resource found' do
       before do
@@ -91,24 +85,24 @@ describe Github::PullRequests::Comments do
 
       it "should fail to get resource without comment id" do
         expect {
-          github.pull_requests.comment user, repo, nil
+          github.pull_requests.comments.get user, repo, nil
         }.to raise_error(ArgumentError)
       end
 
       it "should get the resource" do
-        github.pull_requests.comment user, repo, pull_request_id
+        github.pull_requests.comments.get user, repo, pull_request_id
         a_get("/repos/#{user}/#{repo}/pulls/comments/#{comment_id}").
           should have_been_made
       end
 
       it "should get comment information" do
-        comment = github.pull_requests.comment user, repo, comment_id
+        comment = github.pull_requests.comments.get user, repo, comment_id
         comment.id.should eq comment_id
         comment.user.login.should == 'octocat'
       end
 
       it "should return mash" do
-        comment = github.pull_requests.comment user, repo, comment_id
+        comment = github.pull_requests.comments.get user, repo, comment_id
         comment.should be_a Hashie::Mash
       end
     end
@@ -123,13 +117,13 @@ describe Github::PullRequests::Comments do
 
       it "should fail to retrive resource" do
         expect {
-          github.pull_requests.comment user, repo, comment_id
+          github.pull_requests.comments.get user, repo, comment_id
         }.to raise_error(Github::Error::NotFound)
       end
     end
-  end # comment
+  end # get
 
-  describe "create_request" do
+  describe "#create" do
     let(:inputs) {
       {
         "body" => "Nice change",
@@ -150,23 +144,23 @@ describe Github::PullRequests::Comments do
 
       it 'raises error when pull_request_id is missing' do
         expect {
-          github.pull_requests user, repo, nil
+          github.pull_requests.comments.create user, repo, nil
         }.to raise_error(ArgumentError)
       end
 
       it "should create resource successfully" do
-        github.pull_requests.create_comment user, repo, pull_request_id, inputs
+        github.pull_requests.comments.create user, repo, pull_request_id, inputs
         a_post("/repos/#{user}/#{repo}/pulls/#{pull_request_id}/comments").
           with(inputs).should have_been_made
       end
 
       it "should return the resource" do
-        pull_request = github.pull_requests.create_comment user, repo, pull_request_id, inputs
+        pull_request = github.pull_requests.comments.create user, repo, pull_request_id, inputs
         pull_request.should be_a Hashie::Mash
       end
 
       it "should get the request information" do
-        pull_request = github.pull_requests.create_comment user, repo, pull_request_id, inputs
+        pull_request = github.pull_requests.comments.create user, repo, pull_request_id, inputs
         pull_request.id.should == pull_request_id
       end
     end
@@ -181,13 +175,13 @@ describe Github::PullRequests::Comments do
 
       it "should faile to retrieve resource" do
         expect {
-          github.pull_requests.create_comment user, repo, pull_request_id, inputs
+          github.pull_requests.comments.create user, repo, pull_request_id, inputs
         }.to raise_error(Github::Error::NotFound)
       end
     end
-  end # create_comment
+  end # create
 
-  describe "#edit_comment" do
+  describe "#edit" do
     let(:inputs) {
       {
         "body" => "Nice change",
@@ -205,25 +199,26 @@ describe Github::PullRequests::Comments do
       end
 
       it "should edit resource successfully" do
-        github.pull_requests.edit_comment user, repo, comment_id, inputs
+        github.pull_requests.comments.edit user, repo, comment_id, inputs
         a_patch("/repos/#{user}/#{repo}/pulls/comments/#{comment_id}").
           with(inputs).should have_been_made
       end
 
       it "should return the resource" do
-        comment = github.pull_requests.edit_comment user, repo, comment_id, inputs
+        comment = github.pull_requests.comments.edit user, repo, comment_id, inputs
         comment.should be_a Hashie::Mash
       end
 
       it "should get the comment information" do
-        comment = github.pull_requests.edit_comment user, repo, comment_id, inputs
+        comment = github.pull_requests.comments.edit user, repo, comment_id, inputs
         comment.user.login.should == 'octocat'
       end
     end
 
     context "failed to edit resource" do
       before do
-        stub_patch("/repos/#{user}/#{repo}/pulls/comments/#{comment_id}").with(inputs).
+        stub_patch("/repos/#{user}/#{repo}/pulls/comments/#{comment_id}").
+          with(inputs).
           to_return(:body => fixture('pull_requests/comment.json'),
             :status => 404,
             :headers => {:content_type => "application/json; charset=utf-8"})
@@ -231,13 +226,13 @@ describe Github::PullRequests::Comments do
 
       it "should fail to retrieve resource" do
         expect {
-          github.pull_requests.edit_comment user, repo, comment_id, inputs
+          github.pull_requests.comments.edit user, repo, comment_id, inputs
         }.to raise_error(Github::Error::NotFound)
       end
     end
-  end # edit_comment
+  end # edit
 
-  context "#delete_comment" do
+  context "#delete" do
     before do
       stub_delete("/repos/#{user}/#{repo}/pulls/comments/#{comment_id}").
         to_return(:body => fixture('pull_requests/comment.json'),
@@ -247,12 +242,12 @@ describe Github::PullRequests::Comments do
 
     it 'should raise error if comment_id not present' do
       expect {
-        github.pull_requests.delete_comment user, repo, nil
+        github.pull_requests.comments.delete user, repo, nil
       }.to raise_error(ArgumentError)
     end
 
     it "should remove resource successfully" do
-      github.pull_requests.delete_comment user, repo, comment_id
+      github.pull_requests.comments.delete user, repo, comment_id
       a_delete("/repos/#{user}/#{repo}/pulls/comments/#{comment_id}").
         should have_been_made
     end
@@ -263,9 +258,9 @@ describe Github::PullRequests::Comments do
           :status => 404,
           :headers => {:content_type => "application/json; charset=utf-8"})
       expect {
-        github.pull_requests.delete_comment user, repo, comment_id
+        github.pull_requests.comments.delete user, repo, comment_id
       }.to raise_error(Github::Error::NotFound)
     end
-  end # delete_comment
+  end # delete
 
 end # Github::PullRequests::Comments
