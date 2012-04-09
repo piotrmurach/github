3facda5b78386650a070edf258f103cabffcfd46
Piotr Murach <pmurach@gmail.com>
2012-04-09 19:34:01 +0100

    Change members api to new dsl, update docs.

--- lib/github_api/orgs/members.rb
+++ lib/github_api/orgs/members.rb
@@ -1,120 +1,117 @@
 # encoding: utf-8
 
 module Github
-  class Orgs
-    module Members
+  class Orgs::Members < API
 
-      # List members
-      #
-      # List all users who are members of an organization. A member is a user
-      # that belongs to at least 1 team in the organization.
-      # If the authenticated user is also a member of this organization then
-      # both concealed and public members will be returned.
-      # Otherwise only public members are returned.
-      #
-      # = Examples
-      #  @github = Github.new
-      #  @github.orgs.members 'org-name'
-      #  @github.orgs.members 'org-name' { |memb| ... }
-      #
-      def members(org_name, params={})
-        _validate_presence_of org_name
-        _normalize_params_keys(params)
-        response = get("/orgs/#{org_name}/members", params)
-        return response unless block_given?
-        response.each { |el| yield el }
-      end
-      alias :list_members :members
+    # List members
+    #
+    # List all users who are members of an organization. A member is a user
+    # that belongs to at least 1 team in the organization.
+    # If the authenticated user is also a member of this organization then
+    # both concealed and public members will be returned.
+    # Otherwise only public members are returned.
+    #
+    # = Examples
+    #  github = Github.new
+    #  github.orgs.members.list 'org-name'
+    #  github.orgs.members.list 'org-name' { |memb| ... }
+    #
+    def list(org_name, params={})
+      _validate_presence_of org_name
+      _normalize_params_keys(params)
+      response = get_request("/orgs/#{org_name}/members", params)
+      return response unless block_given?
+      response.each { |el| yield el }
+    end
+    alias :all :list
 
-      # Check if user is a member of an organization
-      #
-      # = Examples
-      #  @github = Github.new
-      #  @github.orgs.member? 'org-name', 'member-name'
-      #
-      def member?(org_name, member_name, params={})
-        _validate_presence_of org_name, member_name
-        _normalize_params_keys(params)
-        get("/orgs/#{org_name}/members/#{member_name}", params)
-        true
-      rescue Github::Error::NotFound
-        false
-      end
-      alias :is_member? :member?
+    # Check if user is a member of an organization
+    #
+    # = Examples
+    #  github = Github.new
+    #  github.orgs.members.member? 'org-name', 'member-name'
+    #
+    def member?(org_name, member_name, params={})
+      _validate_presence_of org_name, member_name
+      _normalize_params_keys(params)
+      get_request("/orgs/#{org_name}/members/#{member_name}", params)
+      true
+    rescue Github::Error::NotFound
+      false
+    end
 
-      # Remove a member
-      # Removing a user from this list will remove them from all teams and
-      # they will no longer have any access to the organization’s repositories.
-      #
-      # = Examples
-      #  @github = Github.new
-      #  @github.orgs.delete_member 'org-name', 'member-name'
-      #
-      def delete_member(org_name, member_name, params={})
-        _validate_presence_of org_name, member_name
-        _normalize_params_keys(params)
-        delete("/orgs/#{org_name}/members/#{member_name}", params)
-      end
-      alias :remove_member :delete_member
+    # Remove a member
+    # Removing a user from this list will remove them from all teams and
+    # they will no longer have any access to the organization’s repositories.
+    #
+    # = Examples
+    #  github = Github.new
+    #  github.orgs.members.remove 'org-name', 'member-name'
+    #
+    def delete(org_name, member_name, params={})
+      _validate_presence_of org_name, member_name
+      _normalize_params_keys(params)
+      delete_request("/orgs/#{org_name}/members/#{member_name}", params)
+    end
+    alias :remove :delete
 
-      # List public members
-      # Members of an organization can choose to have their membership publicized or not.
-      # = Examples
-      #  @github = Github.new
-      #  @github.orgs.public_members 'org-name'
-      #  @github.orgs.public_members 'org-name' { |memb| ... }
-      #
-      def public_members(org_name, params={})
-        _validate_presence_of org_name
-        _normalize_params_keys(params)
-        response = get("/orgs/#{org_name}/public_members", params)
-        return response unless block_given?
-        response.each { |el| yield el }
-      end
-      alias :list_public_members :public_members
+    # List public members
+    # Members of an organization can choose to have their membership publicized or not.
+    # = Examples
+    #  github = Github.new
+    #  github.orgs.members.list_public 'org-name'
+    #  github.orgs.members.list_public 'org-name' { |memb| ... }
+    #
+    def list_public(org_name, params={})
+      _validate_presence_of org_name
+      _normalize_params_keys(params)
+      response = get_request("/orgs/#{org_name}/public_members", params)
+      return response unless block_given?
+      response.each { |el| yield el }
+    end
+    alias :public_members :list_public
 
-      # Get if a user is a public member of an organization
-      #
-      # = Examples
-      #  @github = Github.new
-      #  @github.orgs.public_member? 'org-name', 'member-name'
-      #
-      def public_member?(org_name, member_name, params={})
-        _validate_presence_of org_name, member_name
-        _normalize_params_keys(params)
-        get("/orgs/#{org_name}/public_members/#{member_name}", params)
-        true
-      rescue Github::Error::NotFound
-        false
-      end
+    # Get if a user is a public member of an organization
+    #
+    # = Examples
+    #  github = Github.new
+    #  github.orgs.members.public_member? 'org-name', 'member-name'
+    #
+    def public_member?(org_name, member_name, params={})
+      _validate_presence_of org_name, member_name
+      _normalize_params_keys(params)
+      get_request("/orgs/#{org_name}/public_members/#{member_name}", params)
+      true
+    rescue Github::Error::NotFound
+      false
+    end
 
-      # Publicize a user’s membership
-      #
-      # = Examples
-      #  @github = Github.new :oauth_token => '...'
-      #  @github.orgs.publicize 'org-name', 'member-name'
-      #
-      def publicize(org_name, member_name, params={})
-        _validate_presence_of org_name, member_name
-        _normalize_params_keys(params)
-        put("/orgs/#{org_name}/public_members/#{member_name}", params)
-      end
-      alias :make_public :publicize
-      alias :publicize_membership :publicize
+    # Publicize a user’s membership
+    #
+    # = Examples
+    #  github = Github.new :oauth_token => '...'
+    #  github.orgs.members.publicize 'org-name', 'member-name'
+    #
+    def publicize(org_name, member_name, params={})
+      _validate_presence_of org_name, member_name
+      _normalize_params_keys(params)
+      put_request("/orgs/#{org_name}/public_members/#{member_name}", params)
+    end
+    alias :make_public :publicize
+    alias :publicize_membership :publicize
 
-      # Conceal a user’s membership
-      #
-      # = Examples
-      #  @github = Github.new :oauth_token => '...'
-      #  @github.orgs.conceal 'org-name', 'member-name'
-      #
-      def conceal(org_name, member_name, params={})
-        _validate_presence_of org_name, member_name
-        _normalize_params_keys(params)
-        delete("/orgs/#{org_name}/public_members/#{member_name}", params)
-      end
-      alias :conceal_membership :conceal
+    # Conceal a user’s membership
+    #
+    # = Examples
+    #  github = Github.new :oauth_token => '...'
+    #  github.orgs.members.conceal 'org-name', 'member-name'
+    #
+    def conceal(org_name, member_name, params={})
+      _validate_presence_of org_name, member_name
+      _normalize_params_keys(params)
+      delete_request("/orgs/#{org_name}/public_members/#{member_name}", params)
+    end
+    alias :conceal_membership :conceal
 
-    end # Members
-  end # Orgs
+  end # Orgs::Members
 end # Github
--- spec/github/orgs/members_spec.rb
+++ spec/github/orgs/members_spec.rb
@@ -5,123 +5,131 @@ describe Github::Orgs::Members do
   let(:member) { 'peter-murach' }
   let(:org)    { 'github' }
 
-  after { github.user, github.repo, github.oauth_token = nil, nil, nil }
+  after { reset_authentication_for github }
 
-  describe "members" do
+  describe "#list" do
+    it { github.orgs.members.should respond_to :all }
+
     context "resource found" do
       before do
         stub_get("/orgs/#{org}/members").
-          to_return(:body => fixture('orgs/members.json'), :status => 200, :headers => {:content_type => "application/json; charset=utf-8"})
+          to_return(:body => fixture('orgs/members.json'), :status => 200,
+            :headers => {:content_type => "application/json; charset=utf-8"})
       end
 
       it "should fail to get resource without org name" do
-        expect { github.orgs.members }.to raise_error(ArgumentError)
+        expect { github.orgs.members.list nil }.to raise_error(ArgumentError)
       end
 
       it "should get the resources" do
-        github.orgs.members org
+        github.orgs.members.list org
         a_get("/orgs/#{org}/members").should have_been_made
       end
 
       it "should return array of resources" do
-        members = github.orgs.members org
+        members = github.orgs.members.list org
         members.should be_an Array
         members.should have(1).items
       end
 
       it "should be a mash type" do
-        members = github.orgs.members org
+        members = github.orgs.members.list org
         members.first.should be_a Hashie::Mash
       end
 
       it "should get members information" do
-        members = github.orgs.members org
+        members = github.orgs.members.list org
         members.first.login.should == 'octocat'
       end
 
       it "should yield to a block" do
-        github.orgs.should_receive(:members).with(org).and_yield('web')
-        github.orgs.members(org) { |param| 'web' }
+        github.orgs.members.should_receive(:list).with(org).and_yield('web')
+        github.orgs.members.list(org) { |param| 'web' }
       end
     end
 
     context "resource not found" do
       before do
         stub_get("/orgs/#{org}/members").
-          to_return(:body => fixture('orgs/members.json'), :status => 404, :headers => {:content_type => "application/json; charset=utf-8"})
+          to_return(:body => fixture('orgs/members.json'), :status => 404,
+            :headers => {:content_type => "application/json; charset=utf-8"})
       end
 
       it "should return 404 with a message 'Not Found'" do
         expect {
-          github.orgs.members org
+          github.orgs.members.list org
         }.to raise_error(Github::Error::NotFound)
       end
     end
-  end # members
+  end # list
 
-  describe "member?" do
+  describe "#member?" do
     context "with username ane reponame passed" do
       context "this repo is being watched by the user"
         before do
           stub_get("/orgs/#{org}/members/#{member}").
-            to_return(:body => "", :status => 404, :headers => {:user_agent => github.user_agent})
+            to_return(:body => "", :status => 404,
+              :headers => {:user_agent => github.user_agent})
         end
 
       it "should return false if resource not found" do
-        membership = github.orgs.member? org, member
+        membership = github.orgs.members.member? org, member
         membership.should be_false
       end
 
       it "should return true if resoure found" do
           stub_get("/orgs/#{org}/members/#{member}").
             to_return(:body => "", :status => 204, :headers => {:user_agent => github.user_agent})
-        membership = github.orgs.member? org, member
+        membership = github.orgs.members.member? org, member
         membership.should be_true
       end
     end
 
     context "without org name and member name passed" do
       it "should fail validation " do
-        expect { github.orgs.member?(nil, nil) }.to raise_error(ArgumentError)
+        expect {
+          github.orgs.members.member?(nil, nil)
+        }.to raise_error(ArgumentError)
       end
     end
   end # member?
 
-  describe "public_members" do
+  describe "#list_public" do
     context "resource found" do
       before do
         stub_get("/orgs/#{org}/public_members").
-          to_return(:body => fixture('orgs/members.json'), :status => 200, :headers => {:content_type => "application/json; charset=utf-8"})
+          to_return(:body => fixture('orgs/members.json'), :status => 200,
+            :headers => {:content_type => "application/json; charset=utf-8"})
       end
 
       it "should fail to get resource without org name" do
-        expect { github.orgs.public_members }.to raise_error(ArgumentError)
+        expect { github.orgs.members.list_public }.to raise_error(ArgumentError)
       end
 
       it "should get the resources" do
-        github.orgs.public_members org
+        github.orgs.members.list_public org
         a_get("/orgs/#{org}/public_members").should have_been_made
       end
 
       it "should return array of resources" do
-        public_members = github.orgs.public_members org
+        public_members = github.orgs.members.list_public org
         public_members.should be_an Array
         public_members.should have(1).items
       end
 
       it "should be a mash type" do
-        public_members = github.orgs.public_members org
+        public_members = github.orgs.members.list_public org
         public_members.first.should be_a Hashie::Mash
       end
 
       it "should get public_members information" do
-        public_members = github.orgs.public_members org
+        public_members = github.orgs.members.list_public org
         public_members.first.login.should == 'octocat'
       end
 
       it "should yield to a block" do
-        github.orgs.should_receive(:public_members).with(org).and_yield('web')
-        github.orgs.public_members(org) { |param| 'web' }
+        github.orgs.members.should_receive(:list_public).with(org).and_yield('web')
+        github.orgs.members.list_public(org) { |param| 'web' }
       end
     end
 
@@ -133,11 +141,11 @@ describe Github::Orgs::Members do
 
       it "should return 404 with a message 'Not Found'" do
         expect {
-          github.orgs.public_members org
+          github.orgs.members.list_public org
         }.to raise_error(Github::Error::NotFound)
       end
     end
-  end # public_members
+  end # list_public
 
   describe "public_member?" do
     context "with username ane reponame passed" do
@@ -148,21 +156,23 @@ describe Github::Orgs::Members do
         end
 
       it "should return false if resource not found" do
-        public_member = github.orgs.public_member? org, member
+        public_member = github.orgs.members.public_member? org, member
         public_member.should be_false
       end
 
       it "should return true if resoure found" do
           stub_get("/orgs/#{org}/public_members/#{member}").
             to_return(:body => "", :status => 204, :headers => {:user_agent => github.user_agent})
-        public_member = github.orgs.public_member? org, member
+        public_member = github.orgs.members.public_member? org, member
         public_member.should be_true
       end
     end
 
     context "without org name and member name passed" do
       it "should fail validation " do
-        expect { github.orgs.public_member?(nil, nil) }.to raise_error(ArgumentError)
+        expect {
+          github.orgs.members.public_member?(nil, nil)
+        }.to raise_error(ArgumentError)
       end
     end
   end # public_member?
@@ -175,11 +185,11 @@ describe Github::Orgs::Members do
       end
 
       it "should fail to get resource without org name" do
-        expect { github.orgs.publicize }.to raise_error(ArgumentError)
+        expect { github.orgs.members.publicize }.to raise_error(ArgumentError)
       end
 
       it "should get the resources" do
-        github.orgs.publicize org, member
+        github.orgs.members.publicize org, member
         a_put("/orgs/#{org}/public_members/#{member}").should have_been_made
       end
     end
@@ -187,30 +197,34 @@ describe Github::Orgs::Members do
     context "resource not found" do
       before do
         stub_put("/orgs/#{org}/public_members/#{member}").
-          to_return(:body => fixture('orgs/members.json'), :status => 404, :headers => {:content_type => "application/json; charset=utf-8"})
+          to_return(:body => fixture('orgs/members.json'), :status => 404,
+            :headers => {:content_type => "application/json; charset=utf-8"})
       end
 
       it "should return 404 with a message 'Not Found'" do
         expect {
-          github.orgs.publicize org, member
+          github.orgs.members.publicize org, member
         }.to raise_error(Github::Error::NotFound)
       end
     end
   end # publicize
 
-  describe "conceal" do
+  describe "#conceal" do
     context "request perfomed successfully" do
       before do
         stub_delete("/orgs/#{org}/public_members/#{member}").
-          to_return(:body => fixture('orgs/members.json'), :status => 204, :headers => {:content_type => "application/json; charset=utf-8"})
+          to_return(:body => fixture('orgs/members.json'), :status => 204,
+            :headers => {:content_type => "application/json; charset=utf-8"})
       end
 
       it "should fail to get resource without org name" do
-        expect { github.orgs.conceal nil, nil }.to raise_error(ArgumentError)
+        expect {
+          github.orgs.members.conceal nil, nil
+        }.to raise_error(ArgumentError)
       end
 
       it "should get the resources" do
-        github.orgs.conceal org, member
+        github.orgs.members.conceal org, member
         a_delete("/orgs/#{org}/public_members/#{member}").should have_been_made
       end
     end
@@ -218,18 +232,19 @@ describe Github::Orgs::Members do
     context "resource not found" do
       before do
         stub_delete("/orgs/#{org}/public_members/#{member}").
-          to_return(:body => fixture('orgs/members.json'), :status => 404, :headers => {:content_type => "application/json; charset=utf-8"})
+          to_return(:body => fixture('orgs/members.json'), :status => 404,
+            :headers => {:content_type => "application/json; charset=utf-8"})
       end
 
       it "should return 404 with a message 'Not Found'" do
         expect {
-          github.orgs.conceal org, member
+          github.orgs.members.conceal org, member
         }.to raise_error(Github::Error::NotFound)
       end
     end
   end # conceal
 
-  describe "delete_member" do
+  describe "#delete" do
     let(:hook_id) { 1 }
 
     context "resource deleted successfully" do
@@ -239,11 +254,11 @@ describe Github::Orgs::Members do
       end
 
       it "should fail to delete without org and member parameters" do
-        expect { github.repos.delete_hook nil, nil }.to raise_error(ArgumentError)
+        expect { github.orgs.members.delete nil, nil }.to raise_error(ArgumentError)
       end
 
       it "should delete the resource" do
-        github.orgs.delete_member org, member
+        github.orgs.members.delete org, member
         a_delete("/orgs/#{org}/members/#{member}").should have_been_made
       end
     end
@@ -251,16 +266,16 @@ describe Github::Orgs::Members do
     context "failed to edit resource" do
       before do
         stub_delete("/orgs/#{org}/members/#{member}").
-          to_return(:body => '', :status => 404, :headers => { :content_type => "application/json; charset=utf-8"})
-
+          to_return(:body => '', :status => 404,
+            :headers => { :content_type => "application/json; charset=utf-8"})
       end
 
       it "should fail to find resource" do
         expect {
-          github.orgs.delete_member org, member
+          github.orgs.members.delete org, member
         }.to raise_error(Github::Error::NotFound)
       end
     end
-  end # delete_member
+  end # delete
 
 end # Github::Orgs::Members
