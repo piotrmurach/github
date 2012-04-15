891e185d1c6d3a1f4c9577b30a71a5b8c8455913
Piotr Murach <pmurach@gmail.com>
2012-04-15 16:55:07 +0100

    Add xml parsing for file upload resposne.

---
+++ lib/github_api/response/xmlize.rb
@@ -0,0 +1,26 @@
+# encoding: utf-8
+
+require 'faraday'
+
+module Github
+  class Response::Xmlize < Response
+    dependency 'nokogiri'
+
+    define_parser do |body|
+      ::Nokogiri::XML body
+    end
+
+    def parse(body)
+      case body
+      when ''
+        nil
+      when 'true'
+        true
+      when 'false'
+        false
+      else
+        self.class.parser.call body
+      end
+    end
+  end # Response::Xmlize
+end # Github
