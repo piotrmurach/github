8660b9a600aa125cff006046e950a2a401f0c0c8
Piotr Murach <pmurach@gmail.com>
2012-04-28 15:16:51 +0100

    Change request methods singatures.

--- lib/github_api/request.rb
+++ lib/github_api/request.rb
@@ -11,26 +11,6 @@ module Github
     METHODS = [:get, :post, :put, :delete, :patch]
     METHODS_WITH_BODIES = [ :post, :put, :patch ]
 
-    def get(path, params={}, options={})
-      request(:get, path, params, options)
-    end
-
-    def patch(path, params={}, options={})
-      request(:patch, path, params, options)
-    end
-
-    def post(path, params={}, options={})
-      request(:post, path, params, options)
-    end
-
-    def put(path, params={}, options={})
-      request(:put, path, params, options)
-    end
-
-    def delete(path, params={}, options={})
-      request(:delete, path, params, options)
-    end
-
     def get_request(path, params={}, options={})
       request(:get, path, params, options)
     end
--- spec/github/request_spec.rb
+++ spec/github/request_spec.rb
@@ -2,6 +2,35 @@
 
 require 'spec_helper'
 
+
 describe Github::Request do
-  pending
-end
+  let(:github) { Github::API.new  }
+  let(:path) { 'github.api/repos/users' }
+  let(:params) { {} }
+  let(:options) { {} }
+
+  it "knows how to make get request" do
+    github.should_receive(:request).with(:get, path, params, options)
+    github.get_request path, params, options
+  end
+
+  it "knows how to make patch request" do
+    github.should_receive(:request).with(:patch, path, params, options)
+    github.patch_request path, params, options
+  end
+
+  it "knows how to make post request" do
+    github.should_receive(:request).with(:post, path, params, options)
+    github.post_request path, params, options
+  end
+
+  it "knows how to make put request" do
+    github.should_receive(:request).with(:put, path, params, options)
+    github.put_request path, params, options
+  end
+
+  it "knows how to make delete request" do
+    github.should_receive(:request).with(:delete, path, params, options)
+    github.delete_request path, params, options
+  end
+end # Github::Request
