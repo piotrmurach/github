c0d9f4e701df48f6b0e4f8998da0aa5504a875ac
Piotr Murach <pmurach@gmail.com>
2012-05-07 15:13:52 +0100

    Add custom setup for running live test suite.

---
+++ features/support/settings.rb
@@ -0,0 +1,7 @@
+require 'yaml'
+
+def settings_file
+  Pathname.new(File.expand_path("../../settings.yml", __FILE__))
+end
+
+SETTINGS = (File.exists?(settings_file) && yaml= YAML.load_file(settings_file)) ? yaml : {}
