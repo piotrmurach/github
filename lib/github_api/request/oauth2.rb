fc6d3be2c602e94c7602ba6a7bbc1c16fc2d5832
Piotr Murach <pmurach@gmail.com>
2012-05-20 13:13:33 +0100

    Change oauth2 token processing.

--- lib/github_api/request/oauth2.rb
+++ lib/github_api/request/oauth2.rb
@@ -5,28 +5,39 @@ require 'faraday'
 module Github
   module Request
     class OAuth2 < Faraday::Middleware
+      include Github::Utils::Url
+
+      ACCESS_TOKEN = 'access_token'.freeze
+      AUTH_HEADER  = 'Authorization'.freeze
+
       dependency 'oauth2'
 
       def call(env)
-#         puts "ENV: #{env.inspect}"
-#         puts "TOKEN : #{@token}"
-#         puts "APP: #{@app}"
-
         # Extract parameters from the query
-        params = env[:url].query_values || {}
+        params = { ACCESS_TOKEN => @token }.update query_params(env[:url])
 
-        env[:url].query_values = { 'access_token' => @token }.merge(params)
-
-        token = env[:url].query_values['access_token']
-
-        env[:request_headers].merge!('Authorization' => "Token token=\"#{token}\"")
+        if token = params[ACCESS_TOKEN] and !token.empty?
+          env[:url].query = build_query params
+          env[:request_headers].merge!(AUTH_HEADER => "Token token=\"#{token}\"")
+        end
 
         @app.call env
       end
 
       def initialize(app, *args)
+        super app
         @app = app
         @token = args.shift
+      end
+
+      def query_params(url)
+        if url.query.nil? or url.query.empty?
+          {}
+        else
+          puts "QUERY BEFORE #{url.query}"
+          puts "QUERY AFTER #{parse_query url.query}"
+          parse_query url.query
+        end
       end
     end # OAuth2
   end # Request
