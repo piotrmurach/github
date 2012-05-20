0d4ed97b7ba85e86e3d7ec26359b22186a08f982
Piotr Murach <pmurach@gmail.com>
2012-05-20 11:50:25 +0100

    Add filtering of your github email.

--- features/support/settings.rb
+++ features/support/settings.rb
@@ -4,4 +4,4 @@ def settings_file
   Pathname.new(File.expand_path("../../settings.yml", __FILE__))
 end
 
-SETTINGS = (File.exists?(settings_file) && yaml= YAML.load_file(settings_file)) ? yaml : {'basic_auth' => 'login:password', 'oauth_token' => 'as79asfd79ads', 'user'=> 'octokat', 'repo' => 'dummy'}
+SETTINGS = (File.exists?(settings_file) && yaml= YAML.load_file(settings_file)) ? yaml : {'basic_auth' => 'login:password', 'oauth_token' => 'as79asfd79ads', 'user'=> 'octokat', 'repo' => 'dummy', 'email' => 'email@example.com'}
--- features/support/vcr.rb
+++ features/support/vcr.rb
@@ -4,6 +4,7 @@ VCR.configure do |conf|
   conf.hook_into :webmock
   conf.cassette_library_dir = 'features/cassettes'
   conf.default_cassette_options = { :record => :new_episodes }
+  conf.filter_sensitive_data('<EMAIL>') { SETTINGS['email'] }
   conf.filter_sensitive_data('<TOKEN>') { SETTINGS['oauth_token'] }
   conf.filter_sensitive_data('<BASIC_AUTH>') { SETTINGS['basic_auth'] }
   conf.filter_sensitive_data('<USER>') { SETTINGS['user'] }
