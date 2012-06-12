41f75a939d70e066d9c8c22c7573e22e96ccb938
Piotr Murach <pmurach@gmail.com>
2012-06-12 15:49:19 +0100

    Bump version up.

--- lib/github_api/version.rb
+++ lib/github_api/version.rb
@@ -3,8 +3,8 @@
 module Github
   module VERSION
     MAJOR = 0
-    MINOR = 5
-    PATCH = 4
+    MINOR = 6
+    PATCH = 0
     BUILD = nil
 
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.');
