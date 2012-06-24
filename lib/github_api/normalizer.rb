3b4344742f0c5ab86a455ba71e9f8abd4e2200f9
Piotr Murach <pmurach@gmail.com>
2012-06-24 16:22:49 +0100

    Add normalizer for params processing.

---
+++ lib/github_api/normalizer.rb
@@ -0,0 +1,27 @@
+# -*- encoding: utf-8 -*-
+
+module Github
+  # Deals with normalazing client supplied parameter keys.
+  module Normalizer
+
+    # Turns any keys from nested hashes including nested arrays into strings
+    #
+    def normalize!(params)
+      case params
+      when Hash
+        params.keys.each do |k|
+          params[k.to_s] = params.delete(k)
+          normalize!(params[k.to_s])
+        end
+      when Array
+        params.map! do |el|
+          normalize!(el)
+        end
+      else
+        params.to_s
+      end
+      return params
+    end
+
+  end # Normalizer
+end # Github
