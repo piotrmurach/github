a89e75edc36b84637478b32564d76e58c33ab438
Piotr Murach <pmurach@gmail.com>
2012-07-28 15:51:21 +0100

    Change service errors to handle status codes and response body parsing.

--- lib/github_api/error/service_error.rb
+++ lib/github_api/error/service_error.rb
@@ -1,19 +1,56 @@
 # encoding: utf-8
+require 'multi_json'
 
-module Github #:nodoc
-  # Raised when Github returns any of the HTTP status codes
+module Github
+  # Raised when GitHub returns any of the HTTP status codes
   module Error
     class ServiceError < GithubError
-      attr_accessor :http_headers
+      attr_reader :http_headers
 
-      def initialize(env)
-        super(generate_message(env))
-        @http_headers = env[:response_headers]
+      def initialize(response)
+        @http_headers = response[:response_headers]
+        message = parse_response(response)
+        super(message)
       end
 
-      def generate_message(env)
-        "#{env[:method].to_s.upcase} #{env[:url].to_s}: #{env[:status]} #{env[:body]}"
+      def parse_response(response)
+        body = parse_body(response[:body])
+        "#{response[:method].to_s.upcase} #{response[:url].to_s}: #{response[:status]} #{body}"
       end
+
+      def decode_body(body)
+        if body.respond_to?(:to_str) && body.length >= 2
+           MultiJson.load(body, :symbolize_keys => true)
+        else
+          body
+        end
+      end
+
+      def parse_body(body)
+        body = decode_body(body)
+
+        return '' if body.nil? || body.empty?
+
+        if body[:error]
+          body[:error]
+        elsif body[:errors]
+          error = Array(body[:errors]).first
+          if error.kind_of?(Hash)
+            error[:message]
+          else
+            error
+          end
+        elsif body[:message]
+          body[:message]
+        else
+          ''
+        end
+      end
+
+      def self.http_status_code(code)
+        define_method(:http_status_code) { code }
+      end
+
     end
   end # Error
 end # Github
