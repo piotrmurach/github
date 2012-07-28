51a532319622658b2064be38558ce0365494bbe4
Piotr Murach <pmurach@gmail.com>
2012-07-28 18:31:16 +0100

    Add validation of request arguments.

--- lib/github_api/error/validations.rb
+++ lib/github_api/error/validations.rb
@@ -7,9 +7,9 @@ module Github #:nodoc
       def initialize(errors)
         super(
           generate_message(
-            :problem => '',
-            :summary => '',
-            :resolution => ''
+            :problem => "Attempted to send request with nil arguments for #{errors.keys.join(', ')}.",
+            :summary => 'Each request expects certain number of arguments.',
+            :resolution => 'Double check that the provided arguments are set to some value.'
           )
         )
       end
