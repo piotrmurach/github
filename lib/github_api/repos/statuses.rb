f017412635365663b99fc328a7ab156f09e4c193
Piotr Murach <pmurach@gmail.com>
2012-09-06 22:00:46 +0100

    Format comments.

--- lib/github_api/repos/statuses.rb
+++ lib/github_api/repos/statuses.rb
@@ -2,7 +2,9 @@
 
 module Github
   class Repos::Statuses < API
-    # The Status API allows external services to mark commits with a success, failure, error, or pending state, which is then reflected in pull requests involving those commits.
+    # The Status API allows external services to mark commits with a success,
+    # failure, error, or pending state, which is then reflected in pull requests
+    # involving those commits.
 
     VALID_STATUS_PARAM_NAMES = %w[
       state
@@ -33,17 +35,19 @@ module Github
     # Create a status
     #
     # = Inputs
-    # * <tt>:state</tt> - Required string - State of the status - can be one of pending, success, error, or failure.
-    # * <tt>:target_url</tt> - Optional string - Target url to associate with this status. This URL will be linked from the GitHub UI to allow users to easily see the ‘source’ of the Status.
+    # * <tt>:state</tt> - Required string - State of the status - can be one of
+    #                                       pending, success, error, or failure.
+    # * <tt>:target_url</tt> - Optional string - Target url to associate with this
+    #         status. This URL will be linked from the GitHub UI to allow users
+    #         to easily see the ‘source’ of the Status.
     # * <tt>:description</tt> - Optional string - Short description of the status
     #
     # = Examples
     #  github = Github.new
-    #  github.repos.statuses.create 'user-name', 'repo-name', 'sha'
+    #  github.repos.statuses.create 'user-name', 'repo-name', 'sha',
     #    "state" =>  "success",
     #    "target_url" => "http://ci.example.com/johndoe/my-repo/builds/sha",
     #    "description" => "Successful build #3 from origin/master"
-    #    }
     #
     def create(user_name, repo_name, sha, params={})
       _update_user_repo_params(user_name, repo_name)
