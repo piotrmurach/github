2e0895c596b471193beac68c19a71f9c990e4fcf
Piotr Murach <pmurach@gmail.com>
2012-10-27 20:52:52 +0100

    Ensure that globals are reset.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -92,6 +92,7 @@ OAUTH_TOKEN = 'bafec72922f31fe86aacc8aca4261117f3bd62cf'
 
 def reset_authentication_for(object)
   [ 'user', 'repo', 'basic_auth', 'oauth_token', 'login', 'password' ].each do |item|
+    Github.send("#{item}=", nil)
     object.send("#{item}=", nil)
   end
 end
