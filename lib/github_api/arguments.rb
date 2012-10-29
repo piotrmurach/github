cfe0351ec374f487a520784f8d1d327ec5715af1
Piotr Murach <pmurach@gmail.com>
2012-10-29 23:05:43 +0000

    Add parameters filtering.

--- lib/github_api/arguments.rb
+++ lib/github_api/arguments.rb
@@ -5,6 +5,7 @@ module Github
   class Arguments
 
     include Normalizer
+    include ParameterFilter
     include Validations
 
     # Arguments used to construct request path
@@ -23,12 +24,22 @@ module Github
     attr_reader :args_required
     private :args_required
 
+    # Allowed keys inside parameter hash
+    #
+    attr_reader :filter
+    private :filter
+
     # Takes api, filters and required arguments
     #
+    # = Parameters
+    #  :args_required - arguments that must be present before request is fired
+    #  :filter        - allowed keys inside parameter hash
+    #
     def initialize(api, options={})
-      @api = api
       normalize! options
+      @api           = api
       @args_required = options.fetch('args_required', []).map(&:to_s)
+      @filter        = options.fetch('filter', nil)
     end
 
     # Parse arguments to allow for flexible api calls.
@@ -44,12 +55,15 @@ module Github
         # Arguments are inside the parameters hash
         parse_options options
       end
+      filter!(filter, options) if filter
       @params = options
       self
     end
 
     private
 
+    # Check and set all requried arguments
+    #
     def parse_arguments(*args)
       assert_presence_of *args
       args.each_with_index do |arg, indx|
@@ -58,6 +72,8 @@ module Github
       check_requirement!(*args)
     end
 
+    # Remove required arguments from parameters
+    #
     def parse_options(options)
       options.each do |key, val|
         key = key.to_s
