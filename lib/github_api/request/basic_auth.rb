b7b6f72e64b5268ba8a3513abd5191f3335d79c3
Piotr Murach <pmurach@gmail.com>
2012-11-10 19:40:04 +0000

    Change to request class scope.

--- lib/github_api/request/basic_auth.rb
+++ lib/github_api/request/basic_auth.rb
@@ -4,28 +4,26 @@ require 'faraday'
 require 'base64'
 
 module Github
-  module Request
-    class BasicAuth < Faraday::Middleware
-      dependency 'base64'
+  class Request::BasicAuth < Faraday::Middleware
+    dependency 'base64'
 
-      def call(env)
-        env[:request_headers].merge!('Authorization' => "Basic #{@auth}\"")
+    def call(env)
+      env[:request_headers].merge!('Authorization' => "Basic #{@auth}\"")
 
-        @app.call env
+      @app.call env
+    end
+
+    def initialize(app, *args)
+      @app = app
+      credentials = ""
+      options = args.extract_options!
+      if options.has_key? :login
+        credentials = "#{options[:login]}:#{options[:password]}"
+      elsif options.has_key? :basic_auth
+        credentials = "#{options[:basic_auth]}"
       end
-
-      def initialize(app, *args)
-        @app = app
-        credentials = ""
-        options = args.extract_options!
-        if options.has_key? :login
-          credentials = "#{options[:login]}:#{options[:password]}"
-        elsif options.has_key? :basic_auth
-          credentials = "#{options[:basic_auth]}"
-        end
-        @auth = Base64.encode64(credentials)
-        @auth.gsub!("\n", "")
-      end
-    end # BasicAuth
-  end # Request
+      @auth = Base64.encode64(credentials)
+      @auth.gsub!("\n", "")
+    end
+  end # Request::BasicAuth
 end # Github
--- lib/github_api/request/oauth2.rb
+++ lib/github_api/request/oauth2.rb
@@ -3,40 +3,38 @@
 require 'faraday'
 
 module Github
-  module Request
-    class OAuth2 < Faraday::Middleware
-      include Github::Utils::Url
+  class Request::OAuth2 < Faraday::Middleware
+    include Github::Utils::Url
 
-      ACCESS_TOKEN = 'access_token'.freeze
-      AUTH_HEADER  = 'Authorization'.freeze
+    ACCESS_TOKEN = 'access_token'.freeze
+    AUTH_HEADER  = 'Authorization'.freeze
 
-      dependency 'oauth2'
+    dependency 'oauth2'
 
-      def call(env)
-        # Extract parameters from the query
-        params = { ACCESS_TOKEN => @token }.update query_params(env[:url])
+    def call(env)
+      # Extract parameters from the query
+      params = { ACCESS_TOKEN => @token }.update query_params(env[:url])
 
-        if token = params[ACCESS_TOKEN] and !token.empty?
-          env[:url].query = build_query params
-          env[:request_headers].merge!(AUTH_HEADER => "Token token=\"#{token}\"")
-        end
-
-        @app.call env
+      if token = params[ACCESS_TOKEN] and !token.empty?
+        env[:url].query = build_query params
+        env[:request_headers].merge!(AUTH_HEADER => "Token token=\"#{token}\"")
       end
 
-      def initialize(app, *args)
-        super app
-        @app = app
-        @token = args.shift
-      end
+      @app.call env
+    end
 
-      def query_params(url)
-        if url.query.nil? or url.query.empty?
-          {}
-        else
-          parse_query url.query
-        end
+    def initialize(app, *args)
+      super app
+      @app = app
+      @token = args.shift
+    end
+
+    def query_params(url)
+      if url.query.nil? or url.query.empty?
+        {}
+      else
+        parse_query url.query
       end
-    end # OAuth2
-  end # Request
+    end
+  end # Request::OAuth2
 end # Github
