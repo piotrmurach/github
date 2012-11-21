7b85190cebbb7fa11e0552572dd70a39de102f99
Piotr Murach <pmurach@gmail.com>
2012-11-21 21:24:33 +0000

    Change merging api spec.

--- spec/github/repos/merging_spec.rb
+++ spec/github/repos/merging_spec.rb
@@ -1,71 +1,58 @@
 require 'spec_helper'
 
-describe Github::Repos::Merging do
-  let(:github) { Github.new }
+describe Github::Repos::Merging, '#merge' do
   let(:user) { 'peter-murach' }
   let(:repo) { 'github' }
+  let(:request_path) { "/repos/#{user}/#{repo}/merges" }
+  let(:inputs) do
+    {
+      "base" => "master",
+      "head" => "cool_feature",
+      "commit_message" => "Shipped cool_feature!"
+    }
+  end
 
-  after { github.user, github.repo, github.oauth_token = nil, nil, nil }
+  before {
+    stub_post(request_path).to_return(:body => body, :status => status,
+      :headers => {:content_type => "application/json; charset=utf-8"})
+  }
 
-  describe "#merge" do
-    let(:inputs) do
-      {
-        "base" => "master",
-        "head" => "cool_feature",
-        "commit_message" => "Shipped cool_feature!"
-      }
+  after { reset_authentication_for(subject) }
+
+  context "resouce merged" do
+    let(:body) { fixture('repos/merge.json') }
+    let(:status) { 201 }
+
+    it "should fail to merge resource if 'base' input is missing" do
+      expect {
+        subject.merge user, repo, inputs.except('base')
+      }.to raise_error(Github::Error::RequiredParams)
     end
 
-    context "resouce merged" do
-      before do
-        stub_post("/repos/#{user}/#{repo}/merges").with(inputs).
-          to_return(:body => fixture('repos/merge.json'),
-            :status => 201,
-            :headers => {:content_type => "application/json; charset=utf-8"})
-      end
-
-      it "should fail to merge resource if 'base' input is missing" do
-        expect {
-          github.repos.merging.merge user, repo, inputs.except('base')
-        }.to raise_error(Github::Error::RequiredParams)
-      end
-
-      it "should fail to create resource if 'head' input is missing" do
-        expect {
-          github.repos.merging.merge user, repo, inputs.except('head')
-        }.to raise_error(Github::Error::RequiredParams)
-      end
-
-      it "should merge resource successfully" do
-        github.repos.merging.merge user, repo, inputs
-        a_post("/repos/#{user}/#{repo}/merges").with(inputs).should have_been_made
-      end
-
-      it "should return the resource" do
-        merge = github.repos.merging.merge user, repo, inputs
-        merge.should be_a Hashie::Mash
-      end
-
-      it "should get the commit comment information" do
-        merge = github.repos.merging.merge user, repo, inputs
-        merge.commit.author.login.should == 'octocat'
-      end
+    it "should fail to create resource if 'head' input is missing" do
+      expect {
+        subject.merge user, repo, inputs.except('head')
+      }.to raise_error(Github::Error::RequiredParams)
     end
 
-    context "failed to create resource" do
-      before do
-        stub_post("/repos/#{user}/#{repo}/merges").with(inputs).
-          to_return(:body => fixture('repos/merge.json'), 
-            :status => 404,
-            :headers => {:content_type => "application/json; charset=utf-8"})
-      end
-
-      it "should fail to retrieve resource" do
-        expect {
-          github.repos.merging.merge user, repo, inputs
-        }.to raise_error(Github::Error::NotFound)
-      end
+    it "should merge resource successfully" do
+      subject.merge user, repo, inputs
+      a_post(request_path).with(inputs).should have_been_made
     end
-  end # merge
 
-end # Github::Repos::Merging
+    it "should return the resource" do
+      merge = subject.merge user, repo, inputs
+      merge.should be_a Hashie::Mash
+    end
+
+    it "should get the commit comment information" do
+      merge = subject.merge user, repo, inputs
+      merge.commit.author.login.should == 'octocat'
+    end
+  end
+
+  it_should_behave_like 'request failure' do
+    let(:requestable) { subject.merge user, repo, inputs }
+  end
+
+end # merge
