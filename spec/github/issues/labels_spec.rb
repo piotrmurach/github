6dd89ecabde5b298b1b90cc64ace2165dd854ea9
Piotr Murach <pmurach@gmail.com>
2012-12-20 23:16:01 +0000

    Change labels api remove call tests.

--- spec/github/issues/labels_spec.rb
+++ spec/github/issues/labels_spec.rb
@@ -10,64 +10,6 @@ describe Github::Issues::Labels do
 
   it { described_class::VALID_LABEL_INPUTS.should_not be_nil }
 
-  describe "#remove" do
-    let(:issue_id) { 1 }
-    let(:label_id) { 1 }
-
-    context "remove a label from an issue" do
-      before do
-        stub_delete("/repos/#{user}/#{repo}/issues/#{issue_id}/labels/#{label_id}").
-          to_return(:body => fixture('issues/labels.json'), :status => 200, :headers => {:content_type => "application/json; charset=utf-8"})
-      end
-
-      it "should throw exception if issue-id not present" do
-        expect {
-          github.issues.labels.remove user, repo, nil
-        }.to raise_error(ArgumentError)
-      end
-
-      it "should remove label successfully" do
-        github.issues.labels.remove user, repo, issue_id, label_id
-        a_delete("/repos/#{user}/#{repo}/issues/#{issue_id}/labels/#{label_id}").should have_been_made
-      end
-
-      it "should return the resource" do
-        labels = github.issues.labels.remove user, repo, issue_id, label_id
-        labels.first.should be_a Hashie::Mash
-      end
-
-      it "should get the label information" do
-        labels = github.issues.labels.remove user, repo, issue_id, label_id
-        labels.first.name.should == 'bug'
-      end
-    end
-
-    context "remove all labels from an issue" do
-      before do
-        stub_delete("/repos/#{user}/#{repo}/issues/#{issue_id}/labels").
-          to_return(:body => "", :status => 204, :headers => {:content_type => "application/json; charset=utf-8"})
-      end
-
-      it "should remove labels successfully" do
-        github.issues.labels.remove user, repo, issue_id
-        a_delete("/repos/#{user}/#{repo}/issues/#{issue_id}/labels").should have_been_made
-      end
-    end
-
-    context "failed to remove label from an issue" do
-      before do
-        stub_delete("/repos/#{user}/#{repo}/issues/#{issue_id}/labels/#{label_id}").
-          to_return(:body => fixture('issues/label.json'), :status => 404, :headers => {:content_type => "application/json; charset=utf-8"})
-      end
-
-      it "should faile to retrieve resource" do
-        expect {
-          github.issues.labels.remove user, repo, issue_id, label_id
-        }.to raise_error(Github::Error::NotFound)
-      end
-    end
-  end # remove
-
   describe "#replace" do
     let(:issue_id) { 1 }
     let(:labels) { "Label 1" }
