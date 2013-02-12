554b74ef83b8756ef21001f43c0da73c428e91d1
Piotr Murach <pmurach@gmail.com>
2013-02-12 22:50:28 +0000

    Change to use new response header.

--- lib/github_api/result.rb
+++ lib/github_api/result.rb
@@ -6,62 +6,8 @@ module Github
 
     # TODO Add result counts method to check total items looking at result links
 
-    # All request headers
-    def headers
-      loaded? ? @env[:response_headers] : nil
-    end
-
-    def oauth_scopes
-      loaded? ? @env[:response_headers][OAUTH_SCOPES] : nil
-    end
-
-    def accepted_oauth_scopes
-      loaded? ? @env[:response_headers][ACCEPTED_OAUTH_SCOPES] : nil
-    end
-
-    # Requests are limited to API v3 to 5000 per hour.
-    def ratelimit_limit
-      loaded? ? @env[:response_headers][RATELIMIT_LIMIT] : nil
-    end
-
-    def ratelimit_remaining
-      loaded? ? @env[:response_headers][RATELIMIT_REMAINING] : nil
-    end
-
-    def cache_control
-      loaded? ? @env[:response_headers][CACHE_CONTROL] : nil
-    end
-
-    def content_type
-      loaded? ? @env[:response_headers][CONTENT_TYPE] : nil
-    end
-
-    def content_length
-      loaded? ? @env[:response_headers][CONTENT_LENGTH] : nil
-    end
-
-    def etag
-      loaded? ? @env[:response_headers][ETAG] : nil
-    end
-
-    def date
-      loaded? ? @env[:response_headers][DATE] : nil
-    end
-
-    def location
-      loaded? ? @env[:response_headers][LOCATION] : nil
-    end
-
-    def server
-      loaded? ? @env[:response_headers][SERVER] : nil
-    end
-
-    def status
-      loaded? ? @env[:status] : nil
-    end
-
-    def success?
-      (200..299).include? status
+    def header
+      @header = Github::Response::Header.new(@env)
     end
 
     # Returns raw body
