ce7751294343c0ce5e1fea4482f23d1f43e38821
Piotr Murach <pmurach@gmail.com>
2013-02-17 18:17:40 +0000

    Simplify code.

--- lib/github_api/api_factory.rb
+++ lib/github_api/api_factory.rb
@@ -6,24 +6,26 @@ module Github
   class ApiFactory
 
     # Instantiates a new github api object
+    #
     def self.new(klass, options={})
       return create_instance(klass, options) if klass
-      raise ArgumentError, 'must provied klass to be instantiated'
+      raise ArgumentError, 'must provide API class to be instantiated'
     end
 
     # Passes configuration options to instantiated class
+    #
     def self.create_instance(klass, options)
       options.symbolize_keys!
-      instance = convert_to_constant(klass.to_s).new options
-      instance
+      convert_to_constant(klass.to_s).new options
     end
 
+    # Convert name to constant
+    #
     def self.convert_to_constant(classes)
-      constant = Github
-      classes.split('::').each do |klass|
-        constant = constant.const_get klass
+      classes.split('::').inject(Github) do |constant, klass|
+        constant.const_get klass
       end
-      return constant
     end
-  end
+
+  end # ApiFactory
 end # Github
