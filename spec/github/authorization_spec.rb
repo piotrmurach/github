4beba6eb080b0a53a77d93bd3dee26f5d42737c1
Piotr Murach <pmurach@gmail.com>
2013-02-17 19:37:53 +0000

    No need to reset.

--- spec/github/authorization_spec.rb
+++ spec/github/authorization_spec.rb
@@ -6,17 +6,16 @@ describe Github::Authorization do
   let(:client_id) { '234jl23j4l23j4l' }
   let(:client_secret) { 'asasd79sdf9a7asfd7sfd97s' }
   let(:code) { 'c9798sdf97df98ds'}
-  let(:github) {
-    Github.new :client_id => client_id, :client_secret => client_secret
-  }
+  let(:options) { {} }
+
+  subject(:github) { Github.new options }
 
   after do
-    github.client_id, github.client_secret = nil, nil
     reset_authentication_for github
   end
 
   context '.client' do
-    it { github.should respond_to :client }
+    it { should respond_to :client }
 
     it "should return OAuth2::Client instance" do
       github.client.should be_a OAuth2::Client
@@ -39,12 +38,10 @@ describe Github::Authorization do
     let(:oauth) { OAuth2::Client.new(client_id, client_secret) }
 
     it "should throw an error if no client_id" do
-      github.client_id = nil
       expect { github.auth_code }.to raise_error(ArgumentError)
     end
 
     it "should throw an error if no client_secret" do
-      github.client_secret = nil
       expect { github.auth_code }.to raise_error(ArgumentError)
     end
 
@@ -57,9 +54,9 @@ describe Github::Authorization do
   end
 
   context "authorize_url" do
-    it "should respond to 'authorize_url' " do
-      github.should respond_to :authorize_url
-    end
+    let(:options) { {:client_id => client_id, :client_secret => client_secret} }
+
+    it { should respond_to(:authorize_url) }
 
     it "should return address containing client_id" do
       github.authorize_url.should =~ /client_id=#{client_id}/
@@ -75,14 +72,14 @@ describe Github::Authorization do
   end
 
   context "get_token" do
+    let(:options) { {:client_id => client_id, :client_secret => client_secret} }
+
     before do
       stub_request(:post, 'https://github.com/login/oauth/access_token').
         to_return(:body => '', :status => 200, :headers => {})
     end
 
-    it "should respond to 'get_token' " do
-      github.should respond_to :get_token
-    end
+    it { should respond_to(:get_token) }
 
     it "should make the authorization request" do
       expect {
@@ -97,11 +94,11 @@ describe Github::Authorization do
   end
 
   context ".authenticated?" do
-    it { github.should respond_to :authenticated? }
+    it { should respond_to(:authenticated?) }
 
     it "should return false if falied on basic authentication" do
       github.stub(:basic_authed?).and_return false
-      github.authenticated?.should be_false
+      expect(github.authenticated?).to be_false
     end
 
     it "should return true if basic authentication performed" do
@@ -120,7 +117,7 @@ describe Github::Authorization do
       github.stub(:basic_auth?).and_return false
     end
 
-    it { github.should respond_to :basic_authed? }
+    it { should respond_to(:basic_authed?) }
 
     it "should return false if login is missing" do
       github.stub(:login?).and_return false
@@ -130,41 +127,34 @@ describe Github::Authorization do
     it "should return true if login && password provided" do
       github.stub(:login?).and_return true
       github.stub(:password?).and_return true
-      github.basic_authed?.should be_true
+      expect(github.basic_authed?).to be_true
     end
   end
 
   context "authentication" do
-    it "should respond to 'authentication'" do
-      github.should respond_to :authentication
-    end
+    it { should respond_to(:authentication) }
 
     it "should return empty hash if no basic authentication params available" do
       github.stub(:login?).and_return false
       github.stub(:basic_auth?).and_return false
-      github.authentication.should be_empty
+      expect(github.authentication).to be_empty
     end
 
     context 'basic_auth' do
-      before do
-        github = Github.new :basic_auth => 'github:pass'
-      end
+      let(:options) { { :basic_auth => 'github:pass' } }
 
       it "should return hash with basic auth params" do
-        github.authentication.should be_a Hash
-        github.authentication.should have_key :basic_auth
+        expect(github.authentication).to be_a Hash
+        expect(github.authentication).to have_key(:basic_auth)
       end
     end
 
     context 'login & password' do
-      before do
-        github = Github.new :login => 'github', :password => 'pass'
-        github.basic_auth = nil
-      end
+      let(:options) { { :login => 'github', :password => 'pass' } }
 
       it "should return hash with login & password params" do
-        github.authentication.should be_a Hash
-        github.authentication.should have_key :login
+        expect(github.authentication).to be_a Hash
+        expect(github.authentication).to have_key(:login)
       end
     end
   end # authentication
