0f5a87501d1948e2de28308c090f87d29d844bd0
Piotr Murach <pmurach@gmail.com>
2013-02-17 20:20:19 +0000

    Test the api behaviour.

--- spec/integration/gists_spec.rb
+++ spec/integration/gists_spec.rb
@@ -6,6 +6,8 @@ describe Github::Gists do
 
   after { reset_authentication_for subject }
 
+  it_should_behave_like 'api interface'
+
   its(:comments)   { should be_a Github::Gists::Comments }
 
 end # Github::Gists
