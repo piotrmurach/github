4d0748bc7c68dbe777146a9a69f8c5946097906e
Piotr Murach <pmurach@gmail.com>
2013-02-18 22:42:14 +0000

    Awesome release version 0.9.0!

--- lib/github_api/version.rb
+++ lib/github_api/version.rb
@@ -3,8 +3,8 @@
 module Github
   module VERSION
     MAJOR = 0
-    MINOR = 8
-    PATCH = 11
+    MINOR = 9
+    PATCH = 0
     BUILD = nil
 
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.');
