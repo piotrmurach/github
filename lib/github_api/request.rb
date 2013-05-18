b635933ba269abf07772f4cef2d44d7035602d25
Piotr Murach <pmurach@gmail.com>
2013-05-18 22:14:48 +0100

    Change to use params hash data call.

--- lib/github_api/request.rb
+++ lib/github_api/request.rb
@@ -43,22 +43,17 @@ module Github
       response = conn.send(method) do |request|
         case method.to_sym
         when *(METHODS - METHODS_WITH_BODIES)
-          request.body = params.delete('data') if params.has_key?('data')
-          request.url(path, params)
+          request.body = params.data if params.has_key?('data')
+          request.url(path, params.to_hash)
         when *METHODS_WITH_BODIES
           request.path = path
-          request.body = extract_data_from_params(params) unless params.empty?
+          request.body = params.data unless params.empty?
         end
       end
       ResponseWrapper.new(response, self)
     end
 
     private
-
-    def extract_data_from_params(params) # :nodoc:
-      return params['data'] if params.has_key?('data') and !params['data'].nil?
-      return params
-    end
 
     def _extract_mime_type(params, options) # :nodoc:
       options['resource']  = params['resource'] ? params.delete('resource') : ''
