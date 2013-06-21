8a25e52b6640f49a16d242140342be7fb0c9d398
Piotr Murach <pmurach@gmail.com>
2013-06-21 21:59:52 +0100

    Change error status detection.

--- lib/github_api/response/raise_error.rb
+++ lib/github_api/response/raise_error.rb
@@ -7,10 +7,10 @@ module Github
   class Response::RaiseError < Faraday::Response::Middleware
 
     def on_complete(env)
-      status_code = env[:status].to_i
+      status_code   = env[:status].to_i
       service_error = Github::Error::ServiceError
       error_class = service_error.errors[status_code]
-      error_class = service_error if !error_class and (400...600).include? status_code
+      error_class = service_error if !error_class and (400...600) === status_code
       raise error_class.new(env) if error_class
     end
 
