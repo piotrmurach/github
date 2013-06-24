e127a78732653912862ea8eaa4473833c2a488a2
Piotr Murach <pmurach@gmail.com>
2013-06-24 21:36:50 +0100

    Add switch for loading settings.

--- features/support/settings.rb
+++ features/support/settings.rb
@@ -4,4 +4,8 @@ def settings_file
   Pathname.new(File.expand_path("../../settings.yml", __FILE__))
 end
 
-SETTINGS = (File.exists?(settings_file) && yaml= YAML.load_file(settings_file)) ? yaml : {'basic_auth' => 'login:password', 'oauth_token' => 'as79asfd79ads', 'user'=> 'octokat', 'repo' => 'dummy', 'email' => 'email@example.com'}
+def live_credetials?
+  true
+end
+
+SETTINGS = (live_credentials? && File.exists?(settings_file) && yaml= YAML.load_file(settings_file)) ? yaml : {'basic_auth' => 'login:password', 'oauth_token' => 'as79asfd79ads', 'user'=> 'murek', 'repo' => 'dummy', 'email' => 'email@example.com'}
