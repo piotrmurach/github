b24cb2f3e78fbb506d34ada89404761da3c56ced
Piotr Murach <pmurach@gmail.com>
2013-11-10 22:44:44 +0000

    Fix typo.

--- lib/github_api/error/unknown_media.rb
+++ lib/github_api/error/unknown_media.rb
@@ -7,7 +7,7 @@ module Github #:nodoc
       def initialize(file)
         super(
           generate_message(
-            :problem => "Unkown content type for: '#{file}' provided for this request.",
+            :problem => "Unknown content type for: '#{file}' provided for this request.",
             :summary => "Github gem infers the content type of the resource by calling the mime-types gem type inference.",
             :resolution => "Please install mime-types gem to infer the resource content type."
           )
