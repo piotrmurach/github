f13bdd17590b7e0d676248cb882c52012ae21562
Piotr Murach <pmurach@gmail.com>
2013-11-10 20:51:35 +0000

    Add unknown media type error.

---
+++ lib/github_api/error/unknown_media.rb
@@ -0,0 +1,18 @@
+# encoding: utf-8
+
+module Github #:nodoc
+  # Raised when invalid options are passed to a request body
+  module Error
+    class UnknownMedia < ClientError
+      def initialize(file)
+        super(
+          generate_message(
+            :problem => "Unkown content type for: '#{file}' provided for this request.",
+            :summary => "Github gem infers the content type of the resource by calling the mime-types gem type inference.",
+            :resolution => "Please install mime-types gem to infer the resource content type."
+          )
+        )
+      end
+    end
+  end # Error
+end # Github
