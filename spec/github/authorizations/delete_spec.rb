59210d25cf4192d7582a91ace4e7d69438468f20
Piotr Murach <pmurach@gmail.com>
2014-02-22 19:18:36 +0000

    Change auth delete spec.

--- spec/github/authorizations/delete_spec.rb
+++ spec/github/authorizations/delete_spec.rb
@@ -4,29 +4,29 @@ require 'spec_helper'
 
 describe Github::Authorizations, '#delete' do
   let(:basic_auth) { 'login:password' }
-  let(:request_path) { "/authorizations/#{authorization_id}" }
-  let(:host) { "https://#{basic_auth}@api.github.com" }
-  let(:authorization_id) { 1 }
-
-  before {
-    stub_delete(request_path, host).to_return(:body => body, :status => status,
-      :headers => {:content_type => "application/json; charset=utf-8"})
-  }
+  let(:host)       { "https://#{basic_auth}@api.github.com" }
 
   before { subject.basic_auth = basic_auth }
 
   after { reset_authentication_for(subject) }
 
-  context "when user" do
+  context "when an user" do
+    let(:authorization_id) { 1 }
+    let(:request_path) { "/authorizations/#{authorization_id}" }
     let(:body) { '' }
     let(:status) { 204 }
 
-    it "should fail to get resource without basic authentication" do
+    before {
+      stub_delete(request_path, host).to_return(body: body, status: status,
+        headers: {content_type: 'application/json; charset=utf-8'})
+    }
+
+    it "fails to get resource without basic authentication" do
       reset_authentication_for subject
       expect { subject.delete }.to raise_error(ArgumentError)
     end
 
-    it "should delete resource successfully" do
+    it "deletes resource successfully" do
       subject.delete authorization_id
       a_delete(request_path, host).should have_been_made
     end
@@ -35,6 +35,4 @@ describe Github::Authorizations, '#delete' do
       let(:requestable) { subject.delete authorization_id }
     end
   end
-
-
 end # delete
