62c895814ac52a51d8e71a89fec4dc5f1ae76b8a
Piotr Murach <pmurach@gmail.com>
2014-06-29 22:17:07 +0100

    Change arguments parsing to stop modyfying global namespace.

--- lib/github_api/api/arguments.rb
+++ lib/github_api/api/arguments.rb
@@ -2,9 +2,8 @@
 
 module Github
   class API
-    # Request arguments handler
+    # A class responsible for handilng request arguments
     class Arguments
-
       include Normalizer
       include ParameterFilter
       include Validations
@@ -14,50 +13,94 @@ module Github
       # Parameters passed to request
       attr_reader :params
 
+      # The remaining unparsed arguments
       attr_reader :remaining
 
       # The request api
-      #
       attr_reader :api
 
-      # Required arguments
+      # Initialize an Arguments
       #
-      attr_reader :required
-      private :required
-
-      # Optional arguments
+      # @param [Hash] options
       #
-      attr_reader :optional
-      private :optional
-
-      # Takes api, filters and required arguments
+      # @option options [Array[String]] :required
+      #   arguments that must be present before request is fired
       #
-      # = Parameters
-      #  :required - arguments that must be present before request is fired
+      # @option options [Github::API] :api
+      #   the reference to the current api
       #
-      def initialize(api, options={})
+      # @api public
+      def initialize(options = {}, &block)
         normalize! options
-        @api      = api
+
+        @api      = options.fetch('api')
         @required = options.fetch('required', []).map(&:to_s)
         @optional = options.fetch('optional', []).map(&:to_s)
+        @assigns  = {}
+
+        yield_or_eval(&block)
       end
 
-      # Parse arguments to allow for flexible api calls.
+      # Specify required attribute(s)
+      #
+      # @api public
+      def require(*attrs, &block)
+        attrs_clone = attrs.clone
+        @required = Array(attrs_clone)
+        self
+      end
+      alias :required :require
+
+      # Specify optional attribute(s)
+      #
+      # @api public
+      def optional(*attrs, &block)
+      end
+
+      # Hash like access to request arguments
+      #
+      # @param [String, Symbol] property
+      #   the property name
+      #
+      # @api public
+      def [](property)
+        @assigns[property.to_s]
+      end
+
+      def []=(property, value)
+        @assigns[property.to_s] = value
+      end
+
+      def method_missing(method_name, *args, &block)
+        if @assigns.key?(method_name.to_s)
+          self[method_name]
+        else
+          super
+        end
+      end
+
+      def respond_to_missing?(method_name, include_private = false)
+        @assigns.key?(method_name) || super
+      end
+
+      # Parse arguments to allow for flexible api calls
+      #
       # Arguments can be part of parameters hash or be simple string arguments.
       #
+      # @api public
       def parse(*args, &block)
         options = ParamsHash.new(args.extract_options!)
         normalize! options
 
-        if !args.size.zero?
-          parse_arguments *args
+        if args.size.zero?  # Arguments are inside the parameters hash
+          parse_hash(options)
         else
-          # Arguments are inside the parameters hash
-          parse_options options
+          parse_array(*args)
         end
-        @params = options
-        @remaining = extract_remaining(args)
+        @params    = options
+        @remaining = args[@required.size..-1]
         extract_pagination(options)
+
         yield_or_eval(&block)
         self
       end
@@ -67,13 +110,14 @@ module Github
       # = Parameters
       #  :recursive - boolean that toggles whether nested filtering should be applied
       #
-      def sift(keys, key=nil, options={})
+      def permit(keys, key=nil, options={})
         filter! keys, (key.nil? ? params : params[key]), options if keys.any?
         self
       end
 
       # Check if required keys are present inside parameters hash.
       #
+      # @api public
       def assert_required(required)
         assert_required_keys required, params
         self
@@ -81,6 +125,7 @@ module Github
 
       # Check if parameters match expected values.
       #
+      # @api public
       def assert_values(values, key=nil)
         assert_valid_values values, (key.nil? ? params : params[key])
         self
@@ -88,23 +133,99 @@ module Github
 
       private
 
-      # Check and set all requried arguments.
+      # Parse array arguments and assign in order to required properties
       #
-      def parse_arguments(*args)
-        assert_presence_of *args
-        required.each_with_index do |req, indx|
-          api.set req, args[indx]
+      # @param [Array[Object]] args
+      #
+      # @raise ArgumentError
+      #
+      # @return [nil]
+      #
+      # @api public
+      def parse_array(*args)
+        assert_presence_of(*args)
+        @required.each_with_index do |req, indx|
+          @assigns[req] = args[indx]
         end
         check_requirement!(*args)
       end
 
-      # Find remaining arguments
+      # Remove required arguments from parameters and
+      # validate their presence(if not nil or empty string).
       #
-      def extract_remaining(args)
-        args[required.size..-1]
+      # @param [Hash[String]] options
+      #
+      # @return [nil]
+      #
+      # @api private
+      def parse_hash(options)
+        options.each { |key, val| remove_required(options, key, val) }
+        hash = update_required_from_global
+        check_requirement!(*hash.keys)
       end
 
-      # Fine auto_pagination parameter in options hash
+      # Remove required property from hash
+      #
+      # @param [Hash[String]] options
+      #   the options to check
+      #
+      # @param [String] key
+      #   the key to remove
+      #
+      # @param [String] val
+      #   the value to assign
+      #
+      # @api private
+      def remove_required(options, key, val)
+        if @required.include?(key.to_s)
+          assert_presence_of(val)
+          options.delete(key)
+          @assigns[key.to_s] = val
+        end
+      end
+
+      # Update required property from globals if not present
+      #
+      # @return [Hash[String]]
+      #
+      # @api private
+      def update_required_from_global
+        @required.reduce({}) do |hash, property|
+          if @assigns.key?(property)
+            hash[property] = self[property]
+          elsif api_property?(property)
+            hash[property] = api.send(:"#{property}")
+          end
+          hash
+        end
+      end
+
+      # Check if api has non-empty property
+      #
+      # @param [String] property
+      #   the property to check
+      #
+      # @return [Boolean]
+      #
+      # @api private
+      def api_property?(property)
+        api.respond_to?(:"#{property}") && api.send(:"#{property}")
+      end
+
+      # Check if required arguments are present.
+      #
+      #
+      def check_requirement!(*args)
+        args_length     = args.length
+        required_length = @required.length
+
+        if args_length < required_length
+          raise ArgumentError, "wrong number of arguments " \
+                               "(#{args_length} for #{required_length})"
+        end
+      end
+
+      # Find auto_pagination parameter in options hash
       #
       def extract_pagination(options)
         if (value = options.delete(AUTO_PAGINATION))
@@ -112,57 +233,13 @@ module Github
         end
       end
 
-      # Remove required arguments from parameters and
-      # validate their presence(if not nil or empty string).
+      # Evaluate a block
       #
-      def parse_options(options)
-        options.each { |key, val| remove_required(options, key, val) }
-        provided_args = check_assignment!(options)
-        check_requirement!(*provided_args.keys)
-      end
-
-      # Remove required argument from parameters
-      #
-      def remove_required(options, key, val)
-        key = key.to_s
-        if required.include? key
-          assert_presence_of val
-          options.delete key
-          api.set key, val
-        end
-      end
-
-      # Check if required arguments have been set on instance.
-      #
-      def check_assignment!(options)
-        result = required.inject({}) { |hash, arg|
-          if api.respond_to?(:"#{arg}") && (value = api.send(:"#{arg}"))
-            hash[arg] = value
-          end
-          hash
-        }
-        assert_presence_of result
-        result
-      end
-
-      # Check if required arguments are present.
-      #
-      def check_requirement!(*args)
-        args_length     = args.length
-        required_length = required.length
-
-        if args_length < required_length
-          ::Kernel.raise ArgumentError, "wrong number of arguments (#{args_length} for #{required_length})"
-        end
-      end
-
-      # Evaluate block
-      #
+      # @api privte
       def yield_or_eval(&block)
         return unless block
-        block.arity > 0 ? yield(self) : self.instance_eval(&block)
+        block.arity > 0 ? yield(self) : instance_eval(&block)
       end
-
     end # Arguments
   end # Api
 end # Github
--- spec/github/api/arguments/parse_spec.rb
+++ spec/github/api/arguments/parse_spec.rb
@@ -3,30 +3,43 @@
 require 'spec_helper'
 
 describe Github::API::Arguments, '#parse' do
-  let(:api)    { Github::Client::Repos.new }
-  let(:object) { described_class.new api, 'required' => required }
-  let(:arguments) { ['peter-murach', 'github', params] }
-  let(:params) { { :page => 23 } }
-  let(:required) { [:user, :repo] }
 
-  subject { object.parse *arguments }
+  let(:object) { described_class }
+
+  let(:api)    { Github::Client::Repos.new }
+
+
+  subject(:arguments) { object.new(api: api, required: required).parse(*args) }
 
   after { api.user =nil; api.repo = nil }
 
-  context 'with required arguments' do
+  context 'with required' do
+    let(:required) { [:user, :repo] }
+    let(:args)     { ['peter-murach', 'github', {page: 23}] }
 
-    it { should == object }
-
-    its(:params) { should == {"page" => 23} }
-
-    context 'sets parameters' do
-      it { subject.api.user.should == 'peter-murach' }
-
-      it { subject.api.repo.should == 'github' }
+    it "parses the request options" do
+      expect(arguments.params).to eq({"page" => 23})
     end
 
-    context 'with no arguments search parameters hash' do
-      let(:arguments) { nil }
+    it "allows to retrieve user" do
+      expect(arguments['user']).to eq('peter-murach')
+      expect(arguments[:user]).to eq('peter-murach')
+      expect(arguments.user).to eq('peter-murach')
+    end
+
+    it "allows to retrieve repo" do
+      expect(arguments['repo']).to eq('github')
+      expect(arguments[:repo]).to eq('github')
+      expect(arguments.repo).to eq('github')
+    end
+
+    it "reports about unknown property" do
+      expect(arguments['unknown']).to be_nil
+      expect { arguments.unknown}.to raise_error(NoMethodError)
+    end
+
+    context 'when no arguments searches attributes hash' do
+      let(:args) { nil }
 
       it 'asserts lack of presence of hash parameters' do
         expect { subject }.to raise_error(ArgumentError)
@@ -34,35 +47,43 @@ describe Github::API::Arguments, '#parse' do
     end
 
     context 'with nil argument' do
-      let(:arguments) { [nil, 'github', params] }
+      let(:args) { [nil, 'github', {page: 23}] }
 
       it 'raises an error' do
         expect { subject }.to raise_error(ArgumentError, /parameter/)
       end
     end
 
-    context 'with hash arguments' do
-      let(:arguments) { [{:user => 'peter-murach', :repo => 'github'}.merge(params)]}
+    context 'when arguments are inside hash' do
+      let(:args) { [{user: 'peter-murach', repo: 'github', page: 23}] }
 
-      it 'sets parameters' do
-        subject.api.user.should == 'peter-murach'
+      it 'allows to retrieve required parameter' do
+        expect(arguments['user']).to eq('peter-murach')
+        expect(arguments[:user]).to eq('peter-murach')
+        expect(arguments.user).to eq('peter-murach')
+      end
+
+      it "doesn't polute global namespace" do
+        expect(arguments.api.user).to_not eq('peter-murach')
       end
     end
   end
 
-  context 'with less than required arguments' do
+  context 'with insufficient required' do
     let(:required) { [:user, :repo] }
-    let(:arguments) { ['peter-murach', params] }
+    let(:args)     { ['peter-murach', {page: 23}] }
 
     it 'raises an error' do
-      expect { subject }.to raise_error(ArgumentError, /wrong number/)
+      expect { arguments }.to raise_error(ArgumentError, /wrong number/)
     end
   end
 
-  context 'without required arguments' do
+  context 'without required' do
     let(:required) { [] }
-    let(:arguments) { [params] }
+    let(:args)     { [{page: 23}] }
 
-    its(:params) { should == {"page" => 23} }
+    it "parses request options" do
+      expect(arguments.params).to eq({"page" => 23})
+    end
   end
 end
