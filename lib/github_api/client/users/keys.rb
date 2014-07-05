e9e368244d1c9c4433bf58c9d11dbd6fe74f7e79
Piotr Murach <pmurach@gmail.com>
2014-07-05 20:25:47 +0100

    Change user keys docs.

--- lib/github_api/client/users/keys.rb
+++ lib/github_api/client/users/keys.rb
@@ -7,17 +7,20 @@ module Github
 
     # List public keys for the authenticated user
     #
-    # = Examples
+    # @example
     #  github = Github.new oauth_token: '...'
     #  github.users.keys.list
     #  github.users.keys.list { |key| ... }
     #
     # List public keys for the specified user
     #
-    # = Examples
+    # @example
     #  github.users.keys.list user: 'user-name'
     #  github.users.keys.list user: 'user-name' { |key| ... }
     #
+    # @return [Hash]
+    #
+    # @api public
     def list(*args)
       params = arguments(args).params
       response = if (user = params.delete('user'))
@@ -32,61 +35,68 @@ module Github
 
     # Get a single pulic key for the authenticated user
     #
-    # = Examples
+    # @example
     #  github = Github.new oauth_token: '...'
     #  github.users.keys.get 'key-id'
     #
+    # @api public
     def get(*args)
-      arguments(args, :required => [:key_id])
-      get_request("/user/keys/#{key_id}", arguments.params)
+      arguments(args, required: [:id])
+      get_request("/user/keys/#{arguments.id}", arguments.params)
     end
     alias :find :get
 
     # Create a public key for the authenticated user
     #
-    # = Inputs
-    # * <tt>:title</tt> - Required string
-    # * <tt>:key</tt> - Required string. sha key
+    # @param [Hash] params
+    # @option [String] :title
+    #   Required string
+    # @option [String] :key
+    #   Required string. sha key
     #
-    # = Examples
+    # @example
     #  github = Github.new oauth_token: '...'
     #  github.users.keys.create "title": "octocat@octomac", "key": "ssh-rsa AAA..."
     #
+    # @api public
     def create(*args)
       arguments(args) do
-        sift VALID_KEY_PARAM_NAMES
+        permit VALID_KEY_PARAM_NAMES
       end
       post_request("/user/keys", arguments.params)
     end
 
     # Update a public key for the authenticated user
     #
-    # = Inputs
-    # * <tt>:title</tt> - Required string
-    # * <tt>:key</tt> - Required string. sha key
+    # @param [Hash] params
+    # @option [String] :title
+    #   Required string
+    # @option [String] :key
+    #   Required string. sha key
     #
-    # = Examples
+    # @example
     #  github = Github.new oauth_token: '...'
     #  github.users.keys.update 'key-id', "title": "octocat@octomac",
     #    "key": "ssh-rsa AAA..."
     #
+    # @api public
     def update(*args)
-      arguments(args, :required => [:key_id]) do
-        sift VALID_KEY_PARAM_NAMES
+      arguments(args, required: [:id]) do
+        permit VALID_KEY_PARAM_NAMES
       end
-      patch_request("/user/keys/#{key_id}", arguments.params)
+      patch_request("/user/keys/#{arguments.id}", arguments.params)
     end
 
     # Delete a public key for the authenticated user
     #
-    # = Examples
+    # @example
     #  github = Github.new oauth_token: '...'
     #  github.users.keys.delete 'key-id'
     #
+    # @api public
     def delete(*args)
-      arguments(args, :required => [:key_id])
-      delete_request("/user/keys/#{key_id}", arguments.params)
+      arguments(args, required: [:id])
+      delete_request("/user/keys/#{arguments.id}", arguments.params)
     end
-
   end # Users::Keys
 end # Github
