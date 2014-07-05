6f3fc60a4d0facae5af3cdaae1cb58a8b578a1b1
Piotr Murach <pmurach@gmail.com>
2014-07-05 20:54:09 +0100

    Change docs and add ability to check following another user.

--- lib/github_api/client/users/followers.rb
+++ lib/github_api/client/users/followers.rb
@@ -5,18 +5,19 @@ module Github
 
     # List a user's followers
     #
-    # = Examples
+    # @xample
     #  github = Github.new
     #  github.users.followers.list 'user-name'
     #  github.users.followers.list 'user-name' { |user| ... }
     #
     # List the authenticated user's followers
     #
-    # = Examples
+    # @example
     #  github = Github.new oauth_token: '...'
     #  github.users.followers
     #  github.users.followers { |user| ... }
     #
+    # @api public
     def list(*args)
       params = arguments(args).params
 
@@ -32,18 +33,18 @@ module Github
 
     # List who a user is following
     #
-    # = Examples
+    # @example
     #  github = Github.new
     #  github.users.followers.following 'user-name'
     #  github.users.followers.following 'user-name' { |user| ... }
     #
     # List who the authenicated user is following
     #
-    # = Examples
-    #
+    # @example
     #  github = Github.new oauth_token: '...'
     #  github.users.followers.following
     #
+    # @api public
     def following(*args)
       params = arguments(args).params
 
@@ -58,14 +59,27 @@ module Github
 
     # Check if you are following a user
     #
-    # = Examples
-    #  github = Github.new oauth_token: '...'
-    #  github.users.followers.following? 'user-name'
-    #  github.users.followers.following? user: 'user-name'
+    # @example
+    #   github = Github.new oauth_token: '...'
+    #   github.users.followers.following? 'user-name'
+    #   github.users.followers.following? username: 'user-name'
     #
+    # Check if one user follows another
+    #
+    # @example
+    #   github = Github.new oauth_token: '...'
+    #   github.users.followers.following? username: 'user-name',
+    #     target_user: 'target-user-name'
+    #
+    # @api public
     def following?(*args)
-      arguments(args, :required => [:user])
-      get_request("/user/following/#{user}", arguments.params)
+      arguments(args, required: [:username])
+      params = arguments.params
+      if target_user = params.delete('target_user')
+        get_request("/users/#{arguments.username}/following/#{target_user}", params)
+      else
+        get_request("/user/following/#{arguments.username}", params)
+      end
       true
     rescue Github::Error::NotFound
       false
@@ -73,27 +87,27 @@ module Github
 
     # Follow a user
     #
-    # = Examples
+    # @example
     #  github = Github.new oauth_token: '...'
     #  github.users.followers.follow 'user-name'
-    #  github.users.followers.follow user: 'user-name'
+    #  github.users.followers.follow username: 'user-name'
     #
+    # @api public
     def follow(*args)
-      arguments(args, :required => [:user])
-      put_request("/user/following/#{user}", arguments.params)
+      arguments(args, required: [:username])
+      put_request("/user/following/#{arguments.username}", arguments.params)
     end
 
     # Unfollow a user
     #
-    # = Examples
+    # @example
     #  github = Github.new oauth_token: '...'
     #  github.users.followers.unfollow 'user-name'
-    #  github.users.followers.unfollow user: 'user-name'
+    #  github.users.followers.unfollow username: 'user-name'
     #
     def unfollow(*args)
-      arguments(args, :required => [:user])
-      delete_request("/user/following/#{user}", arguments.params)
+      arguments(args, required: [:username])
+      delete_request("/user/following/#{arguments.username}", arguments.params)
     end
-
   end # Users::Followers
 end # Github
