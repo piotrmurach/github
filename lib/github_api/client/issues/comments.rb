3ddab75de0f7bdb38114a7bee11f0d64708cfe26
Piotr Murach <pmurach@gmail.com>
2014-07-05 21:35:47 +0100

    Change owner to user to be consistent.

--- lib/github_api/client/issues/comments.rb
+++ lib/github_api/client/issues/comments.rb
@@ -35,13 +35,13 @@ module Github
     #
     # @api public
     def list(*args)
-      arguments(args, required: [:owner, :repo])
+      arguments(args, required: [:user, :repo])
       params = arguments.params
 
       response = if (number = params.delete('number'))
-        get_request("/repos/#{arguments.owner}/#{arguments.repo}/issues/#{number}/comments", params)
+        get_request("/repos/#{arguments.user}/#{arguments.repo}/issues/#{number}/comments", params)
       else
-        get_request("/repos/#{arguments.owner}/#{arguments.repo}/issues/comments", params)
+        get_request("/repos/#{arguments.user}/#{arguments.repo}/issues/comments", params)
       end
       return response unless block_given?
       response.each { |el| yield el }
@@ -58,10 +58,10 @@ module Github
     #  github.issues.comments.find owner: 'user-name', repo: 'repo-name', id: 'id'
     #
     def get(*args)
-      arguments(args, required: [:owner, :repo, :id])
+      arguments(args, required: [:user, :repo, :id])
       params = arguments.params
 
-      get_request("/repos/#{arguments.owner}/#{arguments.repo}/issues/comments/#{arguments.id}", params)
+      get_request("/repos/#{arguments.user}/#{arguments.repo}/issues/comments/#{arguments.id}", params)
     end
     alias :find :get
 
@@ -73,25 +73,25 @@ module Github
     #
     # @example
     #  github = Github.new
-    #  github.issues.comments.create 'owner-name', 'repo-name', 'number',
+    #  github.issues.comments.create 'user-name', 'repo-name', 'number',
     #     body: 'a new comment'
     #
     # @example
     #   github.issues.comments.edit
-    #     owner: 'owner-name',
+    #     user: 'owner-name',
     #     repo: 'repo-name',
     #     id: 'comment-id',
     #     body: 'a new comment body'
     #
     # @api public
     def create(*args)
-      arguments(args, required: [:owner, :repo, :number]) do
+      arguments(args, required: [:user, :repo, :number]) do
         permit VALID_ISSUE_COMMENT_PARAM_NAME
         assert_required %w[ body ]
       end
       params = arguments.params
 
-      post_request("/repos/#{arguments.owner}/#{arguments.repo}/issues/#{arguments.number}/comments", params)
+      post_request("/repos/#{arguments.user}/#{arguments.repo}/issues/#{arguments.number}/comments", params)
     end
 
     # Edit a comment
@@ -107,19 +107,19 @@ module Github
     #
     # @example
     #   github.issues.comments.edit
-    #     owner: 'owner-name',
+    #     user: 'owner-name',
     #     repo: 'repo-name',
     #     id: 'comment-id',
     #     body: 'a new comment body'
     #
     # @api public
     def edit(*args)
-      arguments(args, required: [:owner, :repo, :id]) do
+      arguments(args, required: [:user, :repo, :id]) do
         permit VALID_ISSUE_COMMENT_PARAM_NAME
         assert_required %w[ body ]
       end
 
-      patch_request("/repos/#{arguments.owner}/#{arguments.repo}/issues/comments/#{}", arguments.params)
+      patch_request("/repos/#{arguments.user}/#{arguments.repo}/issues/comments/#{}", arguments.params)
     end
 
     # Delete a comment
@@ -130,15 +130,15 @@ module Github
     #
     # @example
     #   github.issues.comments.delete
-    #     owner: 'owner-name',
+    #     user: 'owner-name',
     #     repo: 'repo-name',
     #     id: 'comment-id',
     #
     # @api public
     def delete(*args)
-      arguments(args, required: [:owner, :repo, :id])
+      arguments(args, required: [:user, :repo, :id])
 
-      delete_request("/repos/#{arguments.owner}/#{arguments.repo}/issues/comments/#{arguments.id}", arguments.params)
+      delete_request("/repos/#{arguments.user}/#{arguments.repo}/issues/comments/#{arguments.id}", arguments.params)
     end
   end # Issues::Comments
 end # Github
