46b28dfd19351f1c27ba5bc9ecf20adde085111c
Piotr Murach <pmurach@gmail.com>
2014-07-12 22:38:30 +0100

    Change tags api to use new arguments.

--- lib/github_api/client/git_data/tags.rb
+++ lib/github_api/client/git_data/tags.rb
@@ -24,55 +24,71 @@ module Github
 
     # Get a tag
     #
-    # = Examples
+    # @example
     #  github = Github.new
     #  github.git_data.tags.get 'user-name', 'repo-name', 'sha'
     #
+    # @api public
     def get(*args)
-      arguments(args, :required => [:user, :repo, :sha])
+      arguments(args, required: [:user, :repo, :sha])
       params = arguments.params
 
-      get_request("/repos/#{user}/#{repo}/git/tags/#{sha}", params)
+      get_request("/repos/#{arguments.user}/#{arguments.repo}/git/tags/#{arguments.sha}", params)
     end
     alias :find :get
 
     # Create a tag object
+    #
     # Note that creating a tag object does not create the reference that
     # makes a tag in Git. If you want to create an annotated tag in Git,
     # you have to do this call to create the tag object, and then create
-    # the <tt>refs/tags/[tag]</tt> reference. If you want to create a lightweight tag, you simply have to create the reference - this call would be unnecessary.
+    # the refs/tags/[tag] reference. If you want to create a lightweight
+    # tag, you simply have to create the reference -
+    # this call would be unnecessary.
     #
-    # = Parameters
-    # * <tt>:tag</tt> - String of the tag
-    # * <tt>:message</tt> - String of the tag message
-    # * <tt>:object</tt> - String of the SHA of the git object this is tagging
-    # * <tt>:type</tt> - String of the type of the object we're tagging. Normally this is a <tt>commit</tt> but it can also be a <tt>tree</tt> or a <tt>blob</tt>
-    # * tagger.name:: String of the name of the author of the tag
-    # * tagger.email:: String of the email of the author of the tag
-    # * tagger.date:: Timestamp of when this object was tagged
+    # @param [Hash] params
+    # @input params [String] :tag
+    #   The tag
+    # @input params [String] :message
+    #   The tag message
+    # @input params [String] :object
+    #   The SHA of the git object this is tagging
+    # @input params [String] :type
+    #   The type of the object we're tagging.
+    #   Normally this is a commit but it can also be a tree or a blob
+    # @input params [Hash] :tagger
+    #   A hash with information about the individual creating the tag.
     #
-    # = Examples
+    # The tagger hash contains the following keys:
+    # @input tagger [String] :name
+    #   The name of the author of the tag
+    # @input tagger [String] :email
+    #   The email of the author of the tag
+    # @input tagger [String] :date
+    #   When this object was tagged. This is a timestamp in ISO 8601
+    #   format: YYYY-MM-DDTHH:MM:SSZ.
+    #
+    # @xample
     #  github = Github.new
     #  github.git_data.tags.create 'user-name', 'repo-name',
-    #    "tag" => "v0.0.1",
-    #    "message" => "initial version\n",
-    #    "type" => "commit",
-    #    "object" => "c3d0be41ecbe669545ee3e94d31ed9a4bc91ee3c",
-    #    "tagger" => {
-    #      "name" => "Scott Chacon",
-    #      "email" => "schacon@gmail.com",
-    #      "date" => "2011-06-17T14:53:3"
-    #    }
+    #     tag: "v0.0.1",
+    #     message: "initial version\n",
+    #     type: "commit",
+    #     object: "c3d0be41ecbe669545ee3e94d31ed9a4bc91ee3c",
+    #     tagger: {
+    #       name: "Scott Chacon",
+    #       email: "schacon@gmail.com",
+    #       date: "2011-06-17T14:53:3"
+    #     }
     #
+    # @api public
     def create(*args)
-      arguments(args, :required => [:user, :repo]) do
-        sift VALID_TAG_PARAM_NAMES
+      arguments(args, required: [:user, :repo]) do
+        permit VALID_TAG_PARAM_NAMES
         assert_values VALID_TAG_PARAM_VALUES
       end
-      params = arguments.params
 
-      post_request("/repos/#{user}/#{repo}/git/tags", params)
+      post_request("/repos/#{arguments.user}/#{arguments.repo}/git/tags", arguments.params)
     end
-
   end # GitData::Tags
 end # Github
