2334dfa1835a6dbc3d1160c4c94cd2ec824da3aa
Piotr Murach <pmurach@gmail.com>
2014-07-12 23:16:45 +0100

    Change organizations api to use new parser.

--- lib/github_api/client/orgs.rb
+++ lib/github_api/client/orgs.rb
@@ -23,15 +23,17 @@ module Github
 
     # List all public organizations for a user.
     #
-    # = Examples
-    #  github = Github.new
-    #  github.orgs.list user: 'user-name'
+    # @example
+    #   github = Github.new
+    #   github.orgs.list user: 'user-name'
     #
     # List public and private organizations for the authenticated user.
     #
-    #  github = Github.new oauth_token: '..'
-    #  github.orgs.list
+    # @example
+    #   github = Github.new oauth_token: '..'
+    #   github.orgs.list
     #
+    # @api public
     def list(*args)
       params = arguments(args).params
 
@@ -48,43 +50,49 @@ module Github
 
     # Get properties for a single organization
     #
-    # = Examples
+    # @example
     #  github = Github.new
     #  github.orgs.get 'github'
     #
+    # @api public
     def get(*args)
-      arguments(args, :required => [:org_name])
+      arguments(args, required: [:org])
 
-      get_request("/orgs/#{org_name}", arguments.params)
+      get_request("/orgs/#{arguments.org}", arguments.params)
     end
     alias :find :get
 
     # Edit organization
     #
-    # = Parameters
-    # <tt>:billing_email</tt> - Optional string - Billing email address. This address is not publicized.
-    # <tt>:company</tt> - Optional string
-    # <tt>:email</tt> - Optional string
-    # <tt>:location</tt> - Optional string
-    # <tt>:name</tt> - Optional string
+    # @param [Hash] params
+    # @input params [String] :billing_email
+    #   Billing email address. This address is not publicized.
+    # @input params [String] :company
+    #   The company name
+    # @input params [String] :email
+    #   The publicly visible email address
+    # @input params [String] :location
+    #   The location
+    # @input params [String] :name
+    #   The shorthand name of the company.
     #
-    # = Examples
+    # @example
     #  github = Github.new oauth_token: '...'
     #  github.orgs.edit 'github',
-    #    "billing_email": "support@github.com",
-    #    "blog": "https://github.com/blog",
-    #    "company": "GitHub",
-    #    "email": "support@github.com",
-    #    "location": "San Francisco",
-    #    "name": "github"
+    #    billing_email: "support@github.com",
+    #    blog: "https://github.com/blog",
+    #    company: "GitHub",
+    #    email: "support@github.com",
+    #    location: "San Francisco",
+    #    name: "github"
     #
+    # @api public
     def edit(*args)
-      arguments(args, :required => [:org_name]) do
-        sift VALID_ORG_PARAM_NAMES
+      arguments(args, required: [:org]) do
+        permit VALID_ORG_PARAM_NAMES
       end
 
-      patch_request("/orgs/#{org_name}", arguments.params)
+      patch_request("/orgs/#{arguments.org}", arguments.params)
     end
-
   end # Orgs
 end # Github
