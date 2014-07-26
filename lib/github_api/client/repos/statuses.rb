11989a0f110ad863266135f0ce4e0dca80181c9b
Piotr Murach <pmurach@gmail.com>
2014-07-26 18:39:19 +0100

    Change repository statuses to use new parser. Add ability to list combined status. Update docs. Close issue #162

--- lib/github_api/client/repos/statuses.rb
+++ lib/github_api/client/repos/statuses.rb
@@ -10,22 +10,39 @@ module Github
       state
       target_url
       description
+      context
     ].freeze # :nodoc:
 
     REQUIRED_PARAMS = %w[ state ].freeze # :nodoc:
 
     # List Statuses for a specific SHA
     #
-    # = Examples
-    #  github = Github.new
-    #  github.repos.statuses.list 'user-name', 'repo-name', 'sha'
-    #  github.repos.statuses.list 'user-name', 'repo-name', 'sha' { |status| ... }
+    # @param [String] :ref
+    #   Ref to fetch the status for. It can be a SHA, a branch name,
+    #   or a tag name.
     #
+    # @example
+    #   github = Github.new
+    #   github.repos.statuses.list 'user-name', 'repo-name', 'ref'
+    #   github.repos.statuses.list 'user-name', 'repo-name', 'ref' { |status| ... }
+    #
+    # Get the combined Status for a specific Ref
+    #
+    # @example
+    #   github = Github.new
+    #   github.repos.statuses.list 'user', 'repo', 'ref', combined: true
+    #
+    # @api public
     def list(*args)
-      arguments(args, :required => [:user, :repo, :sha])
+      arguments(args, required: [:user, :repo, :ref])
       params = arguments.params
+      user, repo, ref = arguments.user, arguments.repo, arguments.ref
 
-      response = get_request("/repos/#{user}/#{repo}/statuses/#{sha}", params)
+      response = if params.delete('combined')
+        get_request("/repos/#{user}/#{repo}/commits/#{ref}/status", params)
+      else
+        get_request("/repos/#{user}/#{repo}/commits/#{ref}/statuses", params)
+      end
       return response unless block_given?
       response.each { |el| yield el }
     end
@@ -33,29 +50,40 @@ module Github
 
     # Create a status
     #
-    # = Inputs
-    # * <tt>:state</tt> - Required string - State of the status - can be one of
-    #                                       pending, success, error, or failure.
-    # * <tt>:target_url</tt> - Optional string - Target url to associate with this
-    #         status. This URL will be linked from the GitHub UI to allow users
-    #         to easily see the ‘source’ of the Status.
-    # * <tt>:description</tt> - Optional string - Short description of the status
+    # @param [Hash] params
+    # @input params [String] :state
+    #   Required. The state of the status. Can be one of pending,
+    #   success, error, or failure.
+    # @input params [String] :target_url
+    #   The target URL to associate with this status. This URL will
+    #   be linked from the GitHub UI to allow users to easily see
+    #   the ‘source’ of the Status.
     #
-    # = Examples
-    #  github = Github.new
-    #  github.repos.statuses.create 'user-name', 'repo-name', 'sha',
-    #    "state" =>  "success",
-    #    "target_url" => "http://ci.example.com/johndoe/my-repo/builds/sha",
-    #    "description" => "Successful build #3 from origin/master"
+    #   For example, if your Continuous Integration system is posting
+    #   build status, you would want to provide the deep link for
+    #   the build output for this specific SHA:
+    #   http://ci.example.com/user/repo/build/sha.
+    # @input params [String] :description
+    #   A short description of the status
+    # @input params [String] :context
+    #   A string label to differentiate this status from the
+    #   status of other systems. Default: "default"
     #
+    # @example
+    #   github = Github.new
+    #   github.repos.statuses.create 'user-name', 'repo-name', 'sha',
+    #     state:  "success",
+    #     target_url: "http://ci.example.com/johndoe/my-repo/builds/sha",
+    #     description: "Successful build #3 from origin/master"
+    #
+    # @api public
     def create(*args)
-      arguments(args, :required => [:user, :repo, :sha]) do
-        sift VALID_STATUS_PARAM_NAMES, :recursive => false
+      arguments(args, required: [:user, :repo, :sha]) do
+        permit VALID_STATUS_PARAM_NAMES, recursive: false
         assert_required REQUIRED_PARAMS
       end
-      params = arguments.params
 
-      post_request("/repos/#{user}/#{repo}/statuses/#{sha}", params)
+      post_request("/repos/#{arguments.user}/#{arguments.repo}/statuses/#{arguments.sha}", arguments.params)
     end
   end # Client::Repos::Statuses
 end # Github
