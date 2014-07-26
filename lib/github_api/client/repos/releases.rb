ba8f3094e6f4c9e47e33c421ca92b90cef7ba6ba
Piotr Murach <pmurach@gmail.com>
2014-07-26 17:45:26 +0100

    Change repository releases api to use new parser. Update docs.

--- lib/github_api/client/repos/releases.rb
+++ lib/github_api/client/repos/releases.rb
@@ -24,14 +24,16 @@ module Github
     # (i.e., published releases and draft releases). Users with pull access
     # will receive published releases only.
     #
-    # = Examples
-    #  github = Github.new
-    #  github.repos.releases.list 'owner', 'repo'
-    #  github.repos.releases.list 'owner', 'repo' { |release| ... }
+    # @example
+    #   github = Github.new
+    #   github.repos.releases.list 'owner', 'repo'
+    #   github.repos.releases.list 'owner', 'repo' { |release| ... }
     #
+    # @api public
     def list(*args)
-      params = arguments(args, required: [:owner, :repo]).params
-      response = get_request("/repos/#{owner}/#{repo}/releases", params)
+      arguments(args, required: [:owner, :repo]).params
+
+      response = get_request("/repos/#{arguments.owner}/#{arguments.repo}/releases", arguments.params)
       return response unless block_given?
       response.each { |el| yield el }
     end
@@ -39,87 +41,97 @@ module Github
 
     # Get a single release
     #
-    # = Examples
-    #  github = Github.new
-    #  github.repos.releases.get 'owner', 'repo', 'id'
+    # @example
+    #   github = Github.new
+    #   github.repos.releases.get 'owner', 'repo', 'id'
     #
+    # @api public
     def get(*args)
-      params = arguments(args, required: [:owner, :repo, :id]).params
-      get_request("/repos/#{owner}/#{repo}/releases/#{id}" , params)
+      arguments(args, required: [:owner, :repo, :id]).params
+      get_request("/repos/#{arguments.owner}/#{arguments.repo}/releases/#{arguments.id}" , arguments.params)
     end
     alias :find :get
 
     # Create a release
     #
-    # = Inputs
-    # * <tt>:tag_name</tt> - Required string
-    # * <tt>:target_commitish</tt> - Optional string - Specifies the commitish
-    #       value that determines where the Git tag is created from. Can be
-    #       any branch or commit SHA. Defaults to the repository's default
-    #       branch (usually 'master'). Unused if the Git tag already exists.
-    # * <tt>:name</tt> - Optional string
-    # * <tt>:body</tt> - Optional string
-    # * <tt>:draft</tt> - Optional boolean - <tt>true</tt> to create a draft
-    #                    (unpublished) release, <tt>false</tt> to create
-    #                     a published one. Default is false.
-    # * <tt>:prerelease</tt> - Optional boolean - <tt>true</tt> to identify
-    #                       the release as a prerelease. false to identify
-    #                       the release as a full release. Default is false.
+    # @param [Hash] params
+    # @input params [String] :tag_name
+    #   Required. The name of the tag.
+    # @input params [String] :target_commitish
+    #   Specifies the commitish value that determines where the Git tag
+    #   is created from. Can be any branch or commit SHA. Defaults to
+    #   the repository's default branch (usually 'master').
+    #   Unused if the Git tag already exists.
+    # @input params [String] :name
+    #   The name of the release.
+    # @input params [String] :body
+    #   Text describing the contents of the tag.
+    # @input params [Boolean] :draft
+    #   true to create a draft (unpublished) release,
+    #   false to create a published one. Default: false
+    # @input params [Boolean] :prerelease
+    #   true to identify the release as a prerelease.
+    #   false to identify the release as a full release. Default: false
     #
-    # = Examples
-    #  github = Github.new
-    #  github.repos.releases.create 'owner', 'repo', 'tag-name',
-    #    "tag_name": "v1.0.0",
-    #    "target_commitish": "master",
-    #    "name": "v1.0.0",
-    #    "body": "Description of the release",
-    #    "draft": false,
-    #    "prerelease": false
+    # @example
+    #   github = Github.new
+    #   github.repos.releases.create 'owner', 'repo', 'tag-name',
+    #     tag_name: "v1.0.0",
+    #     target_commitish: "master",
+    #     name: "v1.0.0",
+    #     body: "Description of the release",
+    #     draft: false,
+    #     prerelease: false
     #
+    # @api public
     def create(*args)
       arguments(args, required: [:owner, :repo, :tag_name]) do
-        sift VALID_RELEASE_PARAM_NAMES
+        permit VALID_RELEASE_PARAM_NAMES
       end
       params = arguments.params
-      params['tag_name'] = tag_name
+      params['tag_name'] = arguments.tag_name
 
-      post_request("/repos/#{owner}/#{repo}/releases", params)
+      post_request("/repos/#{arguments.owner}/#{arguments.repo}/releases", params)
     end
 
     # Edit a release
     #
-    # = Inputs
-    # * <tt>:tag_name</tt> - Required string
-    # * <tt>:target_commitish</tt> - Optional string - Specifies the commitish
-    #       value that determines where the Git tag is created from. Can be
-    #       any branch or commit SHA. Defaults to the repository's default
-    #       branch (usually 'master'). Unused if the Git tag already exists.
-    # * <tt>:name</tt> - Optional string
-    # * <tt>:body</tt> - Optional string
-    # * <tt>:draft</tt> - Optional boolean - <tt>true</tt> to create a draft
-    #                    (unpublished) release, <tt>false</tt> to create
-    #                     a published one. Default is false.
-    # * <tt>:prerelease</tt> - Optional boolean - <tt>true</tt> to identify
-    #                       the release as a prerelease. false to identify
-    #                       the release as a full release. Default is false.
+    # @param [Hash] params
+    # @input params [String] :tag_name
+    #   Required. The name of the tag.
+    # @input params [String] :target_commitish
+    #   Specifies the commitish value that determines where the Git tag
+    #   is created from. Can be any branch or commit SHA. Defaults to
+    #   the repository's default branch (usually 'master').
+    #   Unused if the Git tag already exists.
+    # @input params [String] :name
+    #   The name of the release.
+    # @input params [String] :body
+    #   Text describing the contents of the tag.
+    # @input params [Boolean] :draft
+    #   true to create a draft (unpublished) release,
+    #   false to create a published one. Default: false
+    # @input params [Boolean] :prerelease
+    #   true to identify the release as a prerelease.
+    #   false to identify the release as a full release. Default: false
     #
-    # = Examples
-    #  github = Github.new
-    #  github.repos.releases.edit 'owner', 'repo', 'id',
-    #    "tag_name": "v1.0.0",
-    #    "target_commitish": "master",
-    #    "name": "v1.0.0",
-    #    "body": "Description of the release",
-    #    "draft": false,
-    #    "prerelease": false
+    # @example
+    #   github = Github.new
+    #   github.repos.releases.edit 'owner', 'repo', 'id',
+    #     tag_name: "v1.0.0",
+    #     target_commitish: "master",
+    #     name: "v1.0.0",
+    #     body: "Description of the release",
+    #     draft: false,
+    #     prerelease: false
     #
+    # @api public
     def edit(*args)
       arguments(args, required: [:owner, :repo, :id]) do
-        sift VALID_RELEASE_PARAM_NAMES
+        permit VALID_RELEASE_PARAM_NAMES
       end
-      params = arguments.params
 
-      patch_request("/repos/#{owner}/#{repo}/releases/#{id}", params)
+      patch_request("/repos/#{arguments.owner}/#{arguments.repo}/releases/#{arguments.id}", arguments.params)
     end
     alias :update :edit
 
@@ -127,14 +139,15 @@ module Github
     #
     # Users with push access to the repository can delete a release.
     #
-    # = Examples
-    #  github = Github.new
-    #  github.repos.releases.delete 'owner', 'repo', 'id'
+    # @example
+    #   github = Github.new
+    #   github.repos.releases.delete 'owner', 'repo', 'id'
     #
+    # @api public
     def delete(*args)
-      params = arguments(args, required: [:owner, :repo, :id]).params
+      arguments(args, required: [:owner, :repo, :id]).params
 
-      delete_request("/repos/#{owner}/#{repo}/releases/#{id}", params)
+      delete_request("/repos/#{arguments.owner}/#{arguments.repo}/releases/#{arguments.id}", arguments.params)
     end
   end # Client::Repos::Statuses
 end # Github
