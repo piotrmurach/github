e2110136e8cd7f007eab6f725b2edac701b63ec2
Piotr Murach <pmurach@gmail.com>
2014-07-26 14:33:07 +0100

    Change repository deploy keys api to use new parser.

--- lib/github_api/client/repos/keys.rb
+++ lib/github_api/client/repos/keys.rb
@@ -7,18 +7,20 @@ module Github
 
     # List deploy keys
     #
-    # = Examples
-    #  github = Github.new
-    #  github.repos.keys.list 'user-name', 'repo-name'
-    #  github.repos.keys.list 'user-name', 'repo-name' { |key| ... }
+    # @example
+    #   github = Github.new
+    #   github.repos.keys.list 'user-name', 'repo-name'
+    #   github.repos.keys.list 'user-name', 'repo-name' { |key| ... }
     #
-    #  keys = Github::Repos::Keys.new user: 'user-name', repo: 'repo-name'
-    #  keys.list
+    # @example
+    #   keys = Github::Repos::Keys.new user: 'user-name', repo: 'repo-name'
+    #   keys.list
     #
+    # @api public
     def list(*args)
-      arguments(args, :required => [:user, :repo])
+      arguments(args, required: [:user, :repo])
 
-      response = get_request("/repos/#{user}/#{repo}/keys", arguments.params)
+      response = get_request("/repos/#{arguments.user}/#{arguments.repo}/keys", arguments.params)
       return response unless block_given?
       response.each { |el| yield el }
     end
@@ -26,70 +28,54 @@ module Github
 
     # Get a key
     #
-    # = Examples
-    #  github = Github.new
-    #  github.repos.keys.get 'user-name', 'repo-name', 'key-id'
+    # @example
+    #   github = Github.new
+    #   github.repos.keys.get 'user-name', 'repo-name', 'key-id'
     #
+    # @api public
     def get(*args)
-      arguments(args, :required => [:user, :repo, :key_id])
+      arguments(args, :required => [:user, :repo, :id])
 
-      get_request("/repos/#{user}/#{repo}/keys/#{key_id}", arguments.params)
+      get_request("/repos/#{arguments.user}/#{arguments.repo}/keys/#{arguments.id}", arguments.params)
     end
     alias :find :get
 
     # Create a key
     #
-    # = Inputs
-    # * <tt>:title</tt> - Required string.
-    # * <tt>:key</tt> - Required string.
+    # @param [Hash] params
+    # @option params [String] :title
+    #   Required string.
+    # @option params [String] :key
+    #   Required string.
     #
-    # = Examples
-    #  github = Github.new
-    #  github.repos.keys.create 'user-name', 'repo-name',
-    #    "title" => "octocat@octomac",
-    #    "key" =>  "ssh-rsa AAA..."
+    # @example
+    #   github = Github.new
+    #   github.repos.keys.create 'user-name', 'repo-name',
+    #     title: "octocat@octomac",
+    #     key:  "ssh-rsa AAA..."
     #
+    # @api public
     def create(*args)
-      arguments(args, :required => [:user, :repo]) do
-        sift VALID_KEY_OPTIONS
+      arguments(args, required: [:user, :repo]) do
+        permit VALID_KEY_OPTIONS
         assert_required VALID_KEY_OPTIONS
       end
 
-      post_request("/repos/#{user}/#{repo}/keys", arguments.params)
-    end
-
-    # Edit a key
-    #
-    # = Inputs
-    # * <tt>:title</tt> - Required string.
-    # * <tt>:key</tt> - Required string.
-    #
-    # = Examples
-    #  github = Github.new
-    #  github.repos.keys.edit 'user-name', 'repo-name',
-    #    "title" => "octocat@octomac",
-    #    "key" =>  "ssh-rsa AAA..."
-    #
-    def edit(*args)
-      arguments(args, :required => [:user, :repo, :key_id]) do
-        sift VALID_KEY_OPTIONS
-      end
-      params = arguments.params
-
-      patch_request("/repos/#{user}/#{repo}/keys/#{key_id}", params)
+      post_request("/repos/#{arguments.user}/#{arguments.repo}/keys", arguments.params)
     end
 
     # Delete key
     #
-    # = Examples
-    #  github = Github.new
-    #  github.repos.keys.delete 'user-name', 'repo-name', 'key-id'
+    # @example
+    #   github = Github.new
+    #   github.repos.keys.delete 'user-name', 'repo-name', 'key-id'
     #
+    # @api public
     def delete(*args)
-      arguments(args, :required => [:user, :repo, :key_id])
+      arguments(args, required: [:user, :repo, :id])
       params = arguments.params
 
-      delete_request("/repos/#{user}/#{repo}/keys/#{key_id}", params)
+      delete_request("/repos/#{arguments.user}/#{arguments.repo}/keys/#{arguments.id}", params)
     end
   end # Client::Repos::Keys
 end # Github
