a87f1648995ef82cfaee404bbdc540061aa0a236
Piotr Murach <pmurach@gmail.com>
2014-07-26 19:02:00 +0100

    Change repository new search api to use new parser. Update docs.

--- lib/github_api/client/search.rb
+++ lib/github_api/client/search.rb
@@ -7,8 +7,6 @@ module Github
   class Client::Search < API
     include Github::Utils::Url
 
-    PREVIEW_MEDIA = 'application/vnd.github.preview'.freeze # :nodoc:
-
     require_all 'github_api/client/search', 'legacy'
 
     # Access to Search::Legacy API
@@ -19,24 +17,28 @@ module Github
     # Find issues by state and keyword.
     # (This method returns up to 100 results per page.)
     #
-    # = Parameters
-    #  <tt>:q</tt> - The search terms. This can be any combination of the
-    #                supported issue search parameters.
-    #  <tt>:sort</tt> - Optional sort field. One of comments, created, or
-    #                   updated. If not provided, results are sorted by
-    #                   best match.
-    #  <tt>:order</tt> - Optional Sort order if sort parameter is provided.
-    #                    One of asc or desc; the default is desc.
+    # @param [Hash] params
+    # @option params [String] :q
+    #   The search terms. This can be any combination of the supported
+    #   issue search parameters.
+    # @option params [String] :sort
+    #   Optional sort field. One of comments, created, or updated.
+    #   If not provided, results are sorted by  best match.
+    # @option params [String] :order
+    #  The sort order if sort parameter is provided.
+    #  One of asc or desc. Default: desc
     #
-    # = Examples
-    #  github = Github.new
-    #  github.search.legacy.issues 'query'
-    #  github.search.legacy.issues q: 'query'
+    # @example
+    #   github = Github.new
+    #   github.search.issues 'query'
     #
+    # @example
+    #   github.search.issues q: 'query'
+    #
+    # @api public
     def issues(*args)
       params = arguments(args, required: [:q]).params
-      params['q']      ||= q
-      params['accept'] ||= PREVIEW_MEDIA
+      params['q']      ||= arguments.q
 
       get_request('/search/issues' , params)
     end
@@ -46,23 +48,27 @@ module Github
     # Find repositories via various criteria.
     # (This method returns up to 100 results per page.)
     #
-    # = Parameters
-    #  <tt>:q</tt> - The search terms. This can be any combination of the
-    #                supported issue search parameters.
-    #  <tt>:sort</tt> - Optional sort field. One of stars, forks, or updated.
-    #                   If not provided, results are sorted by best match.
-    #  <tt>:order</tt> - Optional Sort order if sort parameter is provided.
-    #                    One of asc or desc; the default is desc.
+    # @param [Hash] params
+    # @option params [String] :q
+    #   The search keywords, as well as any qualifiers.
+    # @option params [String] :sort
+    #   The sort field. One of stars, forks, or updated.
+    #   Default: results are sorted by best match.
+    # @option params [String] :order
+    #   The sort order if sort parameter is provided.
+    #   One of asc or desc. Default: desc
     #
-    # = Examples
-    #  github = Github.new
-    #  github.search.repos 'query'
-    #  github.search.repos q: 'query'
+    # @example
+    #   github = Github.new
+    #   github.search.repos 'query'
     #
+    # @example
+    #   github.search.repos q: 'query'
+    #
+    # @api public
     def repos(*args)
       params = arguments(args, required: [:q]).params
-      params['q']      ||= q
-      params['accept'] ||= PREVIEW_MEDIA
+      params['q'] ||= arguments.q
 
       get_request('/search/repositories', arguments.params)
     end
@@ -72,22 +78,25 @@ module Github
     #
     # Find users by keyword.
     #
-    # = Parameters
-    #  <tt>:q</tt> - The search terms. This can be any combination of the
-    #                supported issue search parameters.
-    #  <tt>:sort</tt> - Optional sort field. One of followers, repositories, or
-    #                   If not provided, results are sorted by best match.
-    #  <tt>:order</tt> - Optional Sort order if sort parameter is provided.
-    #                    One of asc or desc; the default is desc.
+    # @param [Hash] params
+    # @option params [String] :q
+    #   The search terms. This can be any combination of the supported
+    #   issue search parameters.
+    # @option params [String] :sort
+    #   Optional sort field. One of comments, created, or updated.
+    #   If not provided, results are sorted by  best match.
+    # @option params [String] :order
+    #  The sort order if sort parameter is provided.
+    #  One of asc or desc. Default: desc
     #
-    # = Examples
-    #  github = Github.new
-    #  github.search.users keyword: 'wycats'
+    # @example
+    #   github = Github.new
+    #   github.search.users q: 'wycats'
     #
+    # @api public
     def users(*args)
       params = arguments(args, required: [:q]).params
-      params['q']      ||= q
-      params['accept'] ||= PREVIEW_MEDIA
+      params['q'] ||= arguments.q
 
       get_request('/search/users', arguments.params)
     end
@@ -95,27 +104,27 @@ module Github
     # Find file contents via various criteria.
     # (This method returns up to 100 results per page.)
     #
-    # = Parameters
-    #  <tt>:q</tt> - The search terms. This can be any combination of the
-    #                supported issue search parameters.
-    #  <tt>:sort</tt> - Optional sort field. Can only be indexed, which
-    #                   indicates how recently a file has been indexed
-    #                   by the GitHub search infrastructure. If not provided,
-    #                   results are sorted by best match.
-    #  <tt>:order</tt> - Optional Sort order if sort parameter is provided.
-    #                    One of asc or desc; the default is desc.
+    # @param [Hash] params
+    # @option params [String] :q
+    #   The search terms. This can be any combination of the supported
+    #   issue search parameters.
+    # @option params [String] :sort
+    #   Optional sort field. One of comments, created, or updated.
+    #   If not provided, results are sorted by  best match.
+    # @option params [String] :order
+    #  The sort order if sort parameter is provided.
+    #  One of asc or desc. Default: desc
     #
-    # = Examples
-    #  github = Github.new
-    #  github.search.code email: 'wycats'
+    # @example
+    #   github = Github.new
+    #   github.search.code q: 'wycats'
     #
+    # @api public
     def code(*args)
       params = arguments(args, required: [:q]).params
-      params['q']      ||= q
-      params['accept'] ||= PREVIEW_MEDIA
+      params['q'] ||= arguments.q
 
       get_request('/search/code', params)
     end
-
   end # Search
 end # Github
