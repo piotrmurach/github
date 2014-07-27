c7012c08cb2df317a0f9d88eb23acc41ec658a73
Piotr Murach <pmurach@gmail.com>
2014-07-27 18:05:32 +0100

    Change gitignore api to use new parser. Update docs.

--- lib/github_api/client/gitignore.rb
+++ lib/github_api/client/gitignore.rb
@@ -4,14 +4,17 @@ module Github
   # When you create a new GitHub repository via the API, you can specify a
   # .gitignore template to apply to the repository upon creation.
   class Client::Gitignore < API
-
-    # List all templates available to pass as an option when creating a repository.
+    # List all templates available to pass as an option
+    # when creating a repository.
     #
-    # = Examples
+    # @see https://developer.github.com/v3/gitignore/#listing-available-templates
+    #
+    # @example
     #  github = Github.new
     #  github.gitignore.list
     #  github.gitignore.list { |template| ... }
     #
+    # @api public
     def list(*args)
       arguments(args)
 
@@ -23,26 +26,30 @@ module Github
 
     # Get a single template
     #
-    # = Examples
+    # @see https://developer.github.com/v3/gitignore/#get-a-single-template
+    #
+    # @example
     #  github = Github.new
     #  github.gitignore.get "template-name"
     #
     # Use the raw media type to get the raw contents.
     #
-    # = Examples
+    # @examples
     #  github = Github.new
     #  github.gitignore.get "template-name", accept: 'applicatin/vnd.github.raw'
     #
+    # @api public
     def get(*args)
-      params = arguments(args, :required => [:name]).params
+      arguments(args, required: [:name])
+      params = arguments.params
 
       if (media = params.delete('accept'))
         params['accept'] = media
         params['raw'] = true
       end
 
-      get_request("/gitignore/templates/#{name}", params)
+      get_request("/gitignore/templates/#{arguments.name}", params)
     end
     alias :find :get
-  end # Gitignore
+  end # Client::Gitignore
 end # Github
