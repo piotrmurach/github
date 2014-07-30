3b04d494bbbf827f6827bb972aecb11d4ba92e36
Piotr Murach <pmurach@gmail.com>
2014-07-30 23:09:38 +0100

    Add default middleware.

---
+++ lib/github_api/middleware.rb
@@ -0,0 +1,30 @@
+# encoding: utf-8
+
+require 'github_api/response'
+require 'github_api/response/mashify'
+require 'github_api/response/jsonize'
+require 'github_api/response/raise_error'
+require 'github_api/response/header'
+
+module Github
+  class Middleware
+    def self.default(options = {})
+      api = options[:api]
+      proc do |builder|
+        builder.use Github::Request::Jsonize
+        builder.use Faraday::Request::Multipart
+        builder.use Faraday::Request::UrlEncoded
+        builder.use Github::Request::OAuth2, api.oauth_token if api.oauth_token?
+        builder.use Github::Request::BasicAuth, api.authentication if api.basic_authed?
+
+        builder.use Faraday::Response::Logger if ENV['DEBUG']
+        unless options[:raw]
+          builder.use Github::Response::Mashify
+          builder.use Github::Response::Jsonize
+        end
+        builder.use Github::Response::RaiseError
+        builder.adapter options[:adapter]
+      end
+    end
+  end # Middleware
+end # Github
