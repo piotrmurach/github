6722d652992a21cfd1af364b81265623743194b2
Kota Miyake <miyake.kota@gmail.com>
2014-09-10 23:40:56 +0800

    Set response messages to attributes of client error

--- spec/github/error/client_error_spec.rb
+++ spec/github/error/client_error_spec.rb
@@ -24,5 +24,27 @@ describe Github::Error::ClientError do
     it 'generates resolution line' do
       error.generate_message(attributes).should include "Resolution:\n Glue it!"
     end
+
+    before do
+      error.generate_message(attributes)
+    end
+
+    describe '#problem' do
+      it 'returns problem' do
+        error.problem.should eq attributes[:problem]
+      end
+    end
+
+    describe '#summary' do
+      it 'returns summary' do
+        error.summary.should eq attributes[:summary]
+      end
+    end
+
+    describe '#resolution' do
+      it 'returns resolution' do
+        error.resolution.should eq attributes[:resolution]
+      end
+    end
   end
 end # Github::Error::ClientError
