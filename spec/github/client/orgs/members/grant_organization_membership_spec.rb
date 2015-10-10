d0a480733ed70ae4cb75eadba4893b41bc3351e5
Shweta <shweta@joshsoftware.com>
2015-10-10 20:52:06 +0530

    Pass params to stub request[#238]

--- spec/github/client/orgs/members/grant_organization_membership_spec.rb
+++ spec/github/client/orgs/members/grant_organization_membership_spec.rb
@@ -7,40 +7,42 @@ describe Github::Client::Orgs::Members, '#grant_organization_membership' do
   let(:status) { 200 }
 
   before {
-    stub_put(request_path).to_return(:body => body, :status => status,
-                                     :headers => {:content_type => "application/json; charset=utf-8"})
+    stub_put(request_path).with(inputs).to_return(:body => body, :status => status,
+                                                  :headers => {:content_type => "application/json; charset=utf-8"})
   }
 
   context 'Add/update organization membership - unaffiliated user' do
-    
+
     let(:username) { 'anujaware' }
     let(:request_path) { "/orgs/#{org}/memberships/#{username}" }
     let(:body) { fixture('orgs/membership_to_unaffiliated_user.json') }
-    
+    let(:inputs) {{role: 'member'}}
+
     it 'should create organization membership with pending state' do
-      response = subject.grant_organization_membership org, username, {role: 'member'}
+      response = subject.grant_organization_membership org, username, inputs
       expect(response.state).to eq 'pending'
     end
 
     it 'should create organization membership with role member' do
-      response = subject.grant_organization_membership org, username, {role: 'member'}
+      response = subject.grant_organization_membership org, username, inputs
       expect(response.role).to eq 'member'
     end
   end
 
   context 'Add/update organization membership - affiliated user' do
-    
+
     let(:username) { 'anuja-joshi' }
     let(:request_path) { "/orgs/#{org}/memberships/#{username}" }
     let(:body) { fixture('orgs/membership_to_affilliated_user.json') }
-    
+    let(:inputs) {{role: 'admin'}}
+
     it 'should create organization membership with active state' do
-      response = subject.grant_organization_membership org, username, {role: 'admin'}
+      response = subject.grant_organization_membership org, username, inputs
       expect(response.state).to eq 'active'
     end
 
     it 'should update organization membership with role admin' do
-      response = subject.grant_organization_membership org, username, {role: 'admin'}
+      response = subject.grant_organization_membership org, username, inputs
       expect(response.role).to eq 'admin'
     end
   end
