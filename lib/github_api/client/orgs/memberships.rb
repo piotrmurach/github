a75350c20fad4ea9c7c836274c2303093681954e
Piotr Murach <pmurach@gmail.com>
2015-11-21 20:18:48 +0000

    Add get and remove membership to organizations.

--- lib/github_api/client/orgs/memberships.rb
+++ lib/github_api/client/orgs/memberships.rb
@@ -20,6 +20,40 @@ module Github
     end
     alias_method :all, :list
 
+    # Get organization membership
+    #
+    # In order to get a user's membership with an organization,
+    # the authenticated user must be an organization owner.
+    #
+    # @see https://developer.github.com/v3/orgs/members/#get-organization-membership
+    # @param [String] :org
+    # @param [String] :username
+    #
+    # @example
+    #   github = Github.new oauth_toke: '...'
+    #   github.orgs.memberships.get 'orgname', username: 'piotr'
+    #
+    # Get your organization membership
+    #
+    # @see https://developer.github.com/v3/orgs/members/#get-your-organization-membership
+    #
+    # @example
+    #   github = Github.new oauth_token
+    #   github.orgs.memberships.get 'orgname'
+    #
+    # @api public
+    def get(*args)
+      arguments(args, required: [:org])
+      params = arguments.params
+
+      if (username = params.delete('username'))
+        get_request("/orgs/#{arguments.org}/memberships/#{username}", params)
+      else
+        get_request("/user/memberships/orgs/#{arguments.org}", params)
+      end
+    end
+    alias_method :find, :get
+
     # Add/update user's membership with organization
     #
     # In order to create or update a user's membership with an organization,
@@ -50,5 +84,23 @@ module Github
     end
     alias_method :update, :create
     alias_method :add, :create
+
+    # Remove organization membership
+    #
+    # @see https://developer.github.com/v3/orgs/members/#remove-organization-membership
+    # @param [String] :org
+    # @param [String] :username
+    #
+    # @example
+    #   github = Github.new oauth_token: '...'
+    #   github.orgs.memberships.remove 'orgname', 'username'
+    #
+    # @api public
+    def delete(*args)
+      arguments(args, required: [:org, :username])
+
+      delete_request("/orgs/#{arguments.org}/memberships/#{arguments.username}", arguments.params)
+    end
+    alias_method :remove, :delete
   end # Client::Orgs::Memberships
 end # Github
