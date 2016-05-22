2aaa48334fd3f373d32051131f207c3caabf1e98
Piotr Murach <pmurach@gmail.com>
2016-05-22 19:08:24 +0100

    Change to spec syntax.

--- spec/github/client/gists/list_spec.rb
+++ spec/github/client/gists/list_spec.rb
@@ -2,14 +2,14 @@
 
 require 'spec_helper'
 
-describe Github::Client::Gists, '#list' do
+RSpec.describe Github::Client::Gists, '#list' do
   let(:user) { 'peter-murach' }
   let(:body) { fixture('gists/gists.json') }
   let(:status) { 200 }
 
   before {
-    stub_get(request_path).to_return(:body => body, :status => status,
-      :headers => {:content_type => "application/json; charset=utf-8"})
+    stub_get(request_path).to_return(body: body, status: status,
+      headers: {content_type: "application/json; charset=utf-8"})
   }
 
   after { reset_authentication_for(subject) }
@@ -20,24 +20,24 @@ describe Github::Client::Gists, '#list' do
     context "resource found" do
       it { should respond_to :all }
 
-      it "should get the resources" do
-        subject.list :user => user
-        a_get(request_path).should have_been_made
+      it "gets the resources" do
+        subject.list(user: user)
+        expect(a_get(request_path)).to have_been_made
       end
 
       it_should_behave_like 'an array of resources' do
         let(:requestable) { subject.list :user => user }
       end
 
-      it "should get gist information" do
-        gists = subject.list :user => user
-        gists.first.user.login.should == 'octocat'
+      it "gets gist information" do
+        gists = subject.list(user: user)
+        expect(gists.first.user.login).to eq('octocat')
       end
 
-      it "should yield to a block" do
+      it "yields to a block" do
         yielded = []
-        result = subject.list(:user => user) { |obj| yielded << obj }
-        yielded.should == result
+        result = subject.list(user: user) { |obj| yielded << obj }
+        expect(yielded).to eq(result)
       end
     end
 
@@ -49,9 +49,9 @@ describe Github::Client::Gists, '#list' do
   context 'when public' do
     let(:request_path) { "/gists/public" }
 
-    it "should get the resources" do
-      subject.list :public
-      a_get(request_path).should have_been_made
+    it "gets the resources" do
+      subject.list(:public)
+      expect(a_get(request_path)).to have_been_made
     end
 
     it_should_behave_like 'an array of resources' do
@@ -69,10 +69,10 @@ describe Github::Client::Gists, '#list' do
         :headers => {:content_type => "application/json; charset=utf-8"})
     end
 
-    it "should get the resources" do
+    it "gets the resources" do
       subject.list
-      a_get(request_path).with(:query => {:access_token => OAUTH_TOKEN}).
-        should have_been_made
+      expect(a_get(request_path).with(:query => {:access_token => OAUTH_TOKEN})).
+        to have_been_made
     end
   end
 end # list
