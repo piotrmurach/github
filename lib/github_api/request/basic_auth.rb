929ec0afe8f5d3fba7d596afc2f80f75f8665520
Piotr Murach <pmurach@gmail.com>
2016-06-19 22:22:41 +0100

    Change basic auth middleware to only rely on simple data.

--- lib/github_api/request/basic_auth.rb
+++ lib/github_api/request/basic_auth.rb
@@ -8,25 +8,28 @@ module Github
     class BasicAuth < Faraday::Middleware
       dependency 'base64'
 
+      # @api private
+      def initialize(app, *args)
+        @app    = app
+        @auth   = nil
+        options = args.extract_options!
+
+        if options.key?(:login) && !options[:login].nil?
+          credentials = "#{options[:login]}:#{options[:password]}"
+          @auth = Base64.encode64(credentials)
+          @auth.gsub!("\n", "")
+        end
+      end
+
+      # Update request headers
+      #
+      # @api private
       def call(env)
-        unless @auth.to_s.empty?
+        if @auth
           env[:request_headers].merge!('Authorization' => "Basic #{@auth}\"")
         end
 
-        @app.call env
-      end
-
-      def initialize(app, *args)
-        @app = app
-        credentials = ""
-        options = args.extract_options!
-        if options.has_key? :login
-          credentials = "#{options[:login]}:#{options[:password]}"
-        elsif options.has_key? :basic_auth
-          credentials = "#{options[:basic_auth]}"
-        end
-        @auth = Base64.encode64(credentials)
-        @auth.gsub!("\n", "")
+        @app.call(env)
       end
     end # BasicAuth
   end # Request
