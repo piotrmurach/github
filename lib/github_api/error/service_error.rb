632b431a828111528a54c6ab915274ea153a865d
Piotr Murach <pmurach@gmail.com>
2016-07-02 10:55:04 +0200

    Change service error to correctly parse response body containing multiple error messages and close issue #272

--- lib/github_api/error/service_error.rb
+++ lib/github_api/error/service_error.rb
@@ -6,55 +6,112 @@ module Github
   # Raised when GitHub returns any of the HTTP status codes
   module Error
     class ServiceError < GithubError
-      attr_reader :http_headers, :body, :status
+      # Add http status code method to error type
+      #
+      # @param [Integer] code
+      #   the status code
+      #
+      # @api public
+      def self.http_status_code(code)
+        define_method(:http_status_code) { code }
+      end
+
+      # A mapping of status codes and error types
+      #
+      # @return [Hash[Integer, Object]]
+      #
+      # @api public
+      def self.errors
+        @errors ||= Hash[
+          descendants.map { |klass| [klass.new({}).http_status_code, klass] }
+        ]
+      end
 
       MIN_BODY_LENGTH = 2
 
+      # Crate a ServiceError
+      #
+      # @param [Hash[Symbol]] response
+      #
+      # @api public
       def initialize(response)
-        @http_headers = response[:response_headers]
-        @body         = parse_body(response[:body])
-        @status       = response[:status]
+        @headers = response[:response_headers]
+        @body    = response[:body]
+        @status  = response[:status]
 
         super(create_message(response))
       end
 
+      private
+
+      # Create full error message
+      #
+      # @param [Hash[Symbol]] response
+      #   the http response
+      #
+      # @return [String]
+      #   the error message
+      #
+      # @api private
       def create_message(response)
-        "#{response[:method].to_s.upcase} #{response[:url]}: #{@status} #{@body}"
+        return if response.nil?
+
+        message = "#{response[:method].to_s.upcase} "
+        message << "#{response[:url]}: "
+        message << "#{@status} - #{parse_body(@body)}"
+        message
       end
 
-      def decode_body(body)
-        if body.respond_to?(:to_str) && body.length >= MIN_BODY_LENGTH
+      # Decode body information if in JSON format
+      #
+      # @param [String] body
+      #   the response body
+      #
+      # @api private
+      def decode_data(body)
+        if body.respond_to?(:to_str) &&
+           body.length >= MIN_BODY_LENGTH &&
+           @headers[:content_type] =~ /json/
+
           JSON.parse(body, symbolize_names: true)
         else
           body
         end
       end
 
+      # @api private
       def parse_body(body)
-        body = decode_body(body)
+        data = decode_data(body)
 
-        return '' if body.nil? || body.empty?
+        return '' if data.nil? || data.empty?
 
-        if body[:error]
-          body[:error]
-        elsif body[:errors]
-          error = Array(body[:errors]).first
-          error.is_a?(Hash) ? error[:message] : error
-        elsif body[:message]
-          body[:message]
-        else
-          ''
+        case data
+        when Hash
+          message = data[:message] ? data[:message] : ' '
+          docs = data[:documentation_url]
+          error = create_error_summary(data)
+          message << error if error
+          message << "\nSee: #{docs}" if docs
+          message
+        when String
+          data
         end
       end
 
-      def self.http_status_code(code)
-        define_method(:http_status_code) { code }
-      end
-
-      def self.errors
-        @errors ||= Hash[
-          descendants.map { |klass| [klass.new({}).http_status_code, klass] }
-        ]
+      # Create error summary from response body
+      #
+      # @return [String]
+      #
+      # @api private
+      def create_error_summary(data)
+        if data[:error]
+          return "\nError: #{data[:error]}"
+        elsif data[:errors]
+          message = "\nErrors:\n"
+          message << data[:errors].map do |error|
+            "Error: #{error.map {|key, val| "#{key}: #{val}"}.join(', ')}"
+          end.join("\n")
+        end
       end
     end # ServiceError
   end # Error
--- spec/unit/error/service_error/new_spec.rb
+++ spec/unit/error/service_error/new_spec.rb
@@ -3,39 +3,56 @@
 require 'spec_helper'
 
 RSpec.describe Github::Error::ServiceError, '#new' do
-  let(:message) { 'Requires authentication' }
   let(:url)     { 'https://api.github.com/user/repos' }
-  let(:body)    { "{\"message\":\"#{message}\"}" }
   let(:status)  { "401 Unauthorized" }
-  let(:response_headers) { {"status"=>"401 Unauthorized"} }
-  let(:response) {{
-    body: body,
-    status: status,
-    response_headers: response_headers,
-    url: url
-  }}
+  let(:message) { 'Requires authentication' }
+  let(:response_headers) { {content_type: "application/json; charset=utf-8"} }
 
-  it "parses body" do
-    error = described_class.new(response)
-
-    expect(error.body).to eq(message)
+  def create_response(body)
+    {
+      body: body,
+      status: status,
+      response_headers: response_headers,
+      url: url,
+      method: 'POST'
+    }
   end
 
-  it "parses http headers" do
+  it "creates message without errors summary" do
+    body = "{\"message\":\"#{message}\"}"
+    response = create_response(body)
+
     error = described_class.new(response)
 
-    expect(error.http_headers).to eq(response_headers)
+    expect(error.to_s).to eq("POST #{url}: #{status} - Requires authentication")
   end
 
-  it "parses status" do
+  it "creates message with single error summary" do
+    body = "{\"message\":\"Validation Failed\",\"error\":\"No commits between master and noexist\", \"documentation_url\":\"https://developer.github.com/enterprise/2.6/v3/pulls/#create-a-pull-request\"}"
+    response = create_response(body)
+
     error = described_class.new(response)
 
-    expect(error.status).to eq(status)
+    expect(error.to_s).to eq([
+      "POST #{url}: #{status} - Validation Failed",
+      "Error: No commits between master and noexist",
+      "See: https://developer.github.com/enterprise/2.6/v3/pulls/#create-a-pull-request"
+    ].join("\n"))
   end
 
-  it "assembles error message" do
+  it "creates message with multiple errors summary" do
+    body = "{\"message\":\"Validation Failed\",\"errors\":[{\"resource\":\"PullRequest\",\"code\":\"missing_field\",\"field\":\"head_sha\"},{\"resource\":\"PullRequest\",\"code\":\"missing_field\",\"field\":\"base_sha\"},{\"resource\":\"PullRequest\",\"code\":\"custom\",\"message\":\"No commits between master and noexist\"}],\"documentation_url\":\"https://developer.github.com/enterprise/2.6/v3/pulls/#create-a-pull-request\"}"
+    response = create_response(body)
+
     error = described_class.new(response)
 
-    expect(error.message).to eql(" #{url}: #{status} #{message}")
+    expect(error.to_s).to eq([
+      "POST #{url}: #{status} - Validation Failed",
+      "Errors:",
+      "Error: resource: PullRequest, code: missing_field, field: head_sha",
+      "Error: resource: PullRequest, code: missing_field, field: base_sha",
+      "Error: resource: PullRequest, code: custom, message: No commits between master and noexist",
+      "See: https://developer.github.com/enterprise/2.6/v3/pulls/#create-a-pull-request"
+    ].join("\n"))
   end
 end # Github::Error::ServiceError
