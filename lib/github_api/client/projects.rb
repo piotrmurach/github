ae06e95e012573d468d645fede807375ce90a3b9
Sam Davies <seivadmas@gmail.com>
2017-04-05 12:53:15 +0100

    Update a project

--- lib/github_api/client/projects.rb
+++ lib/github_api/client/projects.rb
@@ -27,5 +27,33 @@ module Github
       get_request("/projects/#{arguments.id}", params)
     end
     alias_method :find, :get
+
+    # Edit a project
+    #
+    # @param [Hash] params
+    # @option params [String] :name
+    #   Optional string
+    # @option params [String] :body
+    #   Optional string
+    # @option params [String] :state
+    #   Optional string
+    #
+    # @example
+    #  github = Github.new
+    #  github.projects.edit 1002604,
+    #    name: "Outcomes Tracker",
+    #    body: "The board to track work for the Outcomes application."
+    #
+    # @api public
+    def edit(*args)
+      arguments(args, required: [:id]) do
+        permit %w[ name body state ]
+      end
+
+      params = arguments.params
+      params['options'] = OPTIONS
+
+      patch_request("/projects/#{arguments.id}",params)
+    end
   end # Projects
 end # Github
