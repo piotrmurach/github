c5643ebbd8da984df35eb6909c0c181d153c188f
Kartik Luke Singh <kartikluke@gmail.com>
2017-06-13 21:38:40 +0530

    shifted the branches and branch spec to list and get

--- spec/github/client/repos/branches/get_spec.rb
+++ spec/github/client/repos/branches/get_spec.rb
@@ -2,7 +2,7 @@
 
 require 'spec_helper'
 
-describe Github::Client::Repos, '#branch' do
+describe Github::Client::Repos::Branches, '#get' do
   let(:user) { 'peter-murach' }
   let(:repo) { 'github' }
   let(:request_path) { "/repos/#{user}/#{repo}/branches/#{branch}" }
@@ -20,22 +20,22 @@ describe Github::Client::Repos, '#branch' do
     let(:status) { 200 }
 
     it "should find resources" do
-      subject.branch user, repo, branch
+      subject.get user, repo, branch
       a_get(request_path).should have_been_made
     end
 
     it "should return repository mash" do
-      repo_branch = subject.branch user, repo, branch
+      repo_branch = subject.get user, repo, branch
       repo_branch.should be_a Github::ResponseWrapper
     end
 
     it "should get repository branch information" do
-      repo_branch = subject.branch user, repo, branch
+      repo_branch = subject.get user, repo, branch
       repo_branch.name.should == 'master'
     end
   end
 
   it_should_behave_like 'request failure' do
-    let(:requestable) { subject.branch user, repo, branch }
+    let(:requestable) { subject.get user, repo, branch }
   end
-end # branch
+end # get
