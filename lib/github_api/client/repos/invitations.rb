e8e5679f0a060239344e212e6ee65e173c2055a5
Seth Siegler <usaseth@gmail.com>
2019-11-21 01:12:56 -0500

    adds list repo invitations

---
+++ lib/github_api/client/repos/invitations.rb
@@ -0,0 +1,29 @@
+# encoding: utf-8
+
+require_relative '../../api'
+
+module Github
+  class Client::Repos::Invitations < API
+    # List repo invitations
+    #
+    # @example
+    #  github = Github.new
+    #  github.repos.invitations.list 'user-name', 'repo-name'
+    #
+    # @example
+    #  github.repos.invitations.list 'user-name', 'repo-name' { |cbr| .. }
+    #
+    # @return [Array]
+    #
+    # @api public
+    def list(*args)
+      arguments(args, required: [:user, :repo])
+
+      response = get_request("/repos/#{arguments.user}/#{arguments.repo}/invitations", arguments.params)
+      return response unless block_given?
+      response.each { |el| yield el }
+    end
+    alias :all :list
+
+  end
+end
